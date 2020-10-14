061256744d6736deace22c5b6c53de23c11fbbd5
Jiayu <mg2015started@gmail.com>
2020-10-14 12:39:27 +0800

    Add rl model zoo

---
+++ zoo/policies/rl-agent/README.md
@@ -0,0 +1,10 @@
+## Install the rl agent zoo
+install command
+```bash
+pip install -e .
+```
+or from whl file:
+```bash
+pip install rl_agent-0.1-py3-none-any.whl 
+```
+
---
+++ zoo/policies/rl-agent/rl_agent/__init__.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+from smarts.core.agent import AgentSpec
+from .lane_space import (
+    ACTION_SPACE,
+    OBSERVATION_SPACE,
+    get_action_adapter,
+    agent_interface,
+    get_observation_adapter,
+)
+from .policy import RLPolicy
+from smarts.zoo.registry import register
+
+
+VERSION = 1
+CHECKPOINT_PATH = Path(__file__).resolve().parent / "checkpoint" / "checkpoint"
+
+
+def entrypoint(
+    goal_is_nearby_threshold=40,
+    lane_end_threshold=51,
+    lane_crash_distance_threshold=6,
+    lane_crash_ttc_threshold=2,
+    intersection_crash_distance_threshold=6,
+    intersection_crash_ttc_threshold=5,
+    target_speed=15,
+    lane_change_speed=12.5,
+):
+
+    return AgentSpec(
+        interface=agent_interface,
+        observation_adapter=get_observation_adapter(
+            goal_is_nearby_threshold=goal_is_nearby_threshold,
+            lane_end_threshold=lane_end_threshold,
+            lane_crash_distance_threshold=lane_crash_distance_threshold,
+            lane_crash_ttc_threshold=lane_crash_ttc_threshold,
+            intersection_crash_distance_threshold=intersection_crash_distance_threshold,
+            intersection_crash_ttc_threshold=intersection_crash_ttc_threshold,
+        ),
+        action_adapter=get_action_adapter(
+            target_speed=target_speed, lane_change_speed=lane_change_speed,
+        ),
+        policy_builder=lambda: RLPolicy(
+            load_path=CHECKPOINT_PATH,
+            policy_name="default_policy",
+            observation_space=OBSERVATION_SPACE,
+            action_space=ACTION_SPACE,
+        ),
+    )
+
+
+register(locator="rl-agent-v0", entry_point=entrypoint)
