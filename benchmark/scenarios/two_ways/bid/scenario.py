6336c31ed28dff33ef179c078dfe816fcda3e115
Julian Villella <julian.villella@hotmail.com>
2020-10-14 00:44:46 -0400

    Modernize two_ways/bid

--- benchmark/scenarios/two_ways/bid/scenario.py
+++ benchmark/scenarios/two_ways/bid/scenario.py
@@ -1,19 +1,15 @@
 from pathlib import Path
 
 from smarts.sstudio import gen_scenario
-from smarts.sstudio.types import (
-    Route,
-    Mission,
-    Scenario,
-)
+import smarts.sstudio.types as t
 
 missions = [
-    Mission(Route(begin=("-gneE1", 0, 5), end=("-gneE1", 0, 97))),
-    Mission(Route(begin=("gneE1", 0, 5), end=("gneE1", 0, 97))),
-    Mission(Route(begin=("-gneE1", 0, 20), end=("-gneE1", 0, 97))),
-    Mission(Route(begin=("gneE1", 0, 20), end=("gneE1", 0, 97))),
+    t.Mission(t.Route(begin=("-gneE1", 0, 5), end=("-gneE1", 0, 97))),
+    t.Mission(t.Route(begin=("gneE1", 0, 5), end=("gneE1", 0, 97))),
+    t.Mission(t.Route(begin=("-gneE1", 0, 20), end=("-gneE1", 0, 97))),
+    t.Mission(t.Route(begin=("gneE1", 0, 20), end=("gneE1", 0, 97))),
 ]
 
 gen_scenario(
-    Scenario(ego_missions=missions), output_dir=Path(__file__).parent,
+    t.Scenario(ego_missions=missions), output_dir=Path(__file__).parent,
 )
