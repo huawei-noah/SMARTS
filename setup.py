e51af2d2375d32560976d9becb02114ce1569e55
Liam <liamchzh@gmail.com>
2020-10-15 17:38:10 -0400

    Add long_description, to indicate detailed description of the package. (#1)
    * Create main.yml
    
    - test to revert
    
    * Delete main.yml
    
    * Add long_description, to indicate detailed description of the package.
    
    Co-authored-by: Tucker <mtuck2005@yahoo.ca>

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="smarts",
     description="Scalable Multi-Agent RL Training School",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version="0.4.1",
     packages=find_packages(exclude="tests"),
     include_package_data=True,
