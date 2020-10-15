de0b4cccc26dc4d0b6d9fa4c25d8cc453eccbdff
Liam Chen <liamchzh@gmail.com>
2020-10-15 14:32:59 -0400

    Bump version to 0.4.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="smarts",
     description="Scalable Multi-Agent RL Training School",
-    version="0.4.1",
+    version="0.4.2",
     packages=find_packages(exclude="tests"),
     include_package_data=True,
     zip_safe=True,
