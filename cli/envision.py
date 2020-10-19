af382457ea57742d17cbdfe00e0a157ea3113e67
Liam Chen <liamchzh@gmail.com>
2020-10-19 15:04:27 -0400

    Remove unused imports

--- cli/envision.py
+++ cli/envision.py
@@ -17,10 +17,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-import socketserver
-from functools import partial
-from http.server import SimpleHTTPRequestHandler
-
 import click
 
 from envision.server import run
