1a2b4653f6c934a394e9a07a8e7bb832a9131833
Tucker <montgomery.alban@huawei.com>
2020-11-03 00:11:38 -0500

    Add another test for episode reset

--- smarts/core/tests/test_smarts_memory_growth.py
+++ smarts/core/tests/test_smarts_memory_growth.py
@@ -9,7 +9,9 @@ from smarts.core.utils.episodes import episodes
 from smarts.core.agent_interface import AgentInterface, AgentType
 from smarts.core.agent import AgentSpec, AgentPolicy
 
-MEMORY_GROWTH_LIMIT = 5e4
+SMARTS_MEMORY_GROWTH_LIMIT = 5e4
+EPISODE_MEMORY_GROWTH_LIMIT = 1e4
+TIMESTEP_SEC = 0.1
 
 
 @pytest.fixture
@@ -59,8 +61,8 @@ def agent_type():
     return ((30, 1, -1), AgentType.Full)
 
 
-def _memory_buildup(
-    agent_id, seed, scenarios, episode_count, action, agent_type, max_episode_steps=10
+def env_and_spec(
+    action, agent_type, max_episode_steps, scenarios, seed=42, agent_id="Agent-006"
 ):
     class Policy(AgentPolicy):
         def act(self, obs):
@@ -78,10 +80,19 @@ def _memory_buildup(
         agent_specs={agent_id: agent_spec},
         headless=True,
         visdom=False,
-        timestep_sec=0.1,
+        timestep_sec=TIMESTEP_SEC,
         sumo_headless=True,
         seed=seed,
-        # envision_record_data_replay_path="./data_replay",
+    )
+
+    return (env, agent_spec)
+
+
+def _memory_buildup(
+    agent_id, seed, scenarios, episode_count, action, agent_type, max_episode_steps=10
+):
+    env, agent_spec = env_and_spec(
+        action, agent_type, max_episode_steps, scenarios, seed, agent_id
     )
 
     for episode in episodes(episode_count):
@@ -96,6 +107,47 @@ def _memory_buildup(
             observations, rewards, dones, infos = env.step({agent_id: agent_action})
             episode.record_step(observations, rewards, dones, infos)
 
+    env.close()
+
+
+def test_smarts_episode_memory_cleanup(
+    agent_id, seed, primative_scenarios, agent_params
+):
+    MAX_EPISODE_STEPS = 100
+    EPISODE_COUNT = 100
+
+    _, action, agent_type = agent_params
+
+    env, agent_spec = env_and_spec(
+        action, agent_type, MAX_EPISODE_STEPS, primative_scenarios, seed, agent_id
+    )
+    last_size = 0
+    episode_index = 0
+    for _ in episodes(EPISODE_COUNT):
+        agent = agent_spec.build_agent()
+        observations = env.reset()
+
+        gc.collect()
+        all_objects = muppy.get_objects()
+        next_size = muppy.get_size(all_objects)
+        all_objects = None
+        if episode_index > 0:
+            assert (
+                next_size - last_size < EPISODE_MEMORY_GROWTH_LIMIT
+            ), f"End size delta {next_size - last_size}"
+        last_size = next_size
+
+        dones = {"__all__": False}
+        while not dones["__all__"]:
+            agent_obs = observations[agent_id]
+            agent_action = agent.act(agent_obs)
+            observations, rewards, dones, infos = env.step({agent_id: agent_action})
+            # episode.record_step(observations, rewards, dones, infos)
+        agent_obs = None
+        agent_action = None
+        observations, rewards, dones, infos = None, None, None, None
+        episode = None
+        episode_index += 1
     env.close()
 
 
@@ -115,7 +167,7 @@ def test_smarts_basic_memory_cleanup(agent_id, seed, primative_scenarios, agent_
     end_size = muppy.get_size(all_objects)
     # Check for a major leak
     assert (
-        end_size - initial_size < MEMORY_GROWTH_LIMIT
+        end_size - initial_size < SMARTS_MEMORY_GROWTH_LIMIT
     ), f"End size delta {end_size - initial_size}"
 
 
@@ -137,7 +189,7 @@ def test_smarts_repeated_runs_memory_cleanup(
 
     # This "should" be roughly the same as `test_smarts_basic_memory_cleanup`
     assert (
-        end_size - initial_size < MEMORY_GROWTH_LIMIT
+        end_size - initial_size < SMARTS_MEMORY_GROWTH_LIMIT
     ), f"End size delta {end_size - initial_size}"
 
 
@@ -166,7 +218,7 @@ def test_smarts_fast_reset_memory_cleanup(agent_id, seed, social_agent_scenarios
 
     # Check for a major leak
     assert (
-        end_size - initial_size < MEMORY_GROWTH_LIMIT
+        end_size - initial_size < SMARTS_MEMORY_GROWTH_LIMIT
     ), f"End size delta {end_size - initial_size}"
 
 
@@ -186,5 +238,5 @@ def test_smarts_social_agent_scenario_memory_cleanup(
     end_size = muppy.get_size(all_objects)
 
     assert (
-        end_size - initial_size < MEMORY_GROWTH_LIMIT
+        end_size - initial_size < SMARTS_MEMORY_GROWTH_LIMIT
     ), f"End size delta {end_size - initial_size}"
