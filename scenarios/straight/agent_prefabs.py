b7f645514e36c166d357556e3592be540f0abb63
Julian Villella <julian.villella@hotmail.com>
2020-11-24 09:11:24 -0500

    Fix failing test_boids.py

--- scenarios/straight/agent_prefabs.py
+++ scenarios/straight/agent_prefabs.py
@@ -1,3 +1,4 @@
+import numpy as np
 from smarts.core.agent import Agent, AgentSpec
 from smarts.core.agent_interface import AgentInterface
 from smarts.core.controllers import ActionSpaceType
@@ -6,11 +7,13 @@ from smarts.zoo.registry import register
 
 class PoseBoidAgent(Agent):
     def act(self, obs):
-        returning = {
-            vehicle_id: [(idx + 1) * 0.2, 0, (idx + 1) * 0.2]
-            for idx, vehicle_id in enumerate(obs)
-        }
-        return returning
+        return {vehicle_id: self._single_act(obs_) for vehicle_id, obs_ in obs.items()}
+
+    def _single_act(self, obs):
+        wp = obs.waypoint_paths[0][:5][-1]
+        dist_to_wp = np.linalg.norm(wp.pos - obs.ego_vehicle_state.position[:2])
+        target_speed = 5  # m/s
+        return np.array([*wp.pos, wp.heading, dist_to_wp / target_speed])
 
 
 class TrajectoryBoidAgent(Agent):
@@ -42,7 +45,7 @@ class TrajectoryBoidAgent(Agent):
 register(
     locator="pose-boid-agent-v0",
     entry_point=lambda **kwargs: AgentSpec(
-        interface=AgentInterface(action=ActionSpaceType.Continuous, waypoints=True),
+        interface=AgentInterface(action=ActionSpaceType.TargetPose, waypoints=True),
         agent_builder=PoseBoidAgent,
     ),
 )
