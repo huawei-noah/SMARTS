9ed3ed983e21d51253e589bd4c69aa81fbeffb0c
Tucker <montgomery.alban@huawei.com>
2020-12-01 09:41:21 -0500

    Add check for capturing agents in trap

--- smarts/core/tests/test_trap_manager.py
+++ smarts/core/tests/test_trap_manager.py
@@ -16,6 +16,7 @@ from helpers.scenario import temp_scenario
 
 
 AGENT_ID = "006"
+AGENT_ID_2 = "007"
 
 
 @pytest.fixture
@@ -75,30 +76,20 @@ def empty_scenarios():
 @pytest.fixture
 def two_agent_capture_offset_tenth_of_second():
     with temp_scenario(name="straight", map="maps/straight.net.xml") as scenario_root:
-        traffic = t.Traffic(
-            flows=[
-                t.Flow(
-                    route=t.Route(begin=("west", 1, 0), end=("east", 1, "max"),),
-                    rate=50,
-                    actors={t.TrafficActor("car"): 1},
-                )
-            ]
-        )
-
         scenario = t.Scenario(
-            traffic={"all": traffic},
+            traffic={},
             ego_missions=[
                 t.Mission(
-                    t.Route(begin=("west", 1, 10), end=("east", 1, "max")),
+                    t.Route(begin=("west", 1, 20), end=("east", 1, "max")),
                     entry_tactic=t.TrapEntryTactic(
-                        wait_to_hijack_limit_s=3,
+                        wait_to_hijack_limit_s=0,
                         zone=t.MapZone(start=("west", 0, 1), length=100, n_lanes=3),
                     ),
                 ),
                 t.Mission(
-                    t.Route(begin=("west", 1, 10), end=("east", 1, "max")),
+                    t.Route(begin=("west", 2, 10), end=("east", 1, "max")),
                     entry_tactic=t.TrapEntryTactic(
-                        wait_to_hijack_limit_s=3.1,
+                        wait_to_hijack_limit_s=0.1,
                         zone=t.MapZone(start=("west", 0, 1), length=100, n_lanes=3),
                     ),
                 ),
@@ -107,7 +98,7 @@ def two_agent_capture_offset_tenth_of_second():
 
         gen_scenario(scenario, output_dir=scenario_root)
         yield Scenario.variations_for_all_scenario_roots(
-            [str(scenario_root)], [AGENT_ID, AGENT_ID + "1"]
+            [str(scenario_root)], [AGENT_ID, AGENT_ID_2]
         )
 
 
@@ -116,6 +107,18 @@ def smarts():
     smarts_ = SMARTS(
         agent_interfaces={
             AGENT_ID: AgentInterface.from_type(AgentType.Laner, max_episode_steps=30)
+        },
+        traffic_sim=SumoTrafficSimulation(time_resolution=0.1),
+    )
+    yield smarts_
+    smarts_.destroy()
+
+@pytest.fixture
+def smarts_two_agents():
+    smarts_ = SMARTS(
+        agent_interfaces={
+            AGENT_ID: AgentInterface.from_type(AgentType.Laner, max_episode_steps=30),
+            AGENT_ID_2: AgentInterface.from_type(AgentType.Laner, max_episode_steps=30),
         },
         traffic_sim=SumoTrafficSimulation(time_resolution=0.1),
     )
@@ -144,8 +147,10 @@ def test_emit_on_default(smarts: SMARTS, empty_scenarios):
     assert len(smarts.vehicle_index.vehicle_ids_by_actor_id(AGENT_ID)) == 1
 
 
-# def test_no_recapture_agent(smarts: SMARTS, two_agent_capture_offset_tenth_of_second):
-#     smarts.reset(next(two_agent_capture_offset_tenth_of_second))
-#     smarts.step({})
-#     smarts.step({})
-#     assert len(smarts.vehicle_index.agent_vehicle_ids) > 1
+def test_no_recapture_agent(smarts_two_agents: SMARTS, two_agent_capture_offset_tenth_of_second):
+    smarts_two_agents.reset(next(two_agent_capture_offset_tenth_of_second))
+    for i in range(3):
+        smarts_two_agents.step({})
+    assert len(smarts_two_agents.agent_manager.pending_agent_ids) < 1
+    assert len(smarts_two_agents.agent_manager.active_agents) > 1
+    assert len(smarts_two_agents.vehicle_index.agent_vehicle_ids) > 1
