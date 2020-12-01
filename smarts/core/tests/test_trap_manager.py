64b9b8647c2c88e7b35e2fe9baa0e2bc8d933bd1
Tucker <montgomery.alban@huawei.com>
2020-12-01 09:10:04 -0500

    Add final tests

--- smarts/core/tests/test_trap_manager.py
+++ smarts/core/tests/test_trap_manager.py
@@ -51,6 +51,67 @@ def scenarios():
 
 
 @pytest.fixture
+def empty_scenarios():
+    with temp_scenario(name="straight", map="maps/straight.net.xml") as scenario_root:
+        scenario = t.Scenario(
+            traffic={},
+            ego_missions=[
+                t.Mission(
+                    t.Route(begin=("west", 1, 10), end=("east", 1, "max")),
+                    entry_tactic=t.TrapEntryTactic(
+                        wait_to_hijack_limit_s=3,
+                        zone=t.MapZone(start=("west", 0, 1), length=100, n_lanes=3),
+                    ),
+                )
+            ],
+        )
+
+        gen_scenario(scenario, output_dir=scenario_root)
+        yield Scenario.variations_for_all_scenario_roots(
+            [str(scenario_root)], [AGENT_ID]
+        )
+
+
+@pytest.fixture
+def no_agent_capture_scenarios():
+    with temp_scenario(name="straight", map="maps/straight.net.xml") as scenario_root:
+        traffic = t.Traffic(
+            flows=[
+                t.Flow(
+                    route=t.Route(begin=("west", 1, 0), end=("east", 1, "max"),),
+                    rate=50,
+                    actors={t.TrafficActor("car"): 1},
+                )
+            ]
+        )
+
+        scenario = t.Scenario(
+            traffic={"all": traffic},
+            ego_missions=[
+                t.Mission(
+                    t.Route(begin=("west", 1, 10), end=("east", 1, "max")),
+                    entry_tactic=t.TrapEntryTactic(
+                        wait_to_hijack_limit_s=3,
+                        zone=t.MapZone(start=("west", 0, 1), length=100, n_lanes=3),
+                    ),
+                ),
+                t.Mission(
+                    t.Route(begin=("west", 1, 10), end=("east", 1, "max")),
+                    entry_tactic=t.TrapEntryTactic(
+                        wait_to_hijack_limit_s=3.1,
+                        zone=t.MapZone(start=("west", 0, 1), length=100, n_lanes=3),
+                    ),
+                ),
+            ],
+        )
+
+        gen_scenario(scenario, output_dir=scenario_root)
+        yield Scenario.variations_for_all_scenario_roots(
+            [str(scenario_root)], [AGENT_ID, AGENT_ID + "1"]
+        )
+
+
+@pytest.fixture
 def smarts():
     smarts_ = SMARTS(
         agent_interfaces={
@@ -76,8 +137,15 @@ def test_capture_vehicle(smarts: SMARTS, scenarios):
     )
 
 
-def test_emit_on_default(smarts: SMARTS, scenarios):
-    smarts.reset(next(scenarios))
-
+def test_emit_on_default(smarts: SMARTS, empty_scenarios):
+    smarts.reset(next(empty_scenarios))
+    assert smarts.elapsed_sim_time >= 3
     assert len(smarts.vehicle_index.agent_vehicle_ids) == 1
     assert len(smarts.vehicle_index.vehicle_ids_by_actor_id(AGENT_ID)) == 1
+
+
+# def test_no_recapture_agent(smarts: SMARTS, no_agent_capture_scenarios):
+#     smarts.reset(next(no_agent_capture_scenarios))
+#     smarts.step({})
+#     smarts.step({})
+#     assert len(smarts.vehicle_index.agent_vehicle_ids) > 1
