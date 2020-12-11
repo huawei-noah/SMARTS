8654d759e001a2d440d5acb1674dacc3787406d5
Julian Villella <julian.villella@hotmail.com>
2020-12-10 23:43:00 -0500

    Update envision client to use window location

--- envision/web/src/index.js
+++ envision/web/src/index.js
@@ -26,7 +26,7 @@ import App from "./components/app.js";
 import "antd/dist/antd.dark.css";
 
 let client = new Client({
-  endpoint: "http://localhost:8081",
+  endpoint: window.location.origin,
   delay: 2000,
   retries: 10,
 });
