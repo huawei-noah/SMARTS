60f332328c46fab229d83e2f5775c5365f43bd93
Liam Chen <liamchzh@gmail.com>
2020-12-15 19:07:40 -0500

    Add test scenario

---
+++ scenarios/cut_in/test_map/scenario.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from smarts.sstudio import gen_missions, gen_traffic
+from smarts.sstudio.types import (
+    Route,
+    Mission,
+    CutIn,
+)
+from smarts.sstudio import types as t
+
+
+scenario = str(Path(__file__).parent)
+
+traffic = t.Traffic(
+    flows=[
+        t.Flow(
+            route=Route(begin=("gneE20", 0, 55), end=("gneE20", 0, "max")),
+            rate=1,
+            actors={t.TrafficActor("car", max_speed=30/3.6): 1},
+        )
+    ],
+)
+
+gen_traffic(
+    scenario=scenario,
+    traffic=traffic,
+    name="basic"
+)
+
+
+gen_missions(
+    scenario=scenario,
+    missions=[
+        Mission(Route(begin=("gneE20", 1, 55), end=("gneE20", 0, "max")), task=CutIn()),
+    ],
+)
