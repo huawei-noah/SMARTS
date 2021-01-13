ed3656b2c13b976d50ce14d942c01e0d4a8dde28
Jeffer Peng <jf2peng@edu.uwaterloo.ca>
2021-01-13 13:56:57 -0500

    fix header_test script

--- header_test.sh
+++ header_test.sh
@@ -6,8 +6,14 @@ js_files="$(find ./envision/web/src -name '*.js')"
 files="$python_files $js_files"  
 
 # Check if file has license notice
+exit_code=0
 for file in $files; do
     if !(grep -q "Copyright (C)" $file); then
         echo "no copyright header in $file" >&2
+        exit_code=1
     fi
 done
+
+if [ $exit_code = 1 ]; then
+    exit 1
+fi
