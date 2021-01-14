fe7bd75203918445ba247ec3126d7cbdf8fe5e6f
Jeffer Jingfei Peng <36685292+JingfeiPeng@users.noreply.github.com>
2021-01-14 12:40:55 -0500

    Header-test (#406) (#415)
    * added header-test, generate header, ci test
    
    * enabled specific files to be passed as args to gen_header
    
    * Excluded auto-generated files

---
+++ header_test.sh
@@ -0,0 +1,21 @@
+# !/usr/bin/env bash
+
+# Collate all files excluding auto generated files of *_pb2.py and *_pb2_grpc.py
+python_files="$(find ./cli ./smarts ./benchmark ./envision -name '*.py' ! -name '*_pb2.py' ! -name '*_pb2_grpc.py')"
+js_files="$(find ./envision/web/src -name '*.js')"
+files="$python_files $js_files"  
+
+# Check if file has license notice
+exit_code=0
+for file in $files; do
+    if !(grep -q "Copyright (C)" $file); then
+        echo "no copyright header in $file" >&2
+        exit_code=1
+    fi
+done
+
+if [ $exit_code = 1 ]; then
+    exit 1
+else
+    echo "Header test passed"
+fi
