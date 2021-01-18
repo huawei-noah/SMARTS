d802177efdd84227cd4b907fdcf6134443b7f3bd
Kimia Hassanzadeh <kim.hszd@gmail.com>
2021-01-18 16:39:54 -0700

    fixing the format

--- ultra/train.py
+++ ultra/train.py
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -71,7 +71,7 @@ def train(task, num_episodes, policy_class, eval_info, timestep_sec, headless, s
                 os.makedirs(experiment_dir)
             with open(f"{experiment_dir}/spec.pkl", "wb") as spec_output:
                 dill.dump(spec, spec_output, pickle.HIGHEST_PROTOCOL)
-                
+
         while not dones["__all__"]:
             if episode.get_itr(AGENT_ID) >= 1000000:  # 1M observation break
                 finished = True
