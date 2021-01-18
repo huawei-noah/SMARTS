3e245300abd9d6d858089f6205d9a0e55e250971
Kimia Hassanzadeh <kim.hszd@gmail.com>
2021-01-18 16:31:35 -0700

    adding headers

--- ultra/utils/common.py
+++ ultra/utils/common.py
@@ -1,3 +1,24 @@
+# MIT License
+# 
+# Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 import torch
 import numpy as np
 import random
@@ -19,7 +40,6 @@ def rotate2d_vector(vectors, angle):
 
 
 def plot_experiment_data(experiment_path, save_dir):
-    # logs/experiment-2020.9.25-12:16:14/pkls/Train
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     with open(f"{experiment_path}/results.pkl", "rb") as handle:
