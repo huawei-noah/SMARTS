d27298cd9540fad3e0cffac0b1f9a66ae9b1af54
iman512003 <iman512003@gmail.com>
2021-01-25 16:46:05 -0500

    Add initial speed for uturn task

--- smarts/sstudio/types.py
+++ smarts/sstudio/types.py
@@ -372,6 +372,7 @@ class TrapEntryTactic(EntryTactic):
 class UTurn:
     trigger_radius: int = 100
     """This task will be triggered if any vehicles within this radius"""
+    initial_speed: float = 10
 
     target_lane_index: int = 0
 
