912907dd3e382de2e2f9f41da2a4baeb780d8d9e
Jenish Patel <jenish@ualberta.ca>
2021-01-25 10:04:18 -0700

    Trying to commit setup.py again

--- ultra/setup.py
+++ ultra/setup.py
@@ -19,6 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
 from setuptools import setup, find_packages
 from glob import glob
 from pathlib import Path
