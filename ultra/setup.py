22440f65dd240f15e5fa71e5f2e3a9e9bbb6feae
Jenish Patel <jenish@ualberta.ca>
2021-01-25 11:01:58 -0700

    formatted using black

--- ultra/setup.py
+++ ultra/setup.py
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,8 +25,8 @@ from glob import glob
 from pathlib import Path
 from collections import defaultdict
 
-''' Modified setup.py to include option for changing SMARTS version or by default
-the latest stable version SMARTS will used'''
+""" Modified setup.py to include option for changing SMARTS version or by default
+the latest stable version SMARTS will used """
 setup(
     name="ultra",
     description="Unprotected Left Turn using Reinforcement-learning Agents",
@@ -36,7 +36,7 @@ setup(
     zip_safe=True,
     python_requires=">=3.7",
     install_requires=[
-        "smarts[train]==0.4.6", # Stable version 
+        "smarts[train]==0.4.6",  # Stable version
         "setuptools>=41.0.0,!=50.0",
         "dill",
         "black==19.10b0",
