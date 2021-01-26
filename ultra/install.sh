f7ee8826cba077cdecb5c20852ca4702bddb77ad
Jenish Patel <jenish@ualberta.ca>
2021-01-26 13:10:05 -0700

    Updated install.sh to fix path issue

--- ultra/install.sh
+++ ultra/install.sh
@@ -4,8 +4,13 @@ pip install --upgrade pip
 pip install -e .
 
 dir="$PWD"
-smartsdir="$(dirname "$dir")"
+path_to_smarts_dir="$(dirname "$dir")"
 
-export PYTHONPATH=$smartsdir
+export_command=$'export PYTHONPATH='\"'${PYTHONPATH}:'$path_to_smarts_dir'"'
 
-
+if grep -q 'PYTHONPATH='\"'${PYTHONPATH}:'$path_to_smarts_dir'"' ~/.bashrc; then
+    echo "Path already exists in PYTHONPATH"
+else
+    echo '# Add absolute path of SMARTS repo to PYTHONPATH' >> ~/.bashrc
+    echo $export_command >> ~/.bashrc 
+fi
