51f576e7c2d59c511f62033ff4c04235905e7ec3
Jenish Patel <jenish@ualberta.ca>
2021-01-26 13:45:53 -0700

    Removed extra lines and added comments

--- ultra/install.sh
+++ ultra/install.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+# Installs SMARTS/ULTRA dependencies
 pip install --upgrade pip
 pip install -e .
 
+# Initializes the path to the SMARTS repo
 dir="$PWD"
 path_to_smarts_dir="$(dirname "$dir")"
-
 export_command=$'export PYTHONPATH='\"'${PYTHONPATH}:'$path_to_smarts_dir'"'
 
+# Checks whether .bashrc script is updated. If not, new path is added
 if grep -q 'PYTHONPATH='\"'${PYTHONPATH}:'$path_to_smarts_dir'"' ~/.bashrc; then
     echo "Path already exists in PYTHONPATH"
 else
