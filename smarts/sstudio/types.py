f1cba0ab177bd088fb1cd6116ce89eaadd391d58
Tucker <montgomery.alban@huawei.com>
2021-02-07 21:33:05 -0500

    Split shadowing and hijacking for consistant vehicles in bubble

--- smarts/sstudio/types.py
+++ smarts/sstudio/types.py
@@ -21,6 +21,7 @@ import logging
 import random
 import collections.abc as collections_abc
 from dataclasses import dataclass, field
+from sys import maxsize
 from typing import Any, Dict, Optional, Sequence, Tuple, Union
 
 from shapely.geometry import GeometryCollection, MultiPolygon, Polygon, Point
@@ -212,7 +213,7 @@ class BoidAgentActor(SocialAgentActor):
 
     # The max number of vehicles that this agent will control at a time. This value is
     # honored when using a bubble for boid dynamic assignment.
-    capacity: int = None
+    capacity: "BubbleLimits" = None
     """The capacity of the boid agent to take over vehicles."""
 
 
@@ -610,6 +611,20 @@ class PositionalZone(Zone):
 
 
 @dataclass(frozen=True)
+class BubbleLimits:
+    hijack_limit: int = maxsize
+    """The maximum number of vehicles the bubble can hijack"""
+    shadow_limit: int = maxsize
+    """The maximum number of vehicles the bubble can shadow"""
+
+    def __post_init__(self):
+        if self.shadow_limit is None:
+            raise ValueError("Shadow limit must be a non-negative real number")
+        if self.hijack_limit is None or self.shadow_limit < self.hijack_limit:
+            raise ValueError("Shadow limit must be >= hijack limit")
+
+
+@dataclass(frozen=True)
 class Bubble:
     """A descriptor that defines a capture bubble for social agents."""
 
@@ -622,7 +637,7 @@ class Bubble:
     # If limit != None it will only allow that specified number of vehicles to be
     # hijacked. N.B. when actor = BoidAgentActor the lesser of the actor capacity
     # and bubble limit will be used.
-    limit: int = None
+    limit: BubbleLimits = None
     """The maximum number of actors that could be captured."""
     exclusion_prefixes: Tuple[str, ...] = field(default_factory=tuple)
     """Used to exclude social actors from capture."""
