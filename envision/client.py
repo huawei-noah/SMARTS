aed9d0050b63078056a9bd443efd1c5b740fdb60
Adaickalavan Meiyappan 84166141 <adaickalavan.meiyappan@huawei.com>
2021-02-09 11:53:55 -0500

    Replace threading with multiprocessing and pipe.

--- envision/client.py
+++ envision/client.py
@@ -1,15 +1,17 @@
-# Copyright (C) 2020. Huawei Technologies Co., Ltd. All rights reserved.
-#
+# MIT License
+
+# Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
+
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-#
+
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-#
+
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -17,24 +19,28 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-import re
-import time
-import uuid
+
 import json
 import logging
+import multiprocessing
+import numpy as np
+import re
 import threading
+import time
+import uuid
+import warnings
+import websocket
+
 from datetime import datetime
+from pathlib import Path
 from queue import Queue
 from typing import Union
-from pathlib import Path
-import warnings
 
-import websocket
-import numpy as np
-
+from envision import types
 from smarts.core.utils.file import unpack
-from . import types
 
+import cProfile
+from envision import profiler
 
 class JSONEncoder(json.JSONEncoder):
     """This custom encoder is to support serializing more complex data from SMARTS
@@ -92,75 +98,80 @@ class Client:
         if endpoint is None:
             endpoint = "ws://localhost:8081"
 
-        self._logging_thread = None
-        self._logging_queue = Queue()
-        if output_dir:
-            self._logging_thread = self._spawn_logging_thread(output_dir, client_id)
-            self._logging_thread.start()
+        # self._logging_thread = None
+        # self._logging_queue = Queue()
+        # if output_dir:
+        #     self._logging_thread = self._spawn_logging_thread(output_dir, client_id)
+        #     self._logging_thread.start()
 
         if not self._headless:
-            self._state_queue = Queue()
-            self._thread = self._connect(
-                endpoint=f"{endpoint}/simulations/{client_id}/broadcast",
-                queue=self._state_queue,
-                num_retries=num_retries,
-                wait_between_retries=wait_between_retries,
-            )
-            self._thread.start()
+            self._p_output, self._p_input = multiprocessing.Pipe()
+            self._process = multiprocessing.Process(
+                target=self._connect,
+                args=(
+                    f"{endpoint}/simulations/{client_id}/broadcast",
+                    self._p_output,
+                    num_retries,
+                    wait_between_retries,
+                    )
+                )
+            self._process.daemon = True
+            self._process.start()
+    
+    # def _spawn_logging_thread(self, output_dir, client_id):
+    #     output_dir = Path(f"{output_dir}/{int(time.time())}")
+    #     output_dir.mkdir(parents=True, exist_ok=True)
+    #     path = (output_dir / client_id).with_suffix(".jsonl")
+    #     return threading.Thread(
+    #         target=self._write_log_state, args=(self._logging_queue, path), daemon=True
+    #     )
 
-    def _spawn_logging_thread(self, output_dir, client_id):
-        output_dir = Path(f"{output_dir}/{int(time.time())}")
-        output_dir.mkdir(parents=True, exist_ok=True)
-        path = (output_dir / client_id).with_suffix(".jsonl")
-        return threading.Thread(
-            target=self._write_log_state, args=(self._logging_queue, path), daemon=True
-        )
+    # @staticmethod
+    # def _write_log_state(queue, path):
+    #     with path.open("w", encoding="utf-8") as f:
+    #         while True:
+    #             state = queue.get()
+    #             if type(state) is Client.QueueDone:
+    #                 break
 
-    @staticmethod
-    def _write_log_state(queue, path):
-        with path.open("w", encoding="utf-8") as f:
-            while True:
-                state = queue.get()
-                if type(state) is Client.QueueDone:
-                    break
+    #             if not isinstance(state, str):
+    #                 state = unpack(state)
+    #                 state = json.dumps(state, cls=JSONEncoder)
 
-                if not isinstance(state, str):
-                    state = unpack(state)
-                    state = json.dumps(state, cls=JSONEncoder)
+    #             f.write(f"{state}\n")
+    
+    # @staticmethod
+    # def read_and_send(
+    #     path: str,
+    #     endpoint: str = "ws://localhost:8081",
+    #     timestep_sec: float = 0.1,
+    #     num_retries: int = 5,
+    #     wait_between_retries: float = 0.5,
+    # ):
+    #     client = Client(
+    #         endpoint=endpoint,
+    #         num_retries=num_retries,
+    #         wait_between_retries=wait_between_retries,
+    #     )
+    #     with open(path, "r") as f:
+    #         for line in f:
+    #             line = line.rstrip("\n")
+    #             time.sleep(timestep_sec)
+    #             client._send_raw(line)
 
-                f.write(f"{state}\n")
-
-    @staticmethod
-    def read_and_send(
-        path: str,
-        endpoint: str = "ws://localhost:8081",
-        timestep_sec: float = 0.1,
-        num_retries: int = 5,
-        wait_between_retries: float = 0.5,
-    ):
-        client = Client(
-            endpoint=endpoint,
-            num_retries=num_retries,
-            wait_between_retries=wait_between_retries,
-        )
-        with open(path, "r") as f:
-            for line in f:
-                line = line.rstrip("\n")
-                time.sleep(timestep_sec)
-                client._send_raw(line)
-
-            client.teardown()
-            logging.info("Finished Envision data replay")
-
+    #         client.teardown()
+    #         logging.info("Finished Envision data replay")
+    
     def _connect(
         self,
         endpoint,
-        queue,
+        p_output,
         num_retries: int = 50,
         wait_between_retries: float = 0.05,
     ):
-        threadlocal = threading.local()
+        connection_established = False
 
+        @profiler.profile_line
         def optionally_serialize_and_write(state: Union[types.State, str], ws):
             # if not already serialized
             if not isinstance(state, str):
@@ -181,19 +192,20 @@ class Client:
             self._log.error(f"Connection to Envision terminated with: {error}")
 
         def on_open(ws):
-            setattr(threadlocal, "connection_established", True)
+            nonlocal connection_established
+            connection_established = True
 
             while True:
-                state = queue.get()
+                state = p_output.recv()
                 if type(state) is Client.QueueDone:
                     ws.close()
                     break
 
                 optionally_serialize_and_write(state, ws)
-
-        def run_socket(endpoint, num_retries, wait_between_retries, threadlocal):
-            connection_established = False
-
+                
+        @profiler.profile_line
+        def run_socket(endpoint, num_retries, wait_between_retries):
+            nonlocal connection_established
             tries = 1
             while tries <= num_retries and not connection_established:
                 ws = websocket.WebSocketApp(
@@ -206,10 +218,6 @@ class Client:
                     warnings.filterwarnings("ignore", category=ResourceWarning)
                     ws.run_forever()
 
-                connection_established = getattr(
-                    threadlocal, "connection_established", False
-                )
-
                 if not connection_established:
                     self._log.info(
                         f"Attempting to connect to Envision tries={tries}/{num_retries}"
@@ -218,34 +226,30 @@ class Client:
                     tries += 1
                     time.sleep(wait_between_retries)
 
-        return threading.Thread(
-            target=run_socket,
-            args=(endpoint, num_retries, wait_between_retries, threadlocal),
-            daemon=True,  # If False, the proc will not terminate until this thread stops
-        )
+        run_socket(endpoint, num_retries, wait_between_retries)
 
     def send(self, state: types.State):
-        if not self._headless and self._thread.is_alive():
-            self._state_queue.put(state)
-        if self._logging_thread:
-            self._logging_queue.put(state)
+        if not self._headless and self._process.is_alive():
+            self._p_input.send(state)
+        # if self._logging_thread:
+        #     self._logging_queue.put(state)
 
-    def _send_raw(self, state: str):
-        """Skip serialization if we already have serialized data. This is useful if
-        we are reading from file and forwarding through the websocket.
-        """
-        self._state_queue.put(state)
+    # def _send_raw(self, state: str):
+    #     """Skip serialization if we already have serialized data. This is useful if
+    #     we are reading from file and forwarding through the websocket.
+    #     """
+    #     self._p_input.send(state)
 
     def teardown(self):
+        # self._logging_queue.put(Client.QueueDone())
+
         if not self._headless:
-            self._state_queue.put(Client.QueueDone())
+            self._p_input.send(Client.QueueDone())
+            self._process.join(timeout=3)
+            self._process = None
+            self._p_input.close()
+            self._p_output.close()
 
-        self._logging_queue.put(Client.QueueDone())
-
-        if not self._headless and self._thread:
-            self._thread.join(timeout=3)
-            self._thread = None
-
-        if self._logging_thread:
-            self._logging_thread.join(timeout=3)
-            self._logging_thread = None
+        # if self._logging_thread:
+        #     self._logging_thread.join(timeout=3)
+        #     self._logging_thread = None
