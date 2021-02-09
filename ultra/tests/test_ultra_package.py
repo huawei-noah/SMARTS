98c7fdf2510a50af2a04cbe0e2147bd452c7c8aa
Jenish Patel <jenish@ualberta.ca>
2021-02-09 16:34:46 -0700

    fixed typo in ultra-ci

--- ultra/tests/test_ultra_package.py
+++ ultra/tests/test_ultra_package.py
@@ -1,13 +1,34 @@
+# MIT License
+#
+# Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 import os, shutil, ray
 
 from ultra.train import train
 from ultra.scenarios.generate_scenarios import build_scenarios
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     save_dir = "tests/scenarios/maps/no-traffic/"
     if os.path.exists(save_dir):
         shutil.rmtree(save_dir)
-    
+
     build_scenarios(
         task="task00",
         level_name="eval_test",
@@ -16,7 +37,7 @@ if __name__ == '__main__':
         root_path="tests/scenarios",
         save_dir=save_dir,
     )
-    
+
     policy_class = "ultra.baselines.sac:sac-v0"
 
     ray.shutdown()
@@ -40,4 +61,3 @@ if __name__ == '__main__':
     except ray.exceptions.WorkerCrashedError as err:
         print(err)
         ray.shutdown()
-
