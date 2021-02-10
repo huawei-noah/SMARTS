2aaf488b968daffba772daa9f24b52629da131aa
Jeffer Peng <jf2peng@edu.uwaterloo.ca>
2021-02-10 16:26:10 -0500

    UTurn added

--- scenarios/uturn/scenario.py
+++ scenarios/uturn/scenario.py
@@ -1,20 +1,43 @@
+import os
 from pathlib import Path
 
-from smarts.sstudio import gen_missions
+from smarts.sstudio.genscenario import gen_scenario
 from smarts.sstudio.types import (
-    Route,
+    EndlessMission,
+    Flow,
+    JunctionEdgeIDResolver,
     Mission,
+    RandomRoute,
+    Route,
+    Scenario,
+    Traffic,
+    TrafficActor,
+    Via,
     UTurn,
 )
 
-scenario = str(Path(__file__).parent)
+ego_missions = [
+    Mission(
+        route=Route(begin=('edge-west-WE', 0, 50), end=('edge-west-EW', 0, 100)),
+        task=UTurn(initial_speed=10),
+    ),
+]
 
-gen_missions(
-    scenario=scenario,
-    missions=[
-        Mission(
-            route=Route(begin=('edge-west-WE', 0, 50), end=('edge-west-EW', 0, 100)),
-            task=UTurn(),
-        ),
-    ],
+scenario = Scenario(
+    traffic={
+        "basic": Traffic(
+            flows=[
+                Flow(
+                    route=Route(begin=('edge-west-EW', 0, 0), end=('edge-west-EW', 1, 'max')),
+                    rate=400,
+                    actors={TrafficActor(name="car"): 1.0},
+                )
+            ]
+        )
+    },
+    ego_missions=ego_missions,
+)
+
+gen_scenario(
+    scenario=scenario, output_dir=Path(__file__).parent,
 )
