0d7ef75f3faba180a1fbc63478dc9e61a21a2a3a
Adaickalavan Meiyappan 84166141 <adaickalavan.meiyappan@huawei.com>
2021-02-17 10:58:12 -0500

    Updated steps.

--- examples/single_agent.py
+++ examples/single_agent.py
@@ -85,6 +85,7 @@ if __name__ == "__main__":
 
 # cd /path/to/SMARTS
 # docker build --network=host -t smarts:v0.4.13 .
-# docker run -it --gpus all --network=host --env DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/kyber/workspaces/SMARTS/examples:/src/examples -d /dev/input:/dev/input smarts:v0.4.13
+# docker run -it --gpus all --network=host --env DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/kyber/workspaces/SMARTS/examples:/src/examples --device=/dev/input:/dev/input smarts:v0.4.13
 # scl envision start -s ./scenarios -p 8081 &
+# pip install line_profiler
 # python3.7 ./examples/single_agent.py ./scenarios/straight/
