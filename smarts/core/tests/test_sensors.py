6ae28d7e9e6ebcc4ceda00cc26bd145de6ac24d1
iman512003 <iman512003@gmail.com>
2021-02-18 13:50:49 -0500

    Fix cutin test

--- smarts/core/tests/test_sensors.py
+++ smarts/core/tests/test_sensors.py
@@ -174,6 +174,7 @@ def test_waypoints_sensor_with_cut_in_task(cut_in_scenarios):
     nei_vehicle = mock.Mock()
     nei_vehicle.speed = 10
     sim.elapsed_sim_time = 4
+    sim.timestep_sec = 0.08
     nei_vehicle.pose = Pose(
         position=np.array([25, -68, 0]), orientation=[0, 0, 0, 0], heading_=Heading(0),
     )
