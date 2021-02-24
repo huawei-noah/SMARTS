6e247689faf364d7f0e651afad64e592dab20659
Jenish Patel <jenish@ualberta.ca>
2021-02-24 23:16:40 +0000

    temporary fix for evaluation test

--- ultra/setup.py
+++ ultra/setup.py
@@ -45,7 +45,7 @@ setup(
     zip_safe=True,
     python_requires=">=3.7",
     install_requires=[
-        "smarts[train, test]==0.4.13",
+        "smarts[train,test]==0.4.13",
         "setuptools>=41.0.0,!=50.0",
         "dill",
         "black==20.8b1",
