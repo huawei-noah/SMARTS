3bfa29adac1452236afffb551ea010761b6d08e6
Jenish Patel <jenish@ualberta.ca>
2021-02-26 18:02:35 +0000

    formatted

--- ultra/ultra/baselines/agent_spec.py
+++ ultra/ultra/baselines/agent_spec.py
@@ -76,9 +76,15 @@ class BaselineAgentSpec(AgentSpec):
             policy_params = load_yaml(f"{policy_dir}/params.yaml")
             social_vehicle_params = dict(
                 encoder_key=policy_params["social_vehicles"]["encoder_key"],
-                social_policy_hidden_units=policy_params["social_vehicles"]["social_policy_hidden_units"],
-                social_polciy_init_std=policy_params["social_vehicles"]["social_polciy_init_std"],
-                num_social_features=policy_params["social_vehicles"]["num_social_features"],
+                social_policy_hidden_units=policy_params["social_vehicles"][
+                    "social_policy_hidden_units"
+                ],
+                social_polciy_init_std=policy_params["social_vehicles"][
+                    "social_polciy_init_std"
+                ],
+                num_social_features=policy_params["social_vehicles"][
+                    "num_social_features"
+                ],
                 seed=policy_params["social_vehicles"]["seed"],
                 observation_num_lookahead=policy_params["observation_num_lookahead"],
                 social_capacity=policy_params["social_vehicles"]["social_capacity"],
