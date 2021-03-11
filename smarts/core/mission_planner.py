6ea220a9ce5b2098a0102b20e102fa4720664392
iman512003 <iman512003@gmail.com>
2021-03-11 18:22:27 -0500

    Update controller and mission plannar for BM

--- smarts/core/mission_planner.py
+++ smarts/core/mission_planner.py
@@ -259,7 +259,7 @@ class MissionPlanner:
         # cut-in offset should consider the aggressiveness and the speed
         # of the other vehicle.
 
-        cut_in_offset = np.clip(18.5 - aggressiveness, 10, 18.5)
+        cut_in_offset = np.clip(20 - aggressiveness, 10, 20)
 
         if (
             abs(offset - (cut_in_offset + target_offset)) > 1
@@ -288,6 +288,17 @@ class MissionPlanner:
             cut_in_speed = target_velocity * 2.3
 
             speed_limit = cut_in_speed
+
+            # 1.5 m/s is the threshold for speed offset. If the vehicle speed
+            # is less than target_velocity plus this offset then it will not
+            # perform the cut-in task and instead the speed of the vehicle is
+            # increased.
+            if vehicle.speed < target_velocity + 1.5:
+                nei_wps = self._waypoints.waypoint_paths_on_lane_at(
+                    position, lane.getID(), 60
+                )
+                speed_limit = np.clip(target_velocity * 2.1, 0.5, 30)
+                self._task_is_triggered = False
 
         p0 = position
         p_temp = nei_wps[0][len(nei_wps[0]) // 3].pos
@@ -366,7 +377,7 @@ class MissionPlanner:
         # The aggressiveness is mapped from [0,10] to [0,0.8] domain which
         # represents the portion of intitial distantce which is used for
         # triggering the u-turn task.
-        aggressiveness = 0.8 * self._agent_behavior.aggressiveness / 10
+        aggressiveness = 0.3 + 0.5 * self._agent_behavior.aggressiveness / 10
         distance_threshold = 8
 
         if not self._uturn_is_initialized:
