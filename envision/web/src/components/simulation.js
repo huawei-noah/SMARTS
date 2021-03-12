04e26f0a78bd367904318c8e0604eb2ff7fc1fb6
Jeffer Peng <jf2peng@edu.uwaterloo.ca>
2021-03-12 00:57:03 -0500

    made sleep based on time difference between 2 consecutive frames

--- envision/web/src/components/simulation.js
+++ envision/web/src/components/simulation.js
@@ -157,17 +157,31 @@ export default function Simulation({
   useEffect(() => {
     let stopPolling = false;
     (async () => {
+      const msInSec = 1000;
       const it = client.worldstate(simulationId);
+      let prevElapsedTime = null;
+      let waitStartTime = null;
       let wstate_and_time;
       if (playing) wstate_and_time = await it.next();
       while (!stopPolling && playing && !wstate_and_time.done) {
         let wstate, elapsed_times;
         [wstate, elapsed_times] = wstate_and_time.value;
+        const currentTime = elapsed_times[0]
+        if (prevElapsedTime == null) {
+          // default: wait 50ms before playing the next frame
+          await sleep(50);
+        } else {
+          // msInSec*(currentTime-prevElapsedTime) is the time difference between 
+          //   current frame and previous frame
+          // Since we could have waited (Date.now() - waitStartTime) to get the current frame,
+          //    we deduct this amount from the time we will be waiting
+          await sleep(msInSec*(currentTime-prevElapsedTime)- (Date.now() - waitStartTime))
+        }
+        prevElapsedTime = currentTime
+
         setWorldState(wstate);
         onElapsedTimesChanged(...elapsed_times);
-
-        // play the frames every 50ms
-        await sleep(50);
+        waitStartTime = Date.now();
         wstate_and_time = await it.next();
       }
     })();
