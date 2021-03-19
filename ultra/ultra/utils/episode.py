ff266b540297aff2b46a474f384f8880ce721022
AlexLewandowski <lewandowskialexp@gmail.com>
2021-03-19 00:06:27 -0600

    Fix calculate_gap

--- ultra/ultra/utils/episode.py
+++ ultra/ultra/utils/episode.py
@@ -192,7 +192,7 @@ class Episode:
         gap_info = self.info["Gap"]
         for agent_id, agent_info in self.info["Train"].items():
             for key in agent_info.data:
-                if np.isscalar(gap_info.data[key]):
+                if np.isscalar(gap_info[agent_id].data[key]):
                     gap_info[agent_id].data[key] = (
                         self.info["Train"][agent_id].data[key]
                         - self.info["Evaluation"][agent_id].data[key]
