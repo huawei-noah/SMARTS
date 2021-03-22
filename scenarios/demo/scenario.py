bae8de2a82b824efd9052947c542f18606c33063
Jeffer Peng <jf2peng@edu.uwaterloo.ca>
2021-03-22 13:34:50 -0400

    WIP

--- scenarios/demo/scenario.py
+++ scenarios/demo/scenario.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 
 from smarts.sstudio import gen_scenario
@@ -7,24 +8,23 @@ traffic = t.Traffic(
     flows=[
         t.Flow(
             route=t.RandomRoute(),
-            rate=1,
-            actors={t.TrafficActor("car", max_speed=8): 1},
-            begin=2,
+            rate=60 * 60,
+            actors={
+                t.TrafficActor(
+                    name="car",
+                    vehicle_type=random.choice(
+                        ["passenger", "bus", "coach", "truck", "trailer"]
+                    ),
+                ): 1
+            },
         )
+        for i in range(5)
     ]
 )
 
 
-ego_missions = [
-    t.Mission(
-        t.Route(begin=("-gneE72", 1, 5), end=("-gneE71", 0, "max")),
-        task=t.CutIn(),
-    )
-]
-
 scenario = t.Scenario(
     traffic={"all": traffic},
-    # ego_missions=ego_missions,
 )
 
 gen_scenario(scenario, output_dir=str(Path(__file__).parent))
