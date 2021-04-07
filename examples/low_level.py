8ad2b8b9eea474fb708a3625976dd99a9ba6a266
Tucker <montgomery.alban@huawei.com>
2021-04-07 18:56:36 -0400

    Add low_level.py example

---
+++ examples/low_level.py
@@ -0,0 +1,71 @@
+import logging
+
+from envision.client import Client as Envision
+from examples import default_argument_parser
+from smarts.core.agent import AgentSpec
+from smarts.core.agent_interface import AgentInterface, AgentType
+from smarts.core.scenario import Scenario
+from smarts.core.smarts import SMARTS
+from smarts.core.sumo_traffic_simulation import SumoTrafficSimulation
+
+logging.basicConfig(level=logging.INFO)
+
+
+def make_smarts():
+    try:
+        smarts = SMARTS(
+            agent_interfaces={},
+            traffic_sim=SumoTrafficSimulation(headless=True, auto_start=True),
+            envision=Envision(),
+        )
+        smarts2 = SMARTS(
+            agent_interfaces={},
+            traffic_sim=SumoTrafficSimulation(headless=True, auto_start=True),
+            envision=Envision(),
+        )
+        yield smarts
+    except Exception as e:
+        raise e
+    finally:
+        smarts.destroy()
+        smarts2.destroy()
+
+
+def main(scenarios, headless, seed):
+    agent_spec = AgentSpec(
+        interface=AgentInterface.from_type(AgentType.Laner, max_episode_steps=None),
+        agent_builder=None,
+        observation_adapter=None,
+    )
+
+    stage_smarts = make_smarts()
+
+    smarts = next(stage_smarts)
+    scenarios_iterator = Scenario.scenario_variations(
+        scenarios,
+        list([]),
+    )
+
+    smarts.reset(next(scenarios_iterator))
+
+    for _ in range(80):
+        smarts.step({})
+        smarts.attach_sensors_to_vehicles(
+            agent_spec, smarts.vehicle_index.social_vehicle_ids()
+        )
+        obs, _, _, _ = smarts.observe_from(smarts.vehicle_index.social_vehicle_ids())
+
+    next(stage_smarts)
+
+    print("success")
+
+
+if __name__ == "__main__":
+    parser = default_argument_parser("observation-collection-example")
+    args = parser.parse_args()
+
+    main(
+        scenarios=args.scenarios,
+        headless=args.headless,
+        seed=args.seed,
+    )
