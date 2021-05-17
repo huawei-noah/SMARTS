50d0186183105dee02ae753b79f4e0f4d4d5ea85
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-05-17 13:44:52 -0400

    removed the additional files to reproduce the bug

--- scenarios/intersections/2lane/scenario.py
+++ scenarios/intersections/2lane/scenario.py
@@ -1,97 +1,77 @@
 import os
 
-from smarts.sstudio import gen_scenario
+from smarts.sstudio import gen_traffic
 from smarts.sstudio.types import (
+    Distribution,
+    Flow,
+    JunctionModel,
+    LaneChangingModel,
     Route,
-    Mission,
-    Scenario,
-    TrapEntryTactic,
+    Traffic,
+    TrafficActor,
 )
 
-from pathlib import Path
+scenario = os.path.dirname(os.path.realpath(__file__))
 
-mission = Mission(Route(begin=('edge-west-WE', 0, 10),
-                        end=('edge-north-SN', 0, 50)),
-                  entry_tactic=TrapEntryTactic(wait_to_hijack_limit_s=0,
-                                               default_entry_speed=3.0))
+impatient_car = TrafficActor(
+    name="car",
+    speed=Distribution(sigma=0.2, mean=1.0),
+    lane_changing_model=LaneChangingModel(impatience=1, cooperative=0.25),
+    junction_model=JunctionModel(
+        drive_after_red_time=1.5, drive_after_yellow_time=1.0, impatience=1.0
+    ),
+)
 
-gen_scenario(Scenario(ego_missions=[mission]),
-             output_dir=Path(__file__).parent)
+patient_car = TrafficActor(
+    name="car",
+    speed=Distribution(sigma=0.2, mean=0.8),
+    lane_changing_model=LaneChangingModel(impatience=0, cooperative=0.5),
+    junction_model=JunctionModel(drive_after_yellow_time=1.0, impatience=0.5),
+)
 
-# import os
-#
-# from smarts.sstudio import gen_traffic
-# from smarts.sstudio.types import (
-#     Distribution,
-#     Flow,
-#     JunctionModel,
-#     LaneChangingModel,
-#     Route,
-#     Traffic,
-#     TrafficActor,
-# )
-#
-# scenario = os.path.dirname(os.path.realpath(__file__))
-#
-# impatient_car = TrafficActor(
-#     name="car",
-#     speed=Distribution(sigma=0.2, mean=1.0),
-#     lane_changing_model=LaneChangingModel(impatience=1, cooperative=0.25),
-#     junction_model=JunctionModel(
-#         drive_after_red_time=1.5, drive_after_yellow_time=1.0, impatience=1.0
-#     ),
-# )
-#
-# patient_car = TrafficActor(
-#     name="car",
-#     speed=Distribution(sigma=0.2, mean=0.8),
-#     lane_changing_model=LaneChangingModel(impatience=0, cooperative=0.5),
-#     junction_model=JunctionModel(drive_after_yellow_time=1.0, impatience=0.5),
-# )
-#
-# vertical_routes = [
-#     ("north-NS", "south-NS"),
-#     ("south-SN", "north-SN"),
-# ]
-#
-# horizontal_routes = [
-#     ("west-WE", "east-WE"),
-#     ("east-EW", "west-EW"),
-# ]
-#
-# turn_left_routes = [
-#     ("south-SN", "west-EW"),
-#     ("west-WE", "north-SN"),
-#     ("north-NS", "east-WE"),
-#     ("east-EW", "south-NS"),
-# ]
-#
-# turn_right_routes = [
-#     ("south-SN", "east-WE"),
-#     ("west-WE", "south-NS"),
-#     ("north-NS", "west-EW"),
-#     ("east-EW", "north-SN"),
-# ]
-#
-# for name, routes in {
-#     "vertical": vertical_routes,
-#     "horizontal": horizontal_routes,
-#     "unprotected_left": turn_left_routes,
-#     "turns": turn_left_routes + turn_right_routes,
-#     "all": vertical_routes + horizontal_routes + turn_left_routes + turn_right_routes,
-# }.items():
-#     traffic = Traffic(
-#         flows=[
-#             Flow(
-#                 route=Route(
-#                     begin=(f"edge-{r[0]}", 0, "random"),
-#                     end=(f"edge-{r[1]}", 0, "random"),
-#                 ),
-#                 rate=1,
-#                 actors={impatient_car: 0.5, patient_car: 0.5},
-#             )
-#             for r in routes
-#         ]
-#     )
-#
-#     gen_traffic(scenario, traffic, name=name)
+vertical_routes = [
+    ("north-NS", "south-NS"),
+    ("south-SN", "north-SN"),
+]
+
+horizontal_routes = [
+    ("west-WE", "east-WE"),
+    ("east-EW", "west-EW"),
+]
+
+turn_left_routes = [
+    ("south-SN", "west-EW"),
+    ("west-WE", "north-SN"),
+    ("north-NS", "east-WE"),
+    ("east-EW", "south-NS"),
+]
+
+turn_right_routes = [
+    ("south-SN", "east-WE"),
+    ("west-WE", "south-NS"),
+    ("north-NS", "west-EW"),
+    ("east-EW", "north-SN"),
+]
+
+for name, routes in {
+    "vertical": vertical_routes,
+    "horizontal": horizontal_routes,
+    "unprotected_left": turn_left_routes,
+    "turns": turn_left_routes + turn_right_routes,
+    "all": vertical_routes + horizontal_routes + turn_left_routes + turn_right_routes,
+}.items():
+    traffic = Traffic(
+        flows=[
+            Flow(
+                route=Route(
+                    begin=(f"edge-{r[0]}", 0, "random"),
+                    end=(f"edge-{r[1]}", 0, "random"),
+                ),
+                rate=1,
+                actors={impatient_car: 0.5, patient_car: 0.5},
+            )
+            for r in routes
+        ]
+    )
+
+    gen_traffic(scenario, traffic, name=name)
