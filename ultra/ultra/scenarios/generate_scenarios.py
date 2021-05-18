4e7424c644bac3f10dde848702c6ea399525628f
JenishPatel99 <55423380+JenishPatel99@users.noreply.github.com>
2021-05-18 09:21:37 -0600

    ULTRA Simple level (#857)
    * added simple level
    
    * classified simple levels, position offsets
    
    Classified simple levels into v0, v1, v2. Move the position offsets to route distribution
    
    * small modification
    
    * pos_offset not mandatory
    
    * tweaked distribution flags & formatted
    
    * moved cross intersection types to test section in levels
    
    * bubbles flags added
    
    * renamed simple distribution, added a more general simple level
    
    * reinstated blocks distribution
    
    * Apply suggestions from code review
    
    Co-authored-by: christianjans <49890535+christianjans@users.noreply.github.com>
    
    * moved debugging levels from task1 -> taskX
    
    Co-authored-by: christianjans <49890535+christianjans@users.noreply.github.com>

--- ultra/ultra/scenarios/generate_scenarios.py
+++ ultra/ultra/scenarios/generate_scenarios.py
@@ -366,6 +366,11 @@ def generate_left_turn_missions(
                 )
                 for ego_mission_config in missions
             ]
+            # Not all routes need to have a custom start/end offset
+            if "pos_offsets" in route_info:
+                pos_offsets = route_info["pos_offsets"]
+            else:
+                pos_offsets = None
             flows, vehicles_log_info = generate_social_vehicles(
                 route_distribution=route_info["distribution"],
                 begin_time_init=route_info["begin_time_init"],
@@ -378,6 +383,7 @@ def generate_left_turn_missions(
                 ],
                 stops=stops,
                 deadlock_optimization=route_info["deadlock_optimization"],
+                pos_offsets=pos_offsets,
                 stopwatcher_info=stopwatcher_info,
                 traffic_params={"speed": speed, "traffic_density": traffic_density},
             )
@@ -497,6 +503,7 @@ def generate_social_vehicles(
     stopwatcher_info,
     traffic_params,
     stops,
+    pos_offsets,
     begin_time_init=None,
     deadlock_optimization=True,
 ):
@@ -590,13 +597,23 @@ def generate_social_vehicles(
             )
         else:
             behavior = get_social_vehicle_behavior(behavior_idx)
+            if pos_offsets:
+                start_offset = random.randint(
+                    pos_offsets["start"][0], pos_offsets["start"][1]
+                )
+                end_offset = random.randint(
+                    pos_offsets["end"][0], pos_offsets["end"][1]
+                )
+            else:
+                start_offset = "base"
+                end_offset = "max"
             flows.append(
                 Flow(
                     begin=begin_time,
                     end=end_time,
                     route=Route(
-                        begin=(f"edge-{start_lane}", start_lane_id, "base"),
-                        end=(f"edge-{end_lane}", end_lane_id, "max"),
+                        begin=(f"edge-{start_lane}", start_lane_id, start_offset),
+                        end=(f"edge-{end_lane}", end_lane_id, end_offset),
                     ),
                     rate=1,
                     actors={behavior: 1.0},
