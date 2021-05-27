Moved speed re-computation to genhistories to get out of step loop.    - use next pos instead of prev pos in speed computation Fixed minor bug with obs_time hack.

smarts/core/traffic_history_provider.py
@@ -41,7 +41,6 @@ class TrafficHistoryProvider(Provider):
         self._this_step_dones = set()
         self._vehicle_id_prefix = "history-vehicle-"
         self._start_time_offset = 0
-        self._prev_pos = {}
 
     @property
     def start_time(self):
@@ -79,7 +78,6 @@ class TrafficHistoryProvider(Provider):
             self._histories.disconnect()
             self._histories = None
         self._replaced_vehicle_ids = set()
-        self._prev_pos = {}
 
     @property
     def action_spaces(self) -> Set[ActionSpaceType]:
@@ -106,28 +104,6 @@ class TrafficHistoryProvider(Provider):
             )
         return "passenger"
 
-    def _compute_speed(
-        self, v_id: str, pos_x: float, pos_y: float, dt: float, default_speed: float
-    ) -> float:
-        # Here we re-compute the speed,
-        # not trusting what was in the original dataset.
-        # The reason is because in the NGSIM dataset,
-        # for example, it is "instantaneous" velocity,
-        # which does not match with dPos/dt.
-        # Since we speed will be dPos/dt in our Imitation action space,
-        # we force that to be the case here.
-        # XXX: speeds could be computed up front when
-        # creating the .shf file to optimize step time.
-        cur_pos = np.array((pos_x, pos_y))
-        prev_pos = self._prev_pos.get(v_id)
-        result = (
-            np.linalg.norm(cur_pos - prev_pos) / dt
-            if prev_pos is not None
-            else default_speed
-        )
-        self._prev_pos[v_id] = cur_pos
-        return result
-
     def step(
         self, provider_actions, dt: float, elapsed_sim_time: float
     ) -> ProviderState:
@@ -148,7 +124,6 @@ class TrafficHistoryProvider(Provider):
             default_dims = VEHICLE_CONFIGS[vehicle_type].dimensions
             pos_x = hr.position_x + self._map_location_offset[0]
             pos_y = hr.position_y + self._map_location_offset[1]
-            speed = self._compute_speed(v_id, pos_x, pos_y, dt, hr.speed)
             vehicles.append(
                 VehicleState(
                     vehicle_id=self._vehicle_id_prefix + v_id,
@@ -164,7 +139,7 @@ class TrafficHistoryProvider(Provider):
                         # Note: Neither NGSIM nor INTERACTION provide the vehicle height
                         height=default_dims.height,
                     ),
-                    speed=speed,
+                    speed=hr.speed,
                     source="HISTORY",
                 )
             )

smarts/sstudio/genhistories.py
@@ -257,8 +257,8 @@ class NGSIM(_TrajectoryDataset):
         # .22 is a magic number corresponding to roughly 5mph.
         # If the car is going very slowly, heading results
         # get a bit wacky due to precision problems.
-        no_v1 = any(np.isnan(p)) or badc or abs(d1) < 0.22
-        no_v2 = any(np.isnan(n)) or badc or abs(d2) < 0.22
+        no_v1 = any(np.isnan(p)) or badc or d1 < 0.22
+        no_v2 = any(np.isnan(n)) or badc or d2 < 0.22
         if no_v1 and no_v2:
             return self._prev_heading
         elif no_v1:
@@ -270,6 +270,16 @@ class NGSIM(_TrajectoryDataset):
         r = math.atan2(avg[1], avg[0]) % (2 * math.pi)
         self._prev_heading = r - math.pi / 2
         return self._prev_heading
+
+    def _cal_speed(self, window):
+        p = window[0, :2]
+        c = window[1, :2]
+        badc = any(np.isnan(c))
+        badp = any(np.isnan(p))
+        if badc or badp:
+            return None
+        # XXX: could try to divide by sim_time delta here instead of assuming .1s
+        return np.linalg.norm(c - p) / 0.1
 
     def _transform_all_data(self):
         self._log.debug("transforming NGSIM data")
@@ -353,6 +363,14 @@ class NGSIM(_TrajectoryDataset):
                 for values in stride(v, (d0 - 2, 3, d1), (s0, s0, s1))
             ]
             df.loc[same_car, "heading_rad"] = headings + [headings[-1], headings[-1]]
+            # ... and new speeds (based on these smoothed positions)
+            # (This also overcomes problem that NGSIM speeds are "instantaneous"
+            # and so don't match with dPos/dt, which can affect some models.)
+            speeds = [
+                self._cal_speed(values)
+                for values in stride(v, (d0 - 1, 2, d1), (s0, s0, s1))
+            ]
+            df.loc[same_car, "speed_discrete"] = speeds + [speeds[-1]]
 
         return df
 
@@ -362,6 +380,8 @@ class NGSIM(_TrajectoryDataset):
             yield t
 
     def column_val_in_row(self, row, col_name):
+        if col_name == "speed":
+            return row.speed_discrete if row.speed_discrete else row.speed
         return getattr(row, col_name, None)
 
 