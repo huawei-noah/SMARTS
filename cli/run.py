495828519f56a7b3338006df910afec7094844fb
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-06-04 14:58:03 -0400

    bug fix

--- cli/run.py
+++ cli/run.py
@@ -72,7 +72,7 @@ def run_cli():
     pass
 
 
-@run_cli.command(name="run", help="Run an experiment on a scenario")
+@run_cli.command(help="Run an experiment on a scenario")
 @click.option(
     "--envision",
     is_flag=True,
