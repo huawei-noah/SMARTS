5cfc53a52520dbc30fe0b193159d26726c4ac66f
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-06-04 14:43:58 -0400

    bug fix

--- cli/run.py
+++ cli/run.py
@@ -37,19 +37,51 @@ def kill_process_group_afterwards():
         os.killpg(0, signal.SIGKILL)
 
 
-@click.group()
+class DefaultCommandGroup(click.Group):
+    """allow a default command for a group"""
+
+    def command(self, *args, **kwargs):
+        default_command = kwargs.pop('default_command', False)
+        if default_command and not args:
+            kwargs['name'] = kwargs.get('name', '<>')
+        decorator = super(
+            DefaultCommandGroup, self).command(*args, **kwargs)
+
+        if default_command:
+            def new_decorator(f):
+                cmd = decorator(f)
+                self.default_command = cmd.name
+                return cmd
+
+            return new_decorator
+
+        return decorator
+
+    def resolve_command(self, ctx, args):
+        try:
+            # test if the command parses
+            return super(
+                DefaultCommandGroup, self).resolve_command(ctx, args)
+        except click.UsageError:
+            # command did not parse, assume it is the default command
+            args.insert(0, self.default_command)
+            return super(
+                DefaultCommandGroup, self).resolve_command(ctx, args)
+
+
+@click.group(name="run", cls=DefaultCommandGroup)
 def run_cli():
     pass
 
 
-@run_cli.command(name="run", help="Run an experiment on a scenario")
+@run_cli.command(default_command=True, help="Run an experiment on a scenario")
 @click.option(
     "--envision",
     is_flag=True,
     default=False,
     help="Start up Envision server when running an experiment",
 )
-@click.argument("script_path", type=click.Path(exists=True), metavar="<script>")
+@click.argument("script_path", type=click.Path(exists=True), metavar="<script>", required=True)
 @click.argument("script_args", nargs=-1, type=click.UNPROCESSED)
 def run_experiment(envision, script_path, script_args):
     # with kill_process_group_afterwards():
