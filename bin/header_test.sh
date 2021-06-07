614c33020beddfddef466f03c0aeb3447d517731
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-06-07 18:06:15 -0400

    path fixes

--- bin/header_test.sh
+++ bin/header_test.sh
@@ -1,8 +1,8 @@
 # !/usr/bin/env bash
 
 # Collate all files excluding auto generated files of *_pb2.py and *_pb2_grpc.py
-python_files="$(find ../benchmark ../cli ../envision ../smarts ../ultra -name '*.py' ! -name '*_pb2.py' ! -name '*_pb2_grpc.py')"
-js_files="$(find ../envision/web/src -name '*.js')"
+python_files="$(find ./baselines/marl_benchmark ./cli ./envision ./smarts ./ultra -name '*.py' ! -name '*_pb2.py' ! -name '*_pb2_grpc.py')"
+js_files="$(find ./envision/web/src -name '*.js')"
 files="$python_files $js_files"  
 
 # Check if file has license notice
