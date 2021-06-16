73bf5217679a515c8f8f855d2967652c15c80cac
christianjans <cjjans@ualberta.ca>
2021-06-16 22:04:18 +0000

    Initial changes

--- ultra/ultra/env/ultra_env.py
+++ ultra/ultra/env/ultra_env.py
@@ -19,22 +19,29 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+from collections import deque
+import copy
 import glob
 import math
 import os
 from itertools import cycle
 from sys import path
+from typing import Dict
 
 import numpy as np
 import yaml, inspect
 from scipy.spatial import distance
 
 from smarts.core.scenario import Scenario
+from smarts.core.sensors import Observation, TopDownRGB
 from smarts.env.hiway_env import HiWayEnv
 import ultra.adapters as adapters
 from ultra.baselines.common.yaml_loader import load_yaml
 
 path.append("./ultra")
+
+
+_STACK_SIZE = 4
 
 
 class UltraEnv(HiWayEnv):
@@ -56,6 +63,8 @@ class UltraEnv(HiWayEnv):
             _scenarios = glob.glob(f"{self.scenarios['train']}")
         else:
             _scenarios = glob.glob(f"{self.scenarios['test']}")
+
+        self.observations_stack = deque(maxlen=_STACK_SIZE)
 
         super().__init__(
             scenarios=_scenarios,
@@ -90,6 +99,10 @@ class UltraEnv(HiWayEnv):
 
         observations, rewards, agent_dones, extras = self._smarts.step(agent_actions)
 
+        # TODO: Stack observations here that need to be stacked.
+        self.observations_stack.append(copy.deepcopy(observations))
+        observations = self._stack_top_down_rgbs(observations)
+
         infos = {
             agent_id: {"score": value, "env_obs": observations[agent_id]}
             for agent_id, value in extras["scores"].items()
@@ -112,6 +125,15 @@ class UltraEnv(HiWayEnv):
 
         return observations, rewards, agent_dones, infos
 
+    def reset(self):
+        observations = super().reset()
+
+        for _ in range(_STACK_SIZE):
+            self.observations_stack.append(copy.deepcopy(observations))
+        observations = self._stack_top_down_rgbs(observations)
+
+        return observations
+
     def get_task(self, task_id, task_level):
         base_dir = os.path.join(os.path.dirname(__file__), "../")
         config_path = os.path.join(base_dir, "config.yaml")
@@ -131,3 +153,65 @@ class UltraEnv(HiWayEnv):
             "timestep_sec": self.timestep_sec,
             "headless": self.headless,
         }
+
+    # TODO: Should this return an observation or mutate the given observation?
+    def _stack_top_down_rgbs(
+        self, current_observations: Dict[str, Observation]
+    ) -> Observation:
+        """For every observation in current_observations with a non-None top_down_rgb
+        attribute, this method replaces the TopDownRGB attribute of the observation with
+        a TopDownRGB instance whose data attribute is a NumPy array of stacked
+        TopDownRGB data of the current observation and previous observations.
+
+        For each agent with an observation, if they have a TopDownRGB in their
+        observation, stack the data of the TopDownRGB with previous TopDownRGB data in
+        the observations stack. If the agent does not have previous TopDownRGB data in
+        the observations stack, copy the current TopDownRGB data to fill the stack.
+
+        Note: This method expects that current_observations is in
+            self.stacked_observations as the rightmost element.
+
+        Note: This can be extended to stack other parts of the observations such as the
+            drivable area grid map, lidar, occupancy grid map, etc.
+
+        Note: This has not been tested with a multi-agent scenario.
+
+        Note: This is like an environment observation adapter.
+
+        Example:
+            For a 64x64 TopDownRGB image received from SMARTS, the data component of the
+            TopDownRGB is a NumPy array with shape (64, 64, 3). Stack this current data
+            with data from the previous observations to make the current TopDownRGB data
+            to be an array with shape (_STACK_SIZE, 64, 64, 3).
+
+        Args:
+            current_observation (Observation): The environment observation.
+
+        Returns:
+            Observation: The environment observation with stacked TopDownRGB data.
+        """
+        for agent_id, current_observation in current_observations.items():
+            if current_observation.top_down_rgb:
+                # This agent's observation contains a TopDownRGB, stack its data.
+                current_top_down_rgb = current_observation.top_down_rgb
+
+                top_down_rgb_data = []
+                for observations in self.observations_stack:
+                    if agent_id in observations:
+                        top_down_rgb_data.append(
+                            observations[agent_id].top_down_rgb.data
+                        )
+                    else:
+                        # Use the current observation's TopDownRGB data if this agent
+                        # doesn't have previous observations to use to build the stack.
+                        top_down_rgb_data.append(current_top_down_rgb.data)
+                stacked_top_down_rgb_data = np.stack(top_down_rgb_data)
+
+                # Create the new TopDownRGB with stacked data.
+                stacked_top_down_rgb = TopDownRGB(
+                    metadata=current_top_down_rgb.metadata,
+                    data=stacked_top_down_rgb_data,
+                )
+
+                current_observations[agent_id].top_down_rgb = stacked_top_down_rgb
+        return current_observations
