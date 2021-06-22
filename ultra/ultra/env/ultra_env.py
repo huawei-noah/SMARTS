be28b33dba5e7fa63b92e861c72d2377c78afc46
christianjans <49890535+christianjans@users.noreply.github.com>
2021-06-22 07:40:20 -0600

    Add frame stacking to ULTRA (#948)
    * Initial changes
    
    * Add initial environment documentation
    
    * Fix ULTRA environment and adapter test to pass tests
    
    * Clean up the ULTRA environment and environment test
    
    * Add note to RLlib training about SMARTS observations adaptation
    
    * Update ULTRA environment documentation

--- ultra/ultra/env/ultra_env.py
+++ ultra/ultra/env/ultra_env.py
@@ -19,22 +19,29 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+from collections import deque
+import copy
 import glob
 import math
 import os
 from itertools import cycle
 from sys import path
+from typing import Dict
 
 import numpy as np
 import yaml, inspect
 from scipy.spatial import distance
 
 from smarts.core.scenario import Scenario
+from smarts.core.sensors import Observation, TopDownRGB
 from smarts.env.hiway_env import HiWayEnv
 import ultra.adapters as adapters
 from ultra.baselines.common.yaml_loader import load_yaml
 
 path.append("./ultra")
+
+
+_STACK_SIZE = 4
 
 
 class UltraEnv(HiWayEnv):
@@ -56,6 +63,8 @@ class UltraEnv(HiWayEnv):
             _scenarios = glob.glob(f"{self.scenarios['train']}")
         else:
             _scenarios = glob.glob(f"{self.scenarios['test']}")
+
+        self.smarts_observations_stack = deque(maxlen=_STACK_SIZE)
 
         super().__init__(
             scenarios=_scenarios,
@@ -88,7 +97,12 @@ class UltraEnv(HiWayEnv):
             for agent_id, action in agent_actions.items()
         }
 
-        observations, rewards, agent_dones, extras = self._smarts.step(agent_actions)
+        smarts_observations, rewards, agent_dones, extras = self._smarts.step(
+            agent_actions
+        )
+
+        self.smarts_observations_stack.append(copy.deepcopy(smarts_observations))
+        observations = self._adapt_smarts_observations(smarts_observations)
 
         infos = {
             agent_id: {"score": value, "env_obs": observations[agent_id]}
@@ -112,6 +126,23 @@ class UltraEnv(HiWayEnv):
 
         return observations, rewards, agent_dones, infos
 
+    def reset(self):
+        scenario = next(self._scenarios_iterator)
+
+        self._dones_registered = 0
+        smarts_observations = self._smarts.reset(scenario)
+
+        for _ in range(_STACK_SIZE):
+            self.smarts_observations_stack.append(copy.deepcopy(smarts_observations))
+        observations = self._adapt_smarts_observations(smarts_observations)
+
+        observations = {
+            agent_id: self._agent_specs[agent_id].observation_adapter(obs)
+            for agent_id, obs in observations.items()
+        }
+
+        return observations
+
     def get_task(self, task_id, task_level):
         base_dir = os.path.join(os.path.dirname(__file__), "../")
         config_path = os.path.join(base_dir, "config.yaml")
@@ -131,3 +162,46 @@ class UltraEnv(HiWayEnv):
             "timestep_sec": self.timestep_sec,
             "headless": self.headless,
         }
+
+    def _adapt_smarts_observations(
+        self, current_observations: Dict[str, Observation]
+    ) -> Dict[str, Observation]:
+        """Adapts the observations received from the SMARTS simulator.
+
+        The ULTRA environment slightly adapts the simulator observations by:
+        - Stacking the TopDownRGB component's data of each observation if the TopDownRGB
+          component of the observation is not None.
+
+        Args:
+            current_observations (Dict[str, Observation]): The current simulator
+                observations.
+
+        Returns:
+            Dict[str, Observation]: The adapted current observations.
+        """
+        for agent_id, current_observation in current_observations.items():
+            if current_observation.top_down_rgb:
+                # This agent's observation contains a TopDownRGB, stack its data.
+                current_top_down_rgb = current_observation.top_down_rgb
+
+                top_down_rgb_data = []
+                for observations in self.smarts_observations_stack:
+                    if agent_id in observations:
+                        top_down_rgb_data.append(
+                            observations[agent_id].top_down_rgb.data
+                        )
+                    else:
+                        # Use the current observation's TopDownRGB data if this agent
+                        # doesn't have previous observations to use to build the stack.
+                        top_down_rgb_data.append(current_top_down_rgb.data)
+                stacked_top_down_rgb_data = np.stack(top_down_rgb_data)
+
+                # Create the new TopDownRGB with stacked data.
+                stacked_top_down_rgb = TopDownRGB(
+                    metadata=current_top_down_rgb.metadata,
+                    data=stacked_top_down_rgb_data,
+                )
+
+                current_observations[agent_id].top_down_rgb = stacked_top_down_rgb
+
+        return current_observations
