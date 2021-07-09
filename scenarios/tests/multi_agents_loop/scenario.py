8f8d4ff0e286206fc6d3bc4c9b7dcfaf7209a2ba
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-07-09 11:28:34 -0400

    Add a minimum framerate test for the env.step()

---
+++ scenarios/tests/multi_agents_loop/scenario.py
@@ -0,0 +1,60 @@
+import random
+from pathlib import Path
+
+from smarts.sstudio import gen_scenario
+from smarts.sstudio import types as t
+
+traffic = t.Traffic(
+    flows=[
+        t.Flow(
+            route=t.RandomRoute(),
+            rate=60 * 60,
+            actors={t.TrafficActor(name="car", vehicle_type=vehicle_type): 1},
+        )
+        for vehicle_type in [
+            "passenger",
+            "bus",
+            "coach",
+            "truck",
+            "trailer",
+        ]
+    ]
+)
+
+open_agent_actor = t.SocialAgentActor(
+    name="open-agent", agent_locator="open_agent:open_agent-v0"
+)
+
+laner_actor = t.SocialAgentActor(
+    name="keep-lane-agent",
+    agent_locator="zoo.policies:keep-lane-agent-v0",
+)
+
+gen_scenario(
+    t.Scenario(
+        traffic={"basic": traffic},
+        social_agent_missions={
+            "group_1": (
+                [laner_actor, open_agent_actor],
+                [t.Mission(route=t.RandomRoute())],
+            ),
+            "group_2": (
+                [laner_actor, open_agent_actor],
+                [t.Mission(route=t.RandomRoute())],
+            ),
+            "group_3": (
+                [laner_actor, open_agent_actor],
+                [t.Mission(route=t.RandomRoute())],
+            ),
+            "group_4": (
+                [laner_actor, open_agent_actor],
+                [t.Mission(route=t.RandomRoute())],
+            ),
+            "group_5": (
+                [laner_actor, open_agent_actor],
+                [t.Mission(route=t.RandomRoute())],
+            ),
+        },
+    ),
+    output_dir=Path(__file__).parent,
+)
---
+++ smarts/core/tests/test_env_frame_rate.py
@@ -0,0 +1,105 @@
+# MIT License
+#
+# Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import gym
+import pytest
+
+import logging
+
+import gym
+from smarts.core.utils.frame_monitor import FrameMonitor
+from smarts.core.agent import Agent, AgentSpec
+from smarts.core.agent_interface import AgentInterface, AgentType
+from smarts.core.sensors import Observation
+from smarts.core.utils.episodes import episodes
+
+logging.basicConfig(level=logging.INFO)
+
+AGENT_ID = "Agent-007"
+
+
+@pytest.fixture
+def scenarios():
+    return ["scenarios/tests/multi_agents_loop"]
+
+
+@pytest.fixture
+def seed():
+    return 42
+
+
+class ChaseViaPointsAgent(Agent):
+    def act(self, obs: Observation):
+        if (
+            len(obs.via_data.near_via_points) < 1
+            or obs.ego_vehicle_state.edge_id != obs.via_data.near_via_points[0].edge_id
+        ):
+            return (obs.waypoint_paths[0][0].speed_limit, 0)
+
+        nearest = obs.via_data.near_via_points[0]
+        if nearest.lane_index == obs.ego_vehicle_state.lane_index:
+            return (nearest.required_speed, 0)
+
+        return (
+            nearest.required_speed,
+            1 if nearest.lane_index > obs.ego_vehicle_state.lane_index else -1,
+        )
+
+
+def env_and_spec(scenarios, seed, headless=True, max_episode_steps=None):
+    agent_spec = AgentSpec(
+        interface=AgentInterface.from_type(
+            AgentType.LanerWithSpeed, max_episode_steps=max_episode_steps
+        ),
+        agent_builder=ChaseViaPointsAgent,
+    )
+
+    env = gym.make(
+        "smarts.env:hiway-v0",
+        scenarios=scenarios,
+        agent_specs={AGENT_ID: agent_spec},
+        sim_name=None,
+        headless=headless,
+        visdom=False,
+        timestep_sec=0.1,
+        sumo_headless=True,
+        seed=seed,
+    )
+    return env, agent_spec
+
+
+def test_env_frame_test(scenarios, seed):
+    env, agent_spec = env_and_spec(scenarios, seed)
+    for episode in episodes(n=10):
+        agent = agent_spec.build_agent()
+        observations = env.reset()
+        episode.record_scenario(env.scenario_log)
+
+        dones = {"__all__": False}
+        while not dones["__all__"]:
+            agent_obs = observations[AGENT_ID]
+            agent_action = agent.act(agent_obs)
+            with FrameMonitor(30):
+                observations, rewards, dones, infos = env.step({AGENT_ID: agent_action})
+            episode.record_step(observations, rewards, dones, infos)
+
+    env.close()
