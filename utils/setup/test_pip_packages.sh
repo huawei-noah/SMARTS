cf39e0c69d602e140e27b520623378d23f909917
christianjans <49890535+christianjans@users.noreply.github.com>
2021-07-30 11:47:38 -0600

    Add Dockerfile that installs ULTRA and all its dependencies (#1028)

--- utils/setup/test_pip_packages.sh
+++ utils/setup/test_pip_packages.sh
@@ -4,11 +4,10 @@ venv_dir=`mktemp -u -d`
 python3.7 -m venv ${venv_dir}
 . ${venv_dir}/bin/activate
 
-# install our requirements
+# Install our requirements
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# install our package on top
-pip install .
-pip install .[train]
+# Install our package on top
+pip install .[test,train,camera-obs]
 pip freeze | grep -v -E '^smarts' | grep -v 'pkg-resources==0.0.0' | diff -u requirements.txt -
