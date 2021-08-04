fc640be84868ce7f9673cfe1953f54f2c5be8059
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-08-04 15:07:12 -0400

    reformatting

--- scenarios/ngsim/scenario.py
+++ scenarios/ngsim/scenario.py
@@ -6,7 +6,11 @@ from smarts.sstudio import gen_scenario
 
 gen_scenario(
     t.Scenario(
-        traffic_histories=["i80_0400-0415.yaml", "i80_0500-0515.yaml", "i80_0515-0530.yaml"],
+        traffic_histories=[
+            "i80_0400-0415.yaml",
+            "i80_0500-0515.yaml",
+            "i80_0515-0530.yaml",
+        ],
     ),
     output_dir=Path(__file__).parent,
 )
