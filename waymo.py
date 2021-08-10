69dcab0e8b8fcbadf89e8e438f12a2868eb7567f
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-08-10 13:56:21 -0400

    Minor changes to improve waymo map conversion

--- waymo.py
+++ waymo.py
@@ -109,7 +109,7 @@ def generate_sumo_map(path, scenario_index):
         edge.set("speed", str(11.0))
         lane = ET.SubElement(edge, "lane")
         lane.set("index", str(0))
-        lane.set("width", str(2.5))
+        lane.set("width", str(4))
         lane.set("shape", shape_str(xs, ys))
 
     scenario_id, map_features = read_map_data(path, scenario_index)
@@ -139,7 +139,6 @@ def generate_sumo_map(path, scenario_index):
             f'--edge-files={edges_path}',
             f'--output-file={net_path}',
             "--offset.disable-normalization"
-            "--geometry.split"
             # '--junctions.join'
         ],
         stdout=subprocess.PIPE
@@ -239,6 +238,7 @@ def animate(path, scenario_index, screenshot=False, outdir=None):
         print(f"Saving {outpath}")
         fig.savefig(outpath, dpi=100)
         plt.close('all')
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
