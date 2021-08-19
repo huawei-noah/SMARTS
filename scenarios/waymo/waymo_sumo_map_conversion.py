75233752acd54707549bad5c80c4193bc0d32143
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-08-19 16:51:20 -0400

    bug fix

--- scenarios/waymo/waymo_sumo_map_conversion.py
+++ scenarios/waymo/waymo_sumo_map_conversion.py
@@ -153,7 +153,6 @@ def generate_sumo_map(path, scenario_id):
             f"--output-file={net_path}",
             "--offset.disable-normalization",
             "--no-internal-links=false",
-            "--junctions.join-same",
         ],
         stdout=subprocess.PIPE,
     )
