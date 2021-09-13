4995c73e3115087b25965cbd61f800954f0dfa62
Saul Field <saul.field@gmail.com>
2021-09-13 11:47:19 -0400

    Changes based on PR feedback

--- examples/dynamic_history_vehicles_replacement.py
+++ examples/dynamic_history_vehicles_replacement.py
@@ -27,22 +27,15 @@ class Trigger:
         self,
         should_trigger: Callable[[Dict[str, Any]], bool],
         on_trigger: Callable[[Dict[str, Any]], None],
-        context: Dict[str, Any] = {},
     ) -> None:
         self._fired = False
         self._should_trigger = should_trigger
         self._on_trigger = on_trigger
-        self._context = context
 
-    def update(self, **kwargs):
-        self._context.update(kwargs)
-        if not self._fired and self._should_trigger(self._context):
-            self._on_trigger(self._context)
+    def update(self, context: Dict[str, Any]):
+        if not self._fired and self._should_trigger(context):
+            self._on_trigger(context)
             self._fired = True
-
-    @property
-    def context(self):
-        return self._context
 
 
 def main(
@@ -80,25 +73,23 @@ def main(
                 interface=AgentInterface(waypoints=True, action=ActionSpaceType.Lane),
                 agent_builder=BasicAgent,
             )
-            if ctx["vehicles_to_replace_randomly"] <= 0:
+
+            # Select a random sample from candidates
+            k = ctx.get("k", 0)
+            if k <= 0:
                 logger.warning(
                     "default (0) or negative value specified for replacement. Replacing all valid vehicle candidates."
                 )
                 sample = ctx["vehicle_candidates"]
             else:
                 logger.info(
-                    "Choosing {} vehicles randomly from {} valid vehicle candidates.".format(
-                        ctx["vehicles_to_replace_randomly"],
-                        len(ctx["vehicle_candidates"]),
-                    )
+                    f"Choosing {k} vehicles randomly from {len(ctx['vehicle_candidates'])} valid vehicle candidates."
                 )
-                sample = random.sample(
-                    ctx["vehicle_candidates"], ctx["vehicles_to_replace_randomly"]
-                )
+                sample = random.sample(ctx["vehicle_candidates"], k)
 
             assert len(sample) != 0
 
-            # Select vehicles and map to agent ids & specs
+            # Map selected vehicles to agent ids & specs
             vehicles_to_trap = {}
             for veh_id in sample:
                 agent_id = f"agent-{veh_id}"
@@ -114,23 +105,19 @@ def main(
             smarts.trap_history_vehicles(vehicles_to_trap, veh_missions)
 
         # Create a table of vehicle trajectory lengths, filtering out non-moving vehicles
-        traj_lens = []
-        for v_id in scenario.traffic_history.all_vehicle_ids():
-            traj = list(scenario.traffic_history.vehicle_trajectory(str(v_id)))
-            if [row for row in traj if row.speed != 0]:
-                traj_lens.append((v_id, len(traj)))
-
-        # Filter out trajectories with less than 100 timesteps
-        vehicle_candidates = [str(t[0]) for t in traj_lens if t[1] > 100]
+        vehicle_candidates = []
+        for v_id in (str(id) for id in scenario.traffic_history.all_vehicle_ids()):
+            traj = list(scenario.traffic_history.vehicle_trajectory(v_id))
+            # Find moving vehicles with more than 100 timesteps
+            if [row for row in traj if row.speed != 0] and len(traj) >= 100:
+                vehicle_candidates.append(v_id)
 
         assert len(vehicle_candidates) > 0
 
         k = vehicles_to_replace_randomly
         if k > len(vehicle_candidates):
             logger.warning(
-                "vehicles_to_replace_randomly={} is greater than the number of vehicle candidates ({}).".format(
-                    vehicles_to_replace_randomly, len(vehicle_candidates)
-                )
+                f"vehicles_to_replace_randomly={k} is greater than the number of vehicle candidates ({len(vehicle_candidates)})."
             )
             k = len(vehicle_candidates)
 
@@ -139,18 +126,21 @@ def main(
             "agents": {},
             "elapsed_sim_time": 0.0,
             "vehicle_candidates": vehicle_candidates,
-            "vehicles_to_replace_randomly": k,
+            "k": k,
         }
-        trigger = Trigger(should_trigger, on_trigger, context=context)
+        trigger = Trigger(should_trigger, on_trigger)
 
         dones = {}
         observations = smarts.reset(scenario)
-        while not dones or not all(done for done in dones.values()):
-            # Step trigger with updated context
-            trigger.update(elapsed_sim_time=smarts.elapsed_sim_time)
+        while not dones or not all(dones.values()):
+            # Update context
+            context["elapsed_sim_time"] = smarts.elapsed_sim_time
+
+            # Step trigger to further update context
+            trigger.update(context)
 
             # Get agents from current context
-            agents = trigger.context["agents"]
+            agents = context["agents"]
 
             # Step simulation
             actions = {
@@ -158,9 +148,7 @@ def main(
                 for agent_id, agent_obs in observations.items()
             }
             logger.debug(
-                "stepping @ sim_time={} for agents={}...".format(
-                    smarts.elapsed_sim_time, list(observations.keys())
-                )
+                "stepping @ sim_time={smarts.elapsed_sim_time} for agents={list(observations.keys())}..."
             )
             observations, rewards, dones, infos = smarts.step(actions)
 
@@ -168,31 +156,21 @@ def main(
                 if dones.get(agent_id, False):
                     if not observations[agent_id].events.reached_goal:
                         logger.warning(
-                            "agent_id={} exited @ sim_time={}".format(
-                                agent_id, smarts.elapsed_sim_time
-                            )
+                            f"agent_id={agent_id} exited @ sim_time={smarts.elapsed_sim_time}"
                         )
-                        logger.warning(
-                            "   ... with {}".format(observations[agent_id].events)
-                        )
+                        logger.warning(f"   ... with {observations[agent_id].events}")
                     else:
                         logger.info(
-                            "agent_id={} reached goal @ sim_time={}".format(
-                                agent_id, smarts.elapsed_sim_time
-                            )
+                            f"agent_id={agent_id} reached goal @ sim_time={smarts.elapsed_sim_time}"
                         )
-                        logger.debug(
-                            "   ... with {}".format(observations[agent_id].events)
-                        )
+                        logger.debug(f"   ... with {observations[agent_id].events}")
                     del observations[agent_id]
 
     smarts.destroy()
 
 
 if __name__ == "__main__":
-    parser = default_argument_parser(
-        "history_vehicles_replacement_for_imitation_learning.py"
-    )
+    parser = default_argument_parser("dynamic_history_vehicles_replacement.py")
     parser.add_argument(
         "--random_replacements-per-episode",
         "-k",
