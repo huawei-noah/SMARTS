548c715f80d74408a614728ce585e5d757401b88
Stephen Hockema <stephen.hockema1@huawei.com>
2021-09-22 02:29:04 -0400

    better logic for None

--- smarts/core/smarts.py
+++ smarts/core/smarts.py
@@ -1006,10 +1006,16 @@ class SMARTS:
         for vehicle_id in self._vehicle_index.agent_vehicle_ids():
             vehicle = self._vehicle_index.vehicle_by_id(vehicle_id)
             map_spot = np.array(vehicle.pose.position)
-            if map_min is None or any(map_spot <= map_min):
+            if map_min is None:
+                map_min = np.array(map_spot)
+                rescale_plane = True
+            elif any(map_spot < map_min):
                 map_min = np.minimum(np.array(map_spot), map_min)
                 rescale_plane = True
-            if map_max is None or any(map_spot >= map_max):
+            if map_max is None:
+                map_max = np.array(map_spot)
+                rescale_plane = True
+            elif any(map_spot > map_max):
                 map_max = np.maximum(np.array(map_spot), map_max)
                 rescale_plane = True
         if rescale_plane:
