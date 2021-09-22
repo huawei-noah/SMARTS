19a23c1bfd558b0611fe7aa06397d62c2e998e3d
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-09-22 11:53:05 -0400

    refactor hijacking code for code usability

--- smarts/core/trap_manager.py
+++ smarts/core/trap_manager.py
@@ -239,26 +239,6 @@ class TrapManager:
         return self._traps
 
     @staticmethod
-    def _hijack_vehicle(sim, vehicle_id, agent_id, mission):
-        agent_interface = sim.agent_manager.agent_interface_for_agent_id(agent_id)
-        plan = Plan(sim.road_map, mission)
-
-        # Apply agent vehicle association.
-        sim.vehicle_index.start_agent_observation(
-            sim, vehicle_id, agent_id, agent_interface, plan
-        )
-        agent_interface = sim.agent_manager.agent_interface_for_agent_id(agent_id)
-        vehicle = sim.vehicle_index.switch_control_to_agent(
-            sim,
-            vehicle_id,
-            agent_id,
-            recreate=True,
-            hijacking=False,
-            agent_interface=agent_interface,
-        )
-        return vehicle
-
-    @staticmethod
     def _make_vehicle(sim, agent_id, mission, initial_speed):
         agent_interface = sim.agent_manager.agent_interface_for_agent_id(agent_id)
         plan = Plan(sim.road_map, mission)
