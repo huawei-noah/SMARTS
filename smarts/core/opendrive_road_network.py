f2c6d5c40d9247c539e64305f44621b657064b02
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-10-26 19:23:38 -0400

    test lane geometry methods

--- smarts/core/opendrive_road_network.py
+++ smarts/core/opendrive_road_network.py
@@ -714,7 +714,8 @@ class OpenDriveRoadNetwork(RoadMap):
             inner_boundary, outer_boundary = self._lane_boundaries[i]
             ds = offset - self._lane_widths[i].start_offset
             t_outer = outer_boundary.calc_t(ds)
-            return abs(t_outer)
+            t_inner = inner_boundary.calc_t(ds)
+            return abs(t_outer -t_inner)
 
     def lane_by_id(self, lane_id: str) -> RoadMap.Lane:
         lane = self._lanes.get(lane_id)
