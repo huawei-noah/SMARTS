43d0127378d65f36f8687c696f0c5b8004bd208d
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-10-26 16:51:45 -0400

    test lane geometry methods

--- smarts/core/tests/test_map.py
+++ smarts/core/tests/test_map.py
@@ -275,7 +275,7 @@ def test_od_map_figure_eight():
 
     point = (13.0, -17.0, 0)
     refline_pt = l1.to_lane_coord(point)
-    assert round(refline_pt.s, 2) == 33.0
+    assert round(refline_pt.s, 2) == 7.21
     assert round(refline_pt.t, 2) == 2.0
 
     offset = refline_pt.s
