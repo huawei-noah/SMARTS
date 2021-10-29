33def58248934cee6f32cfaaf05e06134fdebd2f
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-10-29 12:51:22 -0400

    lane and road shapes test

--- smarts/core/tests/test_map.py
+++ smarts/core/tests/test_map.py
@@ -287,12 +287,17 @@ def test_od_map_junction():
     r5 = road_map.road_by_id("5_0")
     road_left_edge, road_right_edge = r5.edges_at_point(point)
     assert (round(road_left_edge.x, 2), round(road_left_edge.y, 2)) == (115.23, 121.22)
-    assert (round(road_right_edge.x, 2), round(road_right_edge.y, 2)) == (117.02, 117.92)
+    assert (round(road_right_edge.x, 2), round(road_right_edge.y, 2)) == (
+        117.02,
+        117.92,
+    )
 
 
 def test_od_map_figure_eight():
     root = path.join(Path(__file__).parent.absolute(), "maps")
-    road_map = OpenDriveRoadNetwork.from_file(path.join(root, "Figure-Eight_Altered.xodr"))
+    road_map = OpenDriveRoadNetwork.from_file(
+        path.join(root, "Figure-Eight_Altered.xodr")
+    )
     assert isinstance(road_map, OpenDriveRoadNetwork)
 
     # Expected properties for all roads and lanes
