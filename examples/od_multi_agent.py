8722e45ca1ab107f32b548adde8cd633ea13f9df
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2021-12-29 11:19:38 -0500

    add examples using opendrive gym environment

---
+++ examples/od_multi_agent.py
@@ -0,0 +1,74 @@
+import gym
+
+from smarts.core.agent import Agent, AgentSpec
+from smarts.core.agent_interface import AgentInterface, AgentType
+from smarts.core.utils.episodes import episodes
+
+# The following ugliness was made necessary because the `aiohttp` #
+# dependency has an "examples" module too.  (See PR #1120.)
+if __name__ == "__main__":
+    from argument_parser import default_argument_parser
+else:
+    from .argument_parser import default_argument_parser
+
+N_AGENTS = 4
+AGENT_IDS = ["Agent %i" % i for i in range(N_AGENTS)]
+
+
+class KeepLaneAgent(Agent):
+    def act(self, obs):
+        return "keep_lane"
+
+
+def main(scenarios, sim_name, headless, num_episodes, seed, max_episode_steps=None):
+    agent_specs = {
+        agent_id: AgentSpec(
+            interface=AgentInterface.from_type(
+                AgentType.Laner, max_episode_steps=max_episode_steps
+            ),
+            agent_builder=KeepLaneAgent,
+        )
+        for agent_id in AGENT_IDS
+    }
+
+    env = gym.make(
+        "smarts.env:open_drive_set-v0",
+        scenarios=scenarios,
+        agent_specs=agent_specs,
+        sim_name=sim_name,
+        headless=headless,
+        seed=seed,
+    )
+
+    for episode in episodes(n=num_episodes):
+        agents = {
+            agent_id: agent_spec.build_agent()
+            for agent_id, agent_spec in agent_specs.items()
+        }
+        observations = env.reset()
+        episode.record_scenario(env.scenario_log)
+
+        dones = {"__all__": False}
+        while not dones["__all__"]:
+            actions = {
+                agent_id: agents[agent_id].act(agent_obs)
+                for agent_id, agent_obs in observations.items()
+            }
+
+            observations, rewards, dones, infos = env.step(actions)
+            episode.record_step(observations, rewards, dones, infos)
+
+    env.close()
+
+
+if __name__ == "__main__":
+    parser = default_argument_parser("opendrive-multi-agent-example")
+    args = parser.parse_args()
+
+    main(
+        scenarios=args.scenarios,
+        sim_name=args.sim_name,
+        headless=args.headless,
+        num_episodes=args.episodes,
+        seed=args.seed,
+    )
