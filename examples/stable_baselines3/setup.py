7d7a2a62b5293273d2765adbea0858eeb5bfb074
Allan Liu <allandelong.liu@mail.utoronto.ca>
2022-01-06 21:59:29 +0000

    create setup.py

---
+++ examples/stable_baselines3/setup.py
@@ -0,0 +1,24 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+this_dir = path.abspath(path.dirname(__file__))
+with open(path.join(this_dir, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="stable-baselines-3-example",
+    description="Driving in traffic using SB3 PPO",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    python_requires=">=3.7",
+    install_requires=[
+        "setuptools>=41.0.0,!=50.0",
+        "smarts[camera-obs] @ git+https://git@github.com/huawei-noah/SMARTS@develop",
+        "stable-baselines3==1.3.0",
+    ],
+)
