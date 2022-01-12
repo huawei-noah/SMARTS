8718c8e34df12a5ee54eea99f0485fe926227e76
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2022-01-12 16:47:42 -0500

    fix the custom_preprocessor issue arising from training for centralized paradigm

--- baselines/marl_benchmark/marl_benchmark/__init__.py
+++ baselines/marl_benchmark/marl_benchmark/__init__.py
@@ -59,20 +59,19 @@ def gen_config(**kwargs):
                 "groups": {"group": agent_ids},
             }
         )
-        tune_config.update(config["policy"][-1])
-    else:
-        policies = {}
-        for k in agents:
-            policies[k] = config["policy"][:-1] + (
-                {**config["policy"][-1], "agent_id": k},
-            )
-        tune_config.update(
-            {
-                "multiagent": {
-                    "policies": policies,
-                    "policy_mapping_fn": lambda agent_id: agent_id,
-                }
-            }
+        tune_config.update({"model": {config["policy"][-1]}})
+    policies = {}
+    for k in agents:
+        policies[k] = config["policy"][:-1] + (
+            {**config["policy"][-1], "agent_id": k},
         )
+    tune_config.update(
+        {
+            "multiagent": {
+                "policies": policies,
+                "policy_mapping_fn": lambda agent_id: agent_id,
+            }
+        }
+    )
 
     return config
