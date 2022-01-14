036a71d6c6c9749fed473e5b9bb23fdcc11d1abb
Stephen Hockema <stephen.hockema1@huawei.com>
2022-01-13 23:00:14 -0500

    set env for catkin_make

--- smarts/ros/__main__.py
+++ smarts/ros/__main__.py
@@ -23,7 +23,13 @@ if __name__ == "__main__":
                 sys.exit()
             source_ros = f". {ros_distro}/setup.sh && "
         print("setting up ROS node for SMARTS...")
+        # need to override path because this is likely to be being run in a venv,
+        # but catkin_make for ROS v1 distro should be done with python=python2 not python3.
+        default_path = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
         subprocess.check_call(
-            f"{source_ros}catkin_make install", shell=True, cwd=mod_path
+            f"{source_ros}catkin_make install",
+            shell=True,
+            cwd=mod_path,
+            env={"PATH": default_path},
         )
         print(f"\nnow run:  source {mod_path}/install/setup.bash")
