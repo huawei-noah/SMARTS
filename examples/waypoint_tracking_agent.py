ef04971f00a0d95b0dc3dac76c7e0c6625bcddaf
Allan Liu <allandelong.liu@mail.utoronto.ca>
2022-01-19 21:01:01 +0000

    use trajectory tracking controller

--- examples/waypoint_tracking_agent.py
+++ examples/waypoint_tracking_agent.py
@@ -1,9 +1,10 @@
 import logging
+import random
 
 import gym
 
 from smarts.core.agent import Agent, AgentSpec
-from smarts.core.agent_interface import AgentInterface, AgentType
+from smarts.core.agent_interface import AgentInterface, AgentType, DoneCriteria
 from smarts.core.sensors import Observation
 from smarts.core.utils.episodes import episodes
 from smarts.env.wrappers.single_agent import SingleAgent
@@ -20,57 +21,117 @@ logging.basicConfig(level=logging.INFO)
 AGENT_ID = "WaypointTrackingAgent"
 
 
+# class WaypointTrackingAgent(Agent):
+#     def act(self, obs: Observation):
+
+#         lane_index = obs.ego_vehicle_state.lane_index
+#         num_lane_waypoints = []
+#         lateral_error = []
+#         goal_position = obs.ego_vehicle_state.mission.goal.position
+
+#         for idx in range(len(obs.waypoint_paths)):
+#             num_waypoints = len(obs.waypoint_paths[idx])
+#             num_lane_waypoints.append(num_waypoints)
+#             lateral_error.append(
+#                 abs(
+#                     obs.waypoint_paths[idx][num_waypoints - 1].signed_lateral_error(
+#                         goal_position
+#                     )
+#                 )
+#             )
+
+#         # if number of waypoints are the same, choose the lane with the shortest distance to the goal
+#         if max(num_lane_waypoints) == min(num_lane_waypoints):
+#             choose_lane = obs.waypoint_paths[lateral_error.index(min(lateral_error))][
+#                 len(obs.waypoint_paths[idx]) - 1
+#             ].lane_index
+
+#             # print("from agent curr:", lane_index, "from agent target:", choose_lane)
+
+#             if choose_lane == lane_index:
+#                 return "keep_lane"
+#             elif choose_lane < lane_index:
+#                 # od
+#                 return "change_lane_left"
+#                 # sumo
+#                 # return "change_lane_right"
+#             else:
+#                 # od
+#                 return "change_lane_right"
+#                 #sumo
+#                 # return "change_lane_left"
+
+#         else:
+#             choose_lane = num_lane_waypoints.index(max(num_lane_waypoints))
+
+#             if choose_lane == lane_index:
+#                 return "keep_lane"
+
+#             return (
+#                 "change_lane_left" if choose_lane > lane_index else "change_lane_right"
+#             )
+
+
 class WaypointTrackingAgent(Agent):
-    def act(self, obs: Observation):
+    def __init__(self):
+        self.num_waypoint_paths = 0
+        self.waypoint_path_index = 0
+        self.prev_lane_index = 0
 
-        lane_index = obs.ego_vehicle_state.lane_index
-        num_lane_waypoints = []
-        lateral_error = []
-        goal_position = obs.ego_vehicle_state.mission.goal.position
+    def act(self, obs):
 
-        for idx in range(len(obs.waypoint_paths)):
-            num_waypoints = len(obs.waypoint_paths[idx])
-            num_lane_waypoints.append(num_waypoints)
-            lateral_error.append(
-                abs(
-                    obs.waypoint_paths[idx][num_waypoints - 1].signed_lateral_error(
-                        goal_position
-                    )
-                )
-            )
+        current_lane = obs.ego_vehicle_state.lane_index
 
-        # if number of waypoints are the same, choose the lane with the shortest distance to the goal
-        if max(num_lane_waypoints) == min(num_lane_waypoints):
-            choose_lane = obs.waypoint_paths[lateral_error.index(min(lateral_error))][
-                len(obs.waypoint_paths[idx]) - 1
+        if (
+            len(obs.waypoint_paths) != self.num_waypoint_paths
+            or current_lane != self.prev_lane_index
+        ):
+            self.num_waypoint_paths = len(obs.waypoint_paths)
+
+            valid_waypoint_paths = [self.waypoint_path_index]
+
+            for i in range(len(obs.waypoint_paths)):
+                num_wp = len(obs.waypoint_paths[i]) - 1
+                if obs.waypoint_paths[i][num_wp].lane_index != current_lane:
+                    valid_waypoint_paths.append(i)
+
+            self.waypoint_path_index = random.choice(valid_waypoint_paths)
+            self.prev_lane_index = obs.waypoint_paths[self.waypoint_path_index][
+                0
             ].lane_index
 
-            print("from agent curr:", lane_index, "from agent target:", choose_lane)
+        num_trajectory_points = min([len(obs.waypoint_paths[self.waypoint_path_index])])
+        # Desired speed is in m/s
+        desired_speed = 10
 
-            if choose_lane == lane_index:
-                return "keep_lane"
-            elif choose_lane < lane_index:
-                # return "change_lane_left"
-                return "change_lane_right"
-            else:
-                return "change_lane_left"
-                # return "change_lane_right"
-        else:
-            choose_lane = num_lane_waypoints.index(max(num_lane_waypoints))
-
-            if choose_lane == lane_index:
-                return "keep_lane"
-
-            return (
-                "change_lane_left" if choose_lane > lane_index else "change_lane_right"
-            )
+        trajectory = [
+            [
+                obs.waypoint_paths[self.waypoint_path_index][i].pos[0]
+                for i in range(num_trajectory_points)
+            ],
+            [
+                obs.waypoint_paths[self.waypoint_path_index][i].pos[1]
+                for i in range(num_trajectory_points)
+            ],
+            [
+                obs.waypoint_paths[self.waypoint_path_index][i].heading
+                for i in range(num_trajectory_points)
+            ],
+            [desired_speed for i in range(num_trajectory_points)],
+        ]
+        return trajectory
 
 
 def main(scenarios, sim_name, headless, num_episodes, seed, max_episode_steps=None):
     agent_spec = AgentSpec(
         interface=AgentInterface.from_type(
-            AgentType.Laner,
-            max_episode_steps=max_episode_steps
+            AgentType.Tracker,
+            max_episode_steps=200,
+            done_criteria=DoneCriteria(
+                off_route=False,
+                off_road=False,
+                collision=False,
+            ),
             # AgentType.StandardWithAbsoluteSteering, max_episode_steps=max_episode_steps
         ),
         agent_builder=WaypointTrackingAgent,
