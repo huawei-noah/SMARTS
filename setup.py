19e4f0ce29f82b4808e28c48856243c63ae0d67b
adaickalavan <adaickalavan@gmail.com>
2022-02-03 15:02:07 -0500

    Fix opencv version.

--- setup.py
+++ setup.py
@@ -80,6 +80,8 @@ setup(
             # The following are for testing
             "ipykernel>=6.8.0",
             "jupyter-client==6.1.12",
+            "opencv-python==4.1.2.30",
+            "opencv-python-headless==4.1.2.30",
             "pytest>=6.2.5",
             "pytest-benchmark>=3.4.1",
             "pytest-cov>=3.0.0",
