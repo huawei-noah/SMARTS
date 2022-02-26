fe2e9c5a6dcb2c768cf87ac7b25466ed9007c79a
Tucker <montgomery.alban@huawei.com>
2022-02-26 00:59:20 -0500

    Fix some issues with demo

--- smarts/env/wrappers/episode_logger.py
+++ smarts/env/wrappers/episode_logger.py
@@ -7,7 +7,7 @@ Operation = Any
 
 class EpisodeLogger(gym.Wrapper):
     def __init__(self, env: gym.Env):
-        super(env, self).__init__(env)
+        super(EpisodeLogger, self).__init__(env)
         self._first_step = False
 
     def step(self, action: Action) -> Tuple[Operation, float, bool, Dict[str, Any]]:
