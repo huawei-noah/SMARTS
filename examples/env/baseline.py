24de894c9c48c1c56d22fe30f3b17e4bafcdf850
Tucker <montgomery.alban@huawei.com>
2022-02-26 01:59:17 -0500

    Mock demo works

--- examples/env/baseline.py
+++ examples/env/baseline.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 agent_spec = AgentSpec(
     interface=AgentInterface.from_type(
-        AgentType.LanerWithSpeed, max_episode_steps=150, rgb=True
+        AgentType.Laner, max_episode_steps=150, rgb=True
     ),
     agent_builder=None,
 )
@@ -17,12 +17,11 @@ agent_spec = AgentSpec(
 def entry_point(*args, **kwargs):
     from smarts.env.hiway_env import HiWayEnv
 
-    scenario = Path(__file__).parent / "../../scenarios/figure_eight"
+    scenario = (Path(__file__).parent / "../../scenarios/figure_eight").resolve()
     ## Note: can build the scenario here
     from cli.studio import _build_single_scenario
-    print(scenario.resolve())
     _build_single_scenario(
-        clean=True, allow_offset_map=True, scenario=scenario.resolve()
+        clean=True, allow_offset_map=True, scenario=scenario
     )
     return SingleAgent(
         HiWayEnv(
