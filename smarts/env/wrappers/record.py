c4acd3c3ea8060f77f582c733a2751aa6fe41ecd
Tucker <montgomery.alban@huawei.com>
2022-02-25 23:52:05 -0500

    Update mock

---
+++ smarts/env/wrappers/record.py
@@ -0,0 +1,32 @@
+import gym
+from typing import Any, Dict, Tuple
+
+Action = Any
+Operation = Any
+
+
+class RecordVideo(gym.Wrapper):
+    def __init__(self, env: gym.Env, frequency: int):
+        super(env, self).__init__(env)
+        self._frequency = frequency
+
+    def step(self, action: Action) -> Tuple[Operation, float, bool, Dict[str, Any]]:
+        return super().step(action)
+
+    def reset(self) -> Any:
+        return super().reset()
+
+
+class RenderVideo(gym.Wrapper):
+    def __init__(self, env: gym.Env):
+        super(env, self).__init__(env)
+        self._first_step = False
+
+    def step(self, action: Action) -> Tuple[Operation, float, bool, Dict[str, Any]]:
+        return super().step(action)
+
+    def reset(self) -> Any:
+        return super().reset()
+
+    def close(self) -> None:
+        return super().close()
