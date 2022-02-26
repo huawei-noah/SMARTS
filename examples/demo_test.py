c5b3daa910c8b9a0abb06c4a7173a3d25db6425d
Tucker <montgomery.alban@huawei.com>
2022-02-26 02:05:01 -0500

    Run black

--- examples/demo_test.py
+++ examples/demo_test.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.insert(0, "./examples/env")
 sys.path.insert(0, "./zoo")
 import baseline
@@ -11,7 +12,7 @@ from smarts.env.wrappers.episode_logger import EpisodeLogger
 from smarts.env.wrappers.record import RecordVideo, RenderVideo
 from smarts.core.utils.episodes import episode_range
 
-#FormatObs should already be applied
+# FormatObs should already be applied
 env = gym.make("figure_eight-v0")
 # gym.wrappers.Monitor
 env: gym.Env = RecordVideo(env, frequency=10)
