8ae4d9555759c4f93f068b1d2cc4f74b3a6d5090
Tucker <montgomery.alban@huawei.com>
2022-02-26 23:41:36 -0500

    Get print utility working

--- smarts/env/wrappers/episode_logger.py
+++ smarts/env/wrappers/episode_logger.py
@@ -19,18 +19,40 @@
 # THE SOFTWARE.
 import gym
 from typing import Any, Dict, Tuple
+from smarts.core.utils.episodes import EpisodeLog, EpisodeLogs
 
 Action = Any
 Operation = Any
 
 
 class EpisodeLogger(gym.Wrapper):
-    def __init__(self, env: gym.Env):
+    def __init__(self, env: gym.Env, col_width: int = 18):
         super(EpisodeLogger, self).__init__(env)
         self._first_step = False
+        self._current_episode = None
+        self._done = False
+        self._log_iter = self._episode_logs(col_width)
 
     def step(self, action: Action) -> Tuple[Operation, float, bool, Dict[str, Any]]:
-        return super().step(action)
+        step_vals = super().step(action)
+        self._current_episode.record_step(*step_vals)
+        return step_vals
 
     def reset(self) -> Any:
-        return super().reset()
+        obs = super().reset()
+        self._current_episode: EpisodeLog = next(self._log_iter)
+        self._current_episode.record_scenario(self.scenario_log)
+        return obs
+
+    def close(self):
+        self._done = True
+        try:
+            next(self._log_iter)
+        except:
+            pass
+        return super().close()
+
+    def _episode_logs(self, col_width) -> EpisodeLog:
+        with EpisodeLogs(col_width) as episode_logs:
+            while not self._done:
+                yield episode_logs.reset()
