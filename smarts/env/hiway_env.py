7503f74d4db2eff4370f8ce0833e3ca4fc9b95b1
Allan Liu <44624435+liu-allan@users.noreply.github.com>
2022-03-02 17:23:59 -0500

    add assert to verify the agent actions are the right type (#1311)
    * add assert to verify the agent actions are the right type
    
    * remove unused import
    
    * remove unnecessary whitespace

--- smarts/env/hiway_env.py
+++ smarts/env/hiway_env.py
@@ -231,6 +231,9 @@ class HiWayEnv(gym.Env):
             agent_id: self._agent_specs[agent_id].action_adapter(action)
             for agent_id, action in agent_actions.items()
         }
+
+        assert isinstance(agent_actions, dict) and all(isinstance(key, str) for key in agent_actions.keys()), 'Expected Dict[str, any]'        
+
         observations, rewards, dones, extras = None, None, None, None
         with timeit("SMARTS Simulation/Scenario Step", self._log):
             observations, rewards, dones, extras = self._smarts.step(agent_actions)
