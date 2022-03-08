bf47e1f45f08bafbb45d3bd9f104d550378afc82
Qianyi Sun <qianyi-sun@users.noreply.github.com>
2022-03-08 20:32:22 +0000

    GitHub Actions: Format

--- scenarios/NGSIM/i80/scenario.py
+++ scenarios/NGSIM/i80/scenario.py
@@ -1,12 +1,15 @@
 from pathlib import Path
- 
+
+from smarts.sstudio import gen_scenario
 from smarts.sstudio import types as t
-from smarts.sstudio import gen_scenario                                                                                                                         
- 
- 
+
 gen_scenario(
     t.Scenario(
-        traffic_histories=["i80_0400-0415.yaml", "i80_0500-0515.yaml", "i80_0515-0530.yaml"],
+        traffic_histories=[
+            "i80_0400-0415.yaml",
+            "i80_0500-0515.yaml",
+            "i80_0515-0530.yaml",
+        ],
     ),
     output_dir=Path(__file__).parent,
 )
