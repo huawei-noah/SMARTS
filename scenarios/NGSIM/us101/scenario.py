06b552e6edff7e2fb149a0be2729db64fe6915f5
qianyi-sun <sqy201x@hotmail.com>
2022-03-08 16:59:11 -0500

    add us101 sumo map support

---
+++ scenarios/NGSIM/us101/scenario.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+ 
+from smarts.sstudio import types as t
+from smarts.sstudio import gen_scenario                                                                                                                         
+ 
+ 
+gen_scenario(
+    t.Scenario(
+        traffic_histories=["us101_0750-0805.yml", "us101_0805-0820.yml", "us101_0820-0835.yml"],
+    ),
+    output_dir=Path(__file__).parent,
+)
