4d5cc4c3a85a91b291022637f4f0acdcacf2464e
Rutvik Gupta <RutvikGupta@users.noreply.github.com>
2022-03-15 17:49:37 -0400

    start of a text based browser

--- scenarios/waymo_motion/waymo_utility.py
+++ scenarios/waymo_motion/waymo_utility.py
@@ -32,7 +32,6 @@ import matplotlib.pyplot as plt
 from waymo_open_dataset.protos import scenario_pb2
 
 from smarts.core.utils.file import read_tfrecord_file
-from cli.studio import _build_single_scenario
 
 
 def convert_polyline(polyline) -> Tuple[List[float], List[float]]:
@@ -162,100 +161,6 @@ def plot(path: str, scenario_id: str):
     plt.show()
 
 
-def get_scenario_dict(tfrecord_file: str) -> List[str]:
-    scenario_dict = {}
-    dataset = read_tfrecord_file(tfrecord_file)
-    for record in dataset:
-        scenario = scenario_pb2.Scenario()
-        scenario.ParseFromString(bytearray(record))
-
-        scenario_id = scenario.scenario_id
-        scenario_dict[scenario_id] = scenario
-
-    return scenario_dict
-
-
-def parse_tfrecords(tfrecord_path: str):
-    scenarios_per_tfrecord = {}
-    if os.path.isdir(tfrecord_path):
-        for f in os.listdir(tfrecord_path):
-            if ".tfrecord" in f and os.path.isfile(os.path.join(tfrecord_path, f)):
-                scenario_dict = get_scenario_dict(os.path.join(tfrecord_path, f))
-                scenarios_per_tfrecord[f] = scenario_dict
-    else:
-        scenarios_per_tfrecord[tfrecord_path] = get_scenario_dict(tfrecord_path)
-    return scenarios_per_tfrecord
-
-
-def display_scenario_info(scenarios_per_tfrecord):
-    for tfrecord_path in scenarios_per_tfrecord:
-        scenario_data_lst = []
-        for scenario_id in scenarios_per_tfrecord[tfrecord_path]:
-            scenario = scenarios_per_tfrecord[tfrecord_path][scenario_id]
-            scenario_data = [
-                scenario_id,
-                len(scenario.timestamps_seconds),
-                len(scenario.tracks),
-                len(scenario.dynamic_map_states),
-                len(scenario.objects_of_interest),
-            ]
-            scenario_data_lst.append(scenario_data)
-        print("                                               ")
-        print("-----------------------------------------------")
-        print(
-            f"{len(scenarios_per_tfrecord[tfrecord_path])} scenarios in {tfrecord_path}:"
-        )
-        print("                                               ")
-        print(
-            tabulate(
-                scenario_data_lst,
-                headers=[
-                    "Scenario ID",
-                    "Timestamps",
-                    "Track Objects",
-                    "Traffic Lights",
-                    "Object of Interest",
-                ],
-            )
-        )
-
-
-def export_scenario(
-    target_base_path: str, tfrecord_file_path: str, scenario_id, overwrite=False
-):
-    subfolder_path = os.path.join(target_base_path, scenario_id)
-    try:
-        os.makedirs(subfolder_path)
-    except FileExistsError:
-        print(f"Folder already exists at path {subfolder_path}")
-    scenario_py = os.path.join(subfolder_path, "scenario.py")
-    if os.path.exists(scenario_py):
-        print(f"scenario.py already exists in {subfolder_path}.")
-    else:
-        scenario_template = os.path.join(
-            Path(__file__).parent, "templates", "scenario_template.py"
-        )
-        shutil.copy2(scenario_template, scenario_py)
-        print(f"Scenario.py created in {subfolder_path}.")
-
-    yaml_dataspec = {
-        "trajectory_dataset": {
-            "source": "Waymo",
-            "input_path": tfrecord_file_path,
-            "scenario_id": scenario_id,
-        }
-    }
-    with open(os.path.join(subfolder_path, "waymo.yaml"), "w") as yaml_file:
-        yaml.dump(yaml_dataspec, yaml_file, default_flow_style=False)
-
-
-def generate_scenario(dataset_path: str, scenario_id: str):
-    scenario_root = Path(__file__).parent
-    yaml_path = os.path.join(scenario_root, "waymo.yaml")
-    edit_scenario_yaml(yaml_path, dataset_path, scenario_id)
-    _build_single_scenario(True, False, str(scenario_root))
-
-
 def dump_plots(out_dir: str, path: str) -> List[str]:
     scenarios = []
     dataset = read_tfrecord_file(path)
@@ -289,6 +194,158 @@ def dump_plots(out_dir: str, path: str) -> List[str]:
     return scenarios
 
 
+def get_scenario_dict(tfrecord_file: str) -> List[str]:
+    scenario_dict = {}
+    dataset = read_tfrecord_file(tfrecord_file)
+    for record in dataset:
+        scenario = scenario_pb2.Scenario()
+        scenario.ParseFromString(bytearray(record))
+        scenario_id = scenario.scenario_id
+        scenario_dict[scenario_id] = scenario
+
+    return scenario_dict
+
+
+def parse_tfrecords(tfrecord_path: str):
+    scenarios_per_tfrecord = {}
+    if os.path.isdir(tfrecord_path):
+        for f in os.listdir(tfrecord_path):
+            if ".tfrecord" in f and os.path.isfile(os.path.join(tfrecord_path, f)):
+                scenario_dict = get_scenario_dict(os.path.join(tfrecord_path, f))
+                scenarios_per_tfrecord[f] = scenario_dict
+    else:
+        scenarios_per_tfrecord[tfrecord_path] = get_scenario_dict(tfrecord_path)
+    return scenarios_per_tfrecord
+
+
+def display_scenarios_in_tfrecord(tfrecord_path, scenario_dict) -> List[str]:
+    scenario_data_lst = []
+    scenario_counter = 1
+    scenario_ids = []
+    for scenario_id in scenario_dict:
+        scenario = scenario_dict[scenario_id]
+        scenario_data = [
+            scenario_counter,
+            scenario_id,
+            len(scenario.timestamps_seconds),
+            len(scenario.tracks),
+            len(scenario.dynamic_map_states),
+            len(scenario.objects_of_interest),
+        ]
+        scenario_ids.append(scenario_id)
+        scenario_data_lst.append(scenario_data)
+        scenario_counter += 1
+    print("                                               ")
+    print("-----------------------------------------------")
+    print(
+        f"{len(scenario_dict)} scenarios in {tfrecord_path}:\n"
+    )
+    print(
+        tabulate(
+            scenario_data_lst,
+            headers=[
+                "Index",
+                "Scenario ID",
+                "Timestamps",
+                "Track Objects",
+                "Traffic Lights",
+                "Object of Interest",
+            ],
+        )
+    )
+    return scenario_ids
+
+
+def export_scenario(
+    target_base_path: str, tfrecord_file_path: str, scenario_id
+):
+    subfolder_path = os.path.join(target_base_path, scenario_id)
+    try:
+        os.makedirs(subfolder_path)
+    except FileExistsError:
+        print(f"Folder already exists at path {subfolder_path}")
+    scenario_py = os.path.join(subfolder_path, "scenario.py")
+    if os.path.exists(scenario_py):
+        print(f"scenario.py already exists in {subfolder_path}.")
+    else:
+        scenario_template = os.path.join(
+            Path(__file__).parent, "templates", "scenario_template.py"
+        )
+        shutil.copy2(scenario_template, scenario_py)
+        print(f"Scenario.py created in {subfolder_path}.")
+
+    yaml_dataspec = {
+        "trajectory_dataset": {
+            "source": "Waymo",
+            "input_path": tfrecord_file_path,
+            "scenario_id": scenario_id,
+        }
+    }
+    with open(os.path.join(subfolder_path, "waymo.yaml"), "w") as yaml_file:
+        yaml.dump(yaml_dataspec, yaml_file, default_flow_style=False)
+
+
+def tfrecords_browser(tfrecord_path: str):
+    scenarios_per_tfrecords = parse_tfrecords(tfrecord_path)
+    tf_records = []
+    tf_counter = 1
+    for tf in scenarios_per_tfrecords:
+        tf_records.append([tf_counter, tf])
+        tf_counter += 1
+    stop_browser = False
+    while not stop_browser:
+        print("-----------------------------------------------")
+        print("Waymo tfRecords:\n")
+        print(tabulate(tf_records, headers=["Index", "TfRecords"],))
+        print("\n")
+        print("You can use the following commands to further explore these datasets:\n"
+              "1. `display all` --> Displays the info of all the scenarios from every tfRecord file together\n"
+              f"2. `explore <index>` --> Explore the tfRecord file at this index of the table. The index should be an integer between 1 and {tf_counter}\n"
+              "3. `exit` --> Exit the program\n")
+
+        raw_input = input("Command: ").lower()
+        user_input = raw_input.strip()
+        if user_input == "display all":
+            for tf_record in tf_records:
+                display_scenarios_in_tfrecord(tf_record, scenarios_per_tfrecords[tf_record])
+        elif user_input == "exit":
+            stop_browser = True
+            print("Exiting Browser")
+        elif "explore" in user_input:
+            input_lst = user_input.split()
+            if len(input_lst) != 2:
+                print("Please enter only one number as an index for the `explore` command")
+                continue
+            try:
+                idx = int(input_lst[1])
+                if not (1 <= idx <= len(tf_records)):
+                    print(f"Please enter an index between 1 and {tf_counter}.")
+                    continue
+                tf_path = tf_records[idx][1]
+                stop_browser = explore_tf_record(tf_path, scenarios_per_tfrecords[tf_path])
+            except Exception:
+                print("Please input an integer for the `explore` command")
+                continue
+        else:
+            print("Please enter a valid command. See command formats above")
+
+
+def explore_tf_record(tfrecord: str, scenario_dict):
+    scenario_ids = display_scenarios_in_tfrecord(tfrecord, scenario_dict)
+    print("\n")
+    print("You can use the following commands to further explore these scenarios:\n"
+          "1. `export all <target_base_path>` --> Export all scenarios in this tf_record to a target path. Path should be valid.\n"
+          f"2. `export <index> <target_base_path>' --> Export the scenario at this index of the table to a target path. The index should be an integer between 1 and {len(scenario_ids)} and path should be valid.\n"
+          "3. `preview all <target_base_path>` --> Plot and dump the images of the map of all scenarios in this tf_record to a target path. Path should be valid.\n"
+          f"4. `preview <index>` --> Plot and display the map of the scenario at this index of the table. The index should be an integer between 1 and {len(scenario_ids)}\n"
+          f"5. `select <index>` --> Select and explore further the scenario at this index of the table. The index should be an integer between 1 and {len(scenario_ids)}\n"
+          "6. `go back` --> Go back to the tfrecords browser\n"
+          "7. `exit` --> Exit the program\n")
+
+    raw_input = input("Command: ").lower()
+    return False
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="waymo_scenario_gen.py",
@@ -297,5 +354,5 @@ if __name__ == "__main__":
     parser.add_argument("file", help="TFRecord file/folder path")
     args = parser.parse_args()
 
-    display_scenario_info(parse_tfrecords(args.file))
+    # display_scenario_info(parse_tfrecords(args.file))
     export_scenario("scenarios/waymo_motion", args.file, "4f30f060069bbeb9")
