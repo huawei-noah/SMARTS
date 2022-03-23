4f794ffa20891203286b8980417bc440d5221272
Tucker <montgomery.alban@huawei.com>
2022-03-23 16:28:41 -0400

    Update docstrings in episode logging wrapper.

--- smarts/env/wrappers/episode_logger.py
+++ smarts/env/wrappers/episode_logger.py
@@ -26,25 +26,32 @@ Operation = Any
 
 
 class EpisodeLogger(gym.Wrapper):
+    """Wraps a gym environment with simple episode logging capabilities."""
+
     def __init__(self, env: gym.Env, col_width: int = 18):
         super(EpisodeLogger, self).__init__(env)
-        self._first_step = False
         self._current_episode = None
         self._done = False
         self._log_iter = self._episode_logs(col_width)
 
     def step(self, action: Action) -> Tuple[Operation, float, bool, Dict[str, Any]]:
+        """Mark a step for logging."""
+        
         step_vals = super().step(action)
         self._current_episode.record_step(*step_vals)
         return step_vals
 
     def reset(self) -> Any:
+        """Mark an episode reset for logging."""
+
         obs = super().reset()
         self._current_episode: EpisodeLog = next(self._log_iter)
         self._current_episode.record_scenario(self.scenario_log)
         return obs
 
     def close(self):
+        """Cap off the episode logging."""
+
         self._done = True
         try:
             next(self._log_iter)
