3cb5138625615bfb4840dd705e0dfd4013c9a907
qianyi-sun <sqy201x@hotmail.com>
2022-03-29 11:36:22 -0400

    add peach and update us101 map

---
+++ scenarios/NGSIM/peachtree/scenario.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+ 
+from smarts.sstudio import types as t
+from smarts.sstudio import gen_scenario                                                                                                                         
+ 
+ 
+gen_scenario(
+    t.Scenario(
+        traffic_histories=["peach_0400-0415.yml", "peach_1245-0100.yml"],
+    ),
+    output_dir=Path(__file__).parent,
+)
