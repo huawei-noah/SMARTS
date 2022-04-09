8f023fa5b8afbcf8aeab00111aa1661128f2e3bc
Tucker <mtuck2005@yahoo.ca>
2022-04-08 23:25:52 -0400

    Test data format

---
+++ envision/tests/test_serialization.py
@@ -0,0 +1,219 @@
+# MIT License
+# 
+# Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+from typing import Sequence
+import pytest
+
+from envision.serialization import _serialization_map, Context, EnvisionDataFormatter
+from envision.types import State, TrafficActorState, TrafficActorType, VehicleType
+from smarts.core.events import Events
+
+
+@pytest.fixture
+def covered_data():
+    return [
+        (VehicleType.Bus, [0]),
+        (VehicleType.Car, [4]),
+        (TrafficActorType.SocialVehicle, [0]),
+        (TrafficActorType.Agent, [2]),
+        (
+            TrafficActorState(
+                actor_type=TrafficActorType.Agent,
+                vehicle_type=VehicleType.Bus,
+                position=(4, 5, 2),
+                heading=-3.141571,
+                speed=20,
+                name="",
+                actor_id="agent_007",
+                lane_id="NE-NW",
+                score=10,
+                events=Events(
+                    [], False, False, False, False, False, False, False, True
+                ),
+            ),
+            [
+                0,  # actor id lookup into values
+                1,  # lane id lookup into values
+                4,  # x
+                5,  # y
+                2,  # z
+                -3.141571,  # heading
+                20,  # speed
+                (
+                    [],
+                    False,
+                    False,
+                    False,
+                    False,
+                    False,
+                    False,
+                    False,
+                    True,
+                ),  # events
+                10,  # score
+                [],  # waypoints
+                [],  # driven path
+                [],  # point cloud positions
+                [],  # mission route geometry
+                2,
+                0,
+                {0: "agent_007", 1: "NE-NW"},  # added values
+                [],  # removed values
+            ],
+        ),
+    ]
+
+
+@pytest.fixture
+def primitive_data():
+    return [(1, [1]), (0.2, [0.2]), ("la", ["la"]), ([], [[]]), ((3, 5), [(3, 5)])]
+
+
+@pytest.fixture
+def complex_data():
+    return [
+        (
+            State(
+                traffic=[
+                    TrafficActorState(
+                        actor_type=TrafficActorType.Agent,
+                        vehicle_type=VehicleType.Bus,
+                        position=(4, 5, 2),
+                        heading=-3.141571,
+                        speed=20,
+                        name="",
+                        actor_id="agent_007",
+                        lane_id="NE-NW",
+                        score=10,
+                        events=Events(
+                            [], False, False, False, False, False, False, False, True
+                        ),
+                    ),
+                ]
+                * 2,
+                scenario_id="scene_id",
+                scenario_name="big blue",
+                bubbles=[],
+                scene_colors=[],
+                scores=[],
+                ego_agent_ids=[],
+                position=[],
+                speed=[],
+                heading=[],
+                lane_ids=[],
+                frame_time=0.1,
+            ),
+            [
+                0.1,
+                "scene_id",
+                "big blue",
+                [
+                    [
+                        0,
+                        1,
+                        4,
+                        5,
+                        2,
+                        -3.141571,
+                        20,
+                        ([], False, False, False, False, False, False, False, True),
+                        10,
+                        [],
+                        [],
+                        [],
+                        [],
+                        2,
+                        0,
+                    ],
+                    [
+                        0,
+                        1,
+                        4,
+                        5,
+                        2,
+                        -3.141571,
+                        20,
+                        ([], False, False, False, False, False, False, False, True),
+                        10,
+                        [],
+                        [],
+                        [],
+                        [],
+                        2,
+                        0,
+                    ],
+                ],
+                [],
+                [],
+                {0: "agent_007", 1: "NE-NW"},
+                [],
+            ],
+        ),
+    ]
+
+
+def test_covered_data_format(covered_data):
+    for item in covered_data:
+        es: EnvisionDataFormatter = EnvisionDataFormatter(None)
+        vt = item[0]
+        _serialization_map[type(vt)](vt, es)
+
+        data = es.resolve()
+
+        assert data == item[1]
+
+
+def test_primitive_data_format(primitive_data):
+    for item in primitive_data:
+        vt = item[0]
+        es = EnvisionDataFormatter(None)
+        es.add_any(vt)
+
+        data = es.resolve()
+
+        assert data == item[1]
+
+
+def test_layer():
+    expected_output = [2, 5, 6, [2, 5, 6], [8, 8], ["Time", ["for", "tea", 12, "noon"]]]
+    es = EnvisionDataFormatter(None)
+    es.add([2, 5, 6], "", op=Context.FLATTEN)
+    es.add([2, 5, 6], "")
+    with es.layer():
+        es.add([8, 8], "", op=Context.FLATTEN)
+
+    with es.layer():
+        es.add("Time", "")
+        with es.layer():
+            es.add(["for", "tea", 12, "noon"], "", op=Context.FLATTEN)
+
+    assert es.resolve() == expected_output
+
+
+def test_complex_data(complex_data):
+    for item in complex_data:
+        vt = item[0]
+        es = EnvisionDataFormatter(None)
+        es.add_any(vt)
+
+        data = es.resolve()
+
+        assert data == item[1]
