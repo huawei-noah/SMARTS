56045cf3ffb8ff0864b33c41b609f1a623c29025
Tucker <mtuck2005@yahoo.ca>
2022-04-10 22:14:07 -0400

    Create ego-centric observation adapter

---
+++ smarts/core/utils/observation_adapter.py
@@ -0,0 +1,133 @@
+# MIT License
+#
+# Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+from collections import namedtuple
+from dataclasses import dc_replace, is_dataclass
+from typing import Any, Callable
+from smarts.core.coordinates import Heading
+from smarts.core.sensors import Observation, ViaPoint
+from smarts.core.utils.math import position_to_ego_frame
+import numpy as np
+
+
+def _isnamedtupleinstance(x):
+    t = type(x)
+    b = t.__bases__
+    if len(b) != 1 or b[0] != tuple:
+        return False
+    f = getattr(t, "_fields", None)
+    if not isinstance(f, tuple):
+        return False
+    return all(type(n) == str for n in f)
+
+
+def _replace(obj, **kwargs):
+    if is_dataclass(obj):
+        return dc_replace(obj, **kwargs)
+    elif _isnamedtupleinstance(obj):
+        return obj._replace(obj, **kwargs)
+
+    raise ValueError("Must be a namedtuple or dataclass.")
+
+
+class EgoCentricObservationAdapter(Callable[[Observation], Observation]):
+    def __init__(self, ego_agent_id) -> None:
+        super().__init__()
+        self.ego_agent_id = ego_agent_id  # TODO: is this needed?
+
+    def __call__(self, obs: Observation, *args: Any, **kwds: Any) -> Any:
+        position = obs.ego_vehicle_state.position
+        heading = obs.ego_vehicle_state.heading
+
+        def ego_frame_dynamics(v):
+            return np.array([np.linalg.norm(v[:2]), 0, *v[2:]])
+
+        def transform(v):
+            return position_to_ego_frame(v, position, heading)
+
+        def adjust_heading(h):
+            return h - heading
+
+        nvs = obs.neighborhood_vehicle_states or []
+        wpps = obs.waypoint_paths or []
+
+        vd = None
+        if obs.via_data:
+            rpvp = lambda vps: [
+                _replace(vp, position=transform(vp.position)) for vp in vps
+            ]
+            vd = _replace(
+                obs.via_data,
+                near_via_points=rpvp(obs.via_data.near_via_points),
+                hit_via_points=rpvp(obs.via_data.hit_via_points),
+            )
+
+        replace_wps = lambda lwps: [
+            [
+                _replace(wp, pos=transform(wp.pos), heading=adjust_heading(wp.heading))
+                for wp in wps
+            ]
+            for wps in lwps
+        ]
+        rwps = None
+        if obs.road_waypoints:
+            rwps = _replace(
+                obs.road_waypoints,
+                lanes={
+                    l_id: replace_wps(wps) for l_id, wps in obs.road_waypoints.lanes
+                },
+            )
+
+        replace_metadata = lambda cam_obs: _replace(
+            cam_obs,
+            metadata=_replace(
+                cam_obs.metadata, camera_pos=(0, 0, 0), camera_heading_in_degrees=0
+            ),
+        )
+        return _replace(
+            obs,
+            ego_vehicle_state=_replace(
+                obs.ego_vehicle_state,
+                position=np.array([0, 0, 0]),
+                steering=Heading(0),
+                linear_velocity=ego_frame_dynamics(
+                    obs.ego_vehicle_state.linear_velocity
+                ),
+                linear_acceleration=ego_frame_dynamics(
+                    obs.ego_vehicle_state.linear_acceleration
+                ),
+                linear_jerk=ego_frame_dynamics(obs.ego_vehicle_state.linear_jerk),
+            ),
+            neighborhood_vehicle_state=[
+                _replace(
+                    nv,
+                    position=transform(nv.position),
+                    heading=adjust_heading(nv.heading),
+                )
+                for nv in nvs
+            ],
+            waypoint_paths=replace_wps(wpps),
+            drivable_area_grid_map=replace_metadata(obs.drivable_area_grid_map),
+            occupancy_grid_map=replace_metadata(obs.occupancy_grid_map),
+            top_down_rgb=replace_metadata(obs.top_down_rgb),
+            road_wapoints=rwps,
+            via_data=vd,
+        )
