00eee8db2684609fe83621a604fa80d2df6d1bea
Tucker <mtuck2005@yahoo.ca>
2022-04-11 16:44:30 -0400

    Generate headers.

--- smarts/core/utils/adapters/tests/test_observation_adapter.py
+++ smarts/core/utils/adapters/tests/test_observation_adapter.py
@@ -1,3 +1,24 @@
+# MIT License
+# 
+# Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 from typing import Iterator
 
 import pytest
@@ -10,7 +31,9 @@ from smarts.core.scenario import Scenario
 from smarts.core.smarts import SMARTS
 from smarts.core.sumo_traffic_simulation import SumoTrafficSimulation
 from smarts.core.tests.helpers.scenario import temp_scenario
-from smarts.core.utils.adapters.ego_centric_observation_adapter import ego_centric_observation_adapter
+from smarts.core.utils.adapters.ego_centric_observation_adapter import (
+    ego_centric_observation_adapter,
+)
 from smarts.sstudio.genscenario import gen_scenario
 from smarts.zoo.agent_spec import AgentSpec
 
@@ -88,7 +111,9 @@ def smarts(agent_spec: AgentSpec):
     smarts.destroy()
 
 
-def test_ego_centric_observation_adapter(smarts: SMARTS, scenarios: Iterator[Scenario], agent_spec: AgentSpec):
+def test_ego_centric_observation_adapter(
+    smarts: SMARTS, scenarios: Iterator[Scenario], agent_spec: AgentSpec
+):
     scenario = next(scenarios)
     obs = smarts.reset(scenario)
 
