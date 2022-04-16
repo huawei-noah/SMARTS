7ff9f0aa1f796919e72664924521ab73ab464986
Tucker Alban <montgomery.alban@huawei.com>
2022-04-15 20:48:43 -0400

    Release 0 6 1 rc0 (#1385)
    Co-authored-by: qianyi-sun <sqy201x@hotmail.com>
    Co-authored-by: Allan Liu <allandelong.liu@mail.utoronto.ca>
    Co-authored-by: adai <adaickalavan@gmail.com>
    Co-authored-by: Saul Field <saul.field@gmail.com>
    Co-authored-by: Zhengbang Zhu <zbzhu.yz@gmail.com>

--- smarts/core/utils/adapters/__init__.py
+++ smarts/core/utils/adapters/__init__.py
@@ -1,4 +1,6 @@
-# Copyright (C) 2020. Huawei Technologies Co., Ltd. All rights reserved.
+# MIT License
+#
+# Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -17,40 +19,3 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-import click
-
-from envision.server import run
-
-
-@click.group(
-    name="envision",
-    help="Commands to utilize an Envision server. The Envision web server is used for visualization purposes. See `scl envision COMMAND --help` for further options.",
-)
-def envision_cli():
-    pass
-
-
-@envision_cli.command(name="start", help="Start an Envision server.")
-@click.option("-p", "--port", help="Port Envision will run on.", default=8081)
-@click.option(
-    "-s",
-    "--scenarios",
-    help="A list of directories where scenarios are stored.",
-    multiple=True,
-    default=["scenarios"],
-)
-@click.option(
-    "-c",
-    "--max_capacity",
-    help=(
-        "Max capacity in MB of Envision's playback buffer. The larger the more contiguous history "
-        "Envision can store."
-    ),
-    default=500,
-    type=float,
-)
-def start_server(port, scenarios, max_capacity):
-    run(scenario_dirs=scenarios, max_capacity_mb=max_capacity, port=port)
-
-
-envision_cli.add_command(start_server)
