34626cab1582b885a834b948e08374e42b236ff4
qianyi-sun <sqy201x@hotmail.com>
2022-05-05 15:18:26 -0400

    randomize the starting points

--- scenarios/cut_off/scenario.py
+++ scenarios/cut_off/scenario.py
@@ -8,21 +8,21 @@ traffic = t.Traffic(
     flows=[
         t.Flow(
             route=t.Route(
-                begin=("E1", lane_idx, "random"),
+                begin=("E1", random.randint(0,2), "random"),
                 end=("E2", random.randint(0,2), "max"),
             ),
             rate=3,
             actors={t.TrafficActor("car",speed=Distribution(mean=0.5, sigma=1)): 1},
         )
-        for lane_idx in range(3)
+        for _ in range(3)
     for _ in range(4)
     ]
 )
-ego_missions = [t.Mission(t.Route(begin=("E1",2,1),end=("E2",2,'max')))]
+ego_mission = [t.Mission(t.Route(begin=("E1",2,1),end=("E2",2,'max')))]
 
 scenario = t.Scenario(
     traffic={"all": traffic},
-    ego_missions=ego_missions,
+    ego_missions=ego_mission,
 )
 
 gen_scenario(scenario, output_dir=str(Path(__file__).parent))
