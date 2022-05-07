20128a93219f145e6f9b85d65182f92fec0c225b
adaickalavan <adaickalavan@gmail.com>
2022-05-06 21:51:13 -0400

    Remove unnecessary codes.

--- examples/rl/intersection/intersection/observation.py
+++ examples/rl/intersection/intersection/observation.py
@@ -1,12 +1,9 @@
 import gym
 import numpy as np
-from intersection import util as intersection_util
 from typing import Dict
 
-from smarts.env.wrappers import format_obs
 
-
-class ObsCNN(gym.Wrapper):
+class Observation(gym.Wrapper):
     def __init__(self, env: gym.Env):
         super().__init__(env)
         old_space = env.observation_space["rgb"]
@@ -28,7 +25,7 @@ class ObsCNN(gym.Wrapper):
                 Observation, reward, done, info, of the agent.
         """
         obs, rewards, dones, infos = self.env.step(action)
-        filtered = filter_obs_cnn(obs)
+        filtered = filter_obs(obs)
         return filtered, rewards, dones, infos
 
     def reset(self):
@@ -38,100 +35,14 @@ class ObsCNN(gym.Wrapper):
             np.ndarray: Agent's observation after reset.
         """
         obs = self.env.reset()
-        filtered = filter_obs_cnn(obs)
+        filtered = filter_obs(obs)
         return filtered
 
 
-def filter_obs_cnn(obs: Dict[str, gym.Space]) -> np.ndarray:
+def filter_obs(obs: Dict[str, gym.Space]) -> np.ndarray:
     rgb = obs["rgb"]
-
-    # Ego vehicle is 1.5mx3.75m
-    # Road width = 6.25m
-
-    # Repaint ego
-    # clr = (122, 140, 153)
-    # repainted = img.copy()
-    # repainted[120:135, 125:131, 0] = clr[0]
-    # repainted[120:135, 125:131, 1] = clr[1]
-    # repainted[120:135, 125:131, 2] = clr[2]
-
-    # RGB to grayscale
-    # R, G, B = repainted[:, :, 0], repainted[:, :, 1], repainted[:, :, 2]
-    # gray = 0.2989 * R + 0.587 * G + 0.114 * B
-
-    # Expand dims
-    # expanded = np.expand_dims(gray, -1)
 
     # Channel first
     rgb = rgb.transpose(2, 0, 1)
 
-    # Resize image to 64x64
-    # resized = transposed[:, 96:160, 96:160]
-
-    # rep = repainted.transpose(2, 0, 1)
-    # plotter(rgb, 3, name="from obs")
-
-    # intersection_util.plotter3d(rgb, rgb_gray=3, name="From Obs")
-
     return np.uint8(rgb)
-
-
-class ObsMLP(gym.Wrapper):
-    def __init__(self, env: gym.Env):
-        super().__init__(env)
-        self.observation_space = gym.spaces.Box(
-            low=-1e8,
-            high=1e8,
-            shape=(50,),
-            dtype=np.float32,
-        )
-
-    def step(self, action):
-        """Steps the environment by one step.
-
-        Args:
-            actions (Any): Agent's action.
-
-        Returns:
-            Tuple[ np.ndarray, float, bool, Dict[str, Any] ]:
-                Observation, reward, done, info, of the agent.
-        """
-        obs, rewards, dones, infos = self.env.step(action)
-        filtered = filter_obs_mlp(obs)
-        return filtered, rewards, dones, infos
-
-    def reset(self):
-        """Resets the environment.
-
-        Returns:
-            np.ndarray: Agent's observation after reset.
-        """
-        obs = self.env.reset()
-        filtered = filter_obs_mlp(obs)
-        return filtered
-
-
-def filter_obs_mlp(obs: Dict[str, gym.Space]) -> np.ndarray:
-    import dataclasses
-
-    print(dataclasses.asdict(obs).keys())
-    print(obs.neighbors)
-    print("===================================================")
-
-    waypoints = obs.waypoints["pos"]
-    neighbors = obs.neighbors["pos"]
-    # neighbors = obs.neighbors["speed"]
-    d = obs.ego["pos"]
-    f = obs.ego["speed"]
-    g = obs.ego["heading"]
-    h = obs.ego["steering"]
-
-    print(waypoints.shape, "ddddddddddddddddddddd")
-    print(waypoints.flatten().shape, "ddddddddddddddddddddd")
-    print(waypoints)
-
-    import sys
-
-    sys.exit(3)
-
-    return np.zeros(shape=(50,), dtype=np.float32)
--- examples/rl/intersection/run.py
+++ examples/rl/intersection/run.py
@@ -14,9 +14,7 @@ from ruamel.yaml import YAML
 from intersection import action as intersection_action
 from intersection import info as intersection_info
 from intersection import observation as intersection_observation
-from intersection import policy as intersection_policy
 from intersection import reward as intersection_reward
-from intersection import util as intersection_util
 from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.evaluation import evaluate_policy
@@ -60,15 +58,15 @@ def main(args: argparse.Namespace):
         raise KeyError(f'Expected \'train\' or \'evaluate\', but got {config["mode"]}.')
 
     # Make training and evaluation environments.
-    env = make_env(config=config, training=True)
-    eval_env = make_env(config=config, training=False)
+    env = make_env(config=config)
+    eval_env = make_env(config=config)
 
     # Run training or evaluation.
     run(env=env, eval_env=eval_env, config=config)
     env.close()
 
 
-def make_env(config: Dict[str, Any], training: bool) -> gym.Env:
+def make_env(config: Dict[str, Any]) -> gym.Env:
     # Create environment
     env = gym.make(
         "smarts.env:intersection-v0",
@@ -81,9 +79,9 @@ def make_env(config: Dict[str, Any], training: bool) -> gym.Env:
 
     # Wrap env with action, reward, and observation wrapper
     env = intersection_info.Info(env=env)
-    env = intersection_action.Action(env=env, space=config["action_wrapper"])
+    env = intersection_action.Action(env=env)
     env = intersection_reward.Reward(env=env)
-    env = getattr(intersection_observation, config["observation_wrapper"])(env=env)
+    env = intersection_observation.Observation(env=env)
 
     # Check custom environment
     check_env(env)
@@ -121,14 +119,12 @@ def run(env: gym.Env, eval_env: gym.Env, config: Dict[str, Any]):
         model = getattr(sb3lib, config["alg"]).load(
             config["model"], print_system_info=True
         )
-        intersection_util.print_model(model, env, config["alg"])
     elif config["mode"] == "train" and config.get("model", None):
         print("\nStart training from an existing model.\n")
         model = getattr(sb3lib, config["alg"]).load(
             config["model"], print_system_info=True
         )
         model.set_env(env)
-        intersection_util.print_model(model, env, config["alg"])
         model.learn(
             total_timesteps=config["train_steps"],
             callback=[checkpoint_callback, eval_callback],
@@ -139,9 +135,8 @@ def run(env: gym.Env, eval_env: gym.Env, config: Dict[str, Any]):
             env=env,
             verbose=1,
             tensorboard_log=config["logdir"] / "tensorboard",
-            **(getattr(intersection_policy, config["policy"])(config)),
+            **config["alg_kwargs"],
         )
-        intersection_util.print_model(model, env, config["alg"])
         model.learn(
             total_timesteps=config["train_steps"],
             callback=[checkpoint_callback, eval_callback],
@@ -193,38 +188,3 @@ if __name__ == "__main__":
         raise Exception("When --mode=evaluate, --model option must be specified.")
 
     main(args)
-
-    # import torchvision.models as th_models
-    # import torch
-    # pip install prefetch_generator tqdm yacs
-
-    # modelut = th_models.video.r2plus1d_18(pretrained=pretrained, progress=True)
-    # modelut = th.hub.load('datvuthanh/hybridnets', 'hybridnets', pretrained=True)
-    # model = torch.hub.load('hustvl/yolop', 'yolop', pretrained=True)
-    # Print model summary
-    # print("\n\n")
-    # network = sb3_util.Network(modelut)
-    # print(model)
-    # sb3_util.summary(model=model, depth=10, input_size=(1,3,256,256))
-    # print("\n\n")
-
-    # import gym
-    # import Box2D
-    # env = gym.make('RoadRunner-v0')
-    # print("obs_space",env.observation_space)
-    # print("action_space",env.action_space)
-
-    # import gym
-    # import torch as th
-
-    # from stable_baselines3 import PPO
-
-    # # Custom actor (pi) and value function (vf) networks
-    # # of two layers of size 32 each with Relu activation function
-    # policy_kwargs = dict(activation_fn=th.nn.ReLU,
-    #                     net_arch=[dict(pi=[32, 32], vf=[32, 32])])
-    # # Create the agent
-    # model = PPO("MlpPolicy", "CartPole-v1", policy_kwargs=policy_kwargs, verbose=1)
-    # # Retrieve the environment
-    # env = model.get_env()
-    # print(env.observation_space)
