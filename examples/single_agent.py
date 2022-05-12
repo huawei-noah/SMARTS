47e1ea9687696e88e32970e1e9d21cb6df796843
qianyi-sun <sqy201x@hotmail.com>
2022-05-12 12:14:08 -0400

    simplify single_agent.py

--- examples/single_agent.py
+++ examples/single_agent.py
@@ -2,7 +2,7 @@ import logging
 import pathlib
 
 import gym
-
+from functools import partial
 from examples.argument_parser import default_argument_parser
 from smarts.core.agent import Agent
 from smarts.core.agent_interface import AgentInterface, AgentType
@@ -15,22 +15,15 @@ from smarts.zoo.agent_spec import AgentSpec
 logging.basicConfig(level=logging.INFO)
 
 
-class ChaseViaPointsAgent(Agent):
+class TargetLaneAgent(Agent):
     def act(self, obs: Observation):
-        if (
-            len(obs.via_data.near_via_points) < 1
-            or obs.ego_vehicle_state.road_id != obs.via_data.near_via_points[0].road_id
-        ):
-            return (obs.waypoint_paths[0][0].speed_limit, 0)
-
-        nearest = obs.via_data.near_via_points[0]
-        if nearest.lane_index == obs.ego_vehicle_state.lane_index:
-            return (nearest.required_speed, 0)
-
         return (
-            nearest.required_speed,
-            1 if nearest.lane_index > obs.ego_vehicle_state.lane_index else -1,
+            obs.waypoint_paths[0][0].speed_limit,
+            self._target_lane - obs.ego_vehicle_state.lane_index,
         )
+
+    def __init__(self, target_lane):
+        self._target_lane = target_lane
 
 
 def main(scenarios, headless, num_episodes, max_episode_steps=None):
@@ -38,7 +31,7 @@ def main(scenarios, headless, num_episodes, max_episode_steps=None):
         interface=AgentInterface.from_type(
             AgentType.LanerWithSpeed, max_episode_steps=max_episode_steps
         ),
-        agent_builder=ChaseViaPointsAgent,
+        agent_builder=partial(TargetLaneAgent, 3),
     )
 
     env = gym.make(
