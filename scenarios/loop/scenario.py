ae7d19f1ce98fb5327533653abd1130b8881d7ab
qianyi-sun <sqy201x@hotmail.com>
2022-05-18 13:35:08 -0400

    set rate to a very small number

--- scenarios/loop/scenario.py
+++ scenarios/loop/scenario.py
@@ -15,7 +15,7 @@ traffic = t.Traffic(
                 begin=(route[0],random.randint(0,2), "random"),
                 end=(route[1], random.randint(0,2), "max"),  
             ),
-            rate=1,
+            rate=0.000000000001,
             actors={t.TrafficActor(name="car",speed=Distribution(mean=0.5, sigma=0.8),vehicle_type=random.choice(["passenger","coach","bus","trailer","truck"])): 1},
         )
         for route in [("445633931","445633932"),("445633932","445633931")]*12
