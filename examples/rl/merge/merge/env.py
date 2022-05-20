2a8edea7ddfcef07e8733e16478acfbfce7f1d53
adaickalavan <adaickalavan@gmail.com>
2022-05-19 22:57:32 -0400

    Reorganise code.

---
+++ examples/rl/merge/merge/env.py
@@ -0,0 +1,58 @@
+from typing import Any, Dict
+
+from gym.wrappers.frame_stack import FrameStack
+from merge import action as merge_action
+from merge import observation as merge_observation
+from merge import reward as merge_reward
+from tf_agents.environments import (
+    PyEnvironment,
+    suite_gym,
+    tf_py_environment,
+    validate_py_environment,
+)
+
+
+def make(config: Dict[str, Any]) -> PyEnvironment:
+    # Create environment in TF.
+    # Refer to https://www.tensorflow.org/agents/tutorials/2_environments_tutorial
+    gym_reward_wrapper = merge_reward.Reward
+    gym_action_wrapper = lambda env: merge_action.Action(
+        env=env, space=config["action_wrapper"]
+    )
+    gym_obs_wrapper = merge_observation.RGB
+    gym_frame_stack = lambda env: FrameStack(env=env, num_stack=config["num_stack"])
+    gym_frame_concatenate = merge_observation.Concatenate
+    pyenv = suite_gym.load(
+        environment_name="smarts.env:merge-v0",
+        gym_env_wrappers=[
+            gym_reward_wrapper,
+            gym_action_wrapper,
+            gym_obs_wrapper,
+            gym_frame_stack,
+            gym_frame_concatenate,
+        ],
+        gym_kwargs={
+            "headless": not config["head"],  # If False, enables Envision display.
+            "visdom": config["visdom"],  # If True, enables Visdom display.
+            "sumo_headless": not config[
+                "sumo_gui"
+            ],  # If False, enables sumo-gui display.
+            "img_meters": config["img_meters"],
+            "img_pixels": config["img_pixels"],
+        },
+    )
+    validate_py_environment(environment=pyenv)
+    # (Optional) Manually verify Py env spaces
+    # print('action_spec:', pyenv.action_spec())
+    # print('time_step_spec.observation:', pyenv.time_step_spec().observation)
+    # print('time_step_spec.step_type:', pyenv.time_step_spec().step_type)
+    # print('time_step_spec.discount:', pyenv.time_step_spec().discount)
+    # print('time_step_spec.reward:', pyenv.time_step_spec().reward)
+
+    tfenv = tf_py_environment.TFPyEnvironment(pyenv)
+    # (Optional) Manually verify TF env specs
+    # print(isinstance(tfenv, tf_environment.TFEnvironment))
+    # print("TimeStep Specs:", tfenv.time_step_spec())
+    # print("Action Specs:", tfenv.action_spec())
+
+    return tfenv
--- examples/rl/merge/run.py
+++ examples/rl/merge/run.py
@@ -10,25 +10,11 @@ from pathlib import Path
 from typing import Any, Dict
 
 import gym
-from gym.utils.env_checker import check_env
-from gym.wrappers.frame_stack import FrameStack
-from merge import action as merge_action
 from merge import agent as merge_agent
 from merge import buffer as merge_buffer
+from merge import env as merge_env
 from merge import network as merge_network
-from merge import observation as merge_observation
-from merge import reward as merge_reward
-
-# from merge import policy as merge_policy
-# from merge import util as merge_util
 from ruamel.yaml import YAML
-from tf_agents.environments import (
-    PyEnvironment,
-    suite_gym,
-    tf_environment,
-    tf_py_environment,
-    validate_py_environment,
-)
 
 warnings.simplefilter("ignore", category=UserWarning)
 warnings.simplefilter("ignore", category=DeprecationWarning)
@@ -75,59 +61,13 @@ def main(args: argparse.Namespace):
         raise KeyError(f'Expected \'train\' or \'evaluate\', but got {config["mode"]}.')
 
     # Make training and evaluation environments.
-    train_env = make_env(config=config)
-    eval_env = make_env(config=config)
+    train_env = merge_env.make(config=config)
+    eval_env = merge_env.make(config=config)
 
     # Run training or evaluation.
     run(train_env=train_env, eval_env=eval_env, config=config)
     train_env.close()
     eval_env.close()
-
-
-def make_env(config: Dict[str, Any]) -> PyEnvironment:
-    # Create environment in TF.
-    # Refer to https://www.tensorflow.org/agents/tutorials/2_environments_tutorial
-    gym_reward_wrapper = merge_reward.Reward
-    gym_action_wrapper = lambda env: merge_action.Action(
-        env=env, space=config["action_wrapper"]
-    )
-    gym_obs_wrapper = merge_observation.RGB
-    gym_frame_stack = lambda env: FrameStack(env=env, num_stack=config["num_stack"])
-    gym_frame_concatenate = merge_observation.Concatenate
-    pyenv = suite_gym.load(
-        environment_name="smarts.env:merge-v0",
-        gym_env_wrappers=[
-            gym_reward_wrapper,
-            gym_action_wrapper,
-            gym_obs_wrapper,
-            gym_frame_stack,
-            gym_frame_concatenate,
-        ],
-        gym_kwargs={
-            "headless": not config["head"],  # If False, enables Envision display.
-            "visdom": config["visdom"],  # If True, enables Visdom display.
-            "sumo_headless": not config[
-                "sumo_gui"
-            ],  # If False, enables sumo-gui display.
-            "img_meters": config["img_meters"],
-            "img_pixels": config["img_pixels"],
-        },
-    )
-    validate_py_environment(environment=pyenv)
-    # (Optional) Manually verify Py env spaces
-    # print('action_spec:', pyenv.action_spec())
-    # print('time_step_spec.observation:', pyenv.time_step_spec().observation)
-    # print('time_step_spec.step_type:', pyenv.time_step_spec().step_type)
-    # print('time_step_spec.discount:', pyenv.time_step_spec().discount)
-    # print('time_step_spec.reward:', pyenv.time_step_spec().reward)
-
-    tfenv = tf_py_environment.TFPyEnvironment(pyenv)
-    # (Optional) Manually verify TF env specs
-    # print(isinstance(tfenv, tf_environment.TFEnvironment))
-    # print("TimeStep Specs:", tfenv.time_step_spec())
-    # print("Action Specs:", tfenv.action_spec())
-
-    return tfenv
 
 
 def run(train_env: gym.Env, eval_env: gym.Env, config: Dict[str, Any]):
