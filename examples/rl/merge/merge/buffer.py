98fbf44b9d1f58bbf31ef57b783582f6ecc03a94
adaickalavan <adaickalavan@gmail.com>
2022-05-21 01:30:23 -0400

    Cleanup.

--- examples/rl/merge/merge/buffer.py
+++ examples/rl/merge/merge/buffer.py
@@ -10,35 +10,6 @@ from tf_agents.specs import tensor_spec
 from tf_agents.agents import tf_agent
 from typing import Dict
 
-def reverb_replay(env, agent, config):
-    table_name = "uniform_table"
-    replay_buffer_signature = tensor_spec.from_spec(agent.collect_data_spec)
-    replay_buffer_signature = tensor_spec.add_outer_dim(replay_buffer_signature)
-
-    table = reverb.Table(
-        table_name,
-        max_size=config["buffer_kwargs"]["replay_buffer_max_length"],
-        sampler=reverb.selectors.Uniform(),
-        remover=reverb.selectors.Fifo(),
-        rate_limiter=reverb.rate_limiters.MinSize(1),
-        signature=replay_buffer_signature,
-    )
-
-    reverb_server = reverb.Server([table])
-
-    replay_buffer = reverb_replay_buffer.ReverbReplayBuffer(
-        agent.collect_data_spec,
-        table_name=table_name,
-        sequence_length=2,
-        local_server=reverb_server,
-    )
-
-    replay_buffer_observer = reverb_utils.ReverbAddTrajectoryObserver(
-        replay_buffer.py_client, table_name, sequence_length=2
-    )
-
-    return replay_buffer, replay_buffer_observer
-
 
 def uniform_replay(env, agent, config):
     replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
@@ -70,5 +41,35 @@ def hashed_replay(env:gym.Env, agent:tf_agent.TFAgent, config:Dict):
         log_interval=None,
     )
     replay_buffer_observer = replay_buffer.add_batch
+
+    return replay_buffer, replay_buffer_observer
+
+
+def reverb_replay(env, agent, config):
+    table_name = "uniform_table"
+    replay_buffer_signature = tensor_spec.from_spec(agent.collect_data_spec)
+    replay_buffer_signature = tensor_spec.add_outer_dim(replay_buffer_signature)
+
+    table = reverb.Table(
+        table_name,
+        max_size=config["buffer_kwargs"]["replay_buffer_max_length"],
+        sampler=reverb.selectors.Uniform(),
+        remover=reverb.selectors.Fifo(),
+        rate_limiter=reverb.rate_limiters.MinSize(1),
+        signature=replay_buffer_signature,
+    )
+
+    reverb_server = reverb.Server([table])
+
+    replay_buffer = reverb_replay_buffer.ReverbReplayBuffer(
+        agent.collect_data_spec,
+        table_name=table_name,
+        sequence_length=2,
+        local_server=reverb_server,
+    )
+
+    replay_buffer_observer = reverb_utils.ReverbAddTrajectoryObserver(
+        replay_buffer.py_client, table_name, sequence_length=2
+    )
 
     return replay_buffer, replay_buffer_observer
--- examples/rl/merge/merge/driver.py
+++
@@ -1,8 +0,0 @@
-
-
-py_driver.PyDriver(
-    env,
-    py_tf_eager_policy.PyTFEagerPolicy(random_policy, use_tf_function=True),
-    [rb_observer],
-    max_steps=initial_collect_steps,
-).run(train_py_env.reset())
