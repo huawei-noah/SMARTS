2cab61099ea2a5b9378aaff0057a3c79bc26a28f
adaickalavan <adaickalavan@gmail.com>
2022-06-07 13:59:34 -0400

    Add multi_scenario example.

--- smarts/env/multi_scenario.py
+++ smarts/env/multi_scenario.py
@@ -38,7 +38,7 @@ from smarts.sstudio import build_scenario
 from smarts.zoo.agent_spec import AgentSpec
 
 
-def merge_v0_env(
+def multi_scenario_env(
     headless: bool = True,
     visdom: bool = False,
     sumo_headless: bool = True,
