2d89bde3d975f9f728524c4549f3c88bac03ef94
adaickalavan <adaickalavan@gmail.com>
2022-06-14 23:41:52 -0400

    Modify the cut in map.

--- smarts/scenarios/straight/3lane_cut_in/scenario.py
+++ smarts/scenarios/straight/3lane_cut_in/scenario.py
@@ -19,46 +19,115 @@
 # THE SOFTWARE.
 
 import random
+from itertools import combinations
 from pathlib import Path
 
-import numpy as np
-
 from smarts.sstudio import gen_scenario
-from smarts.sstudio.types import (
-    Distribution,
-    Flow,
-    Mission,
-    Route,
-    Scenario,
-    Traffic,
-    TrafficActor,
-)
+from smarts.sstudio.types import Flow, Mission, Route, Scenario, Traffic, TrafficActor, LaneChangingModel
 
 normal = TrafficActor(
     name="car",
-    speed=Distribution(mean=0.5, sigma=0.8),
+    lane_changing_model=LaneChangingModel(impatience=0, cooperative=0.5),
+
 )
 
-traffic = Traffic(
-    flows=[
-        Flow(
-            route=Route(
-                begin=("E1", random.randint(0, 2), "random"),
-                end=("E2", random.randint(0, 2), "max"),
-            ),
-            rate=3,
-            begin=np.random.exponential(scale=2.5),
-            actors={normal: 1},
-        )
-        for _ in range(3)
-        for _ in range(4)
-    ]
-)
-ego_mission = [Mission(Route(begin=("E1", 1, 1), end=("E2", 1, "max")))]
+# See SUMO doc
+# Lane changing model
+# https://sumo.dlr.de/docs/Definition_of_Vehicles%2C_Vehicle_Types%2C_and_Routes.html#lane-changing_models
+# Junction model
+# https://sumo.dlr.de/docs/Definition_of_Vehicles%2C_Vehicle_Types%2C_and_Routes.html#junction_model_parameters
 
-scenario = Scenario(
-    traffic={"all": traffic},
-    ego_missions=ego_mission,
-)
+# normal = TrafficActor(
+#     name="car",
+#     speed=Distribution(sigma=0.8, mean=0.8),
+# )
+# cooperative = TrafficActor(
+#     name="cooperative",
+#     speed=Distribution(sigma=0.3, mean=1.0),
+#     lane_changing_model=LaneChangingModel(
+#         pushy=0.1,
+#         impatience=0.1,
+#         cooperative=0.9,
+#         speed_Gain=0.8,
+#     ),
+#     junction_model=JunctionModel(
+#         impatience=0.1,
+#     ),
+# )
+# aggressive = TrafficActor(
+#     name="aggressive",
+#     speed=Distribution(sigma=0.3, mean=1.0),
+#     lane_changing_model=LaneChangingModel(
+#         pushy=0.8,
+#         impatience=1,
+#         cooperative=0.1,
+#         speed_Gain=2.0,
+#     ),
+#     junction_model=JunctionModel(
+#         impatience=0.6,
+#     ),
+# )
 
-gen_scenario(scenario, output_dir=str(Path(__file__).parent))
+# flow_name = (start_lane, end_lane,)
+route_opt = [
+    (0, 0),
+    (0, 1),
+    (0, 2),
+    (1, 0),
+    (1, 1),
+    (1, 2),
+    (2, 0),
+    (2, 1),
+    (2, 2),
+]
+
+# Traffic combinations = 3C2 + 3C3 = 3 + 1 = 4
+# Repeated traffic combinations = 4 * 100 = 400
+min_flows = 3
+max_flows = 6
+route_comb = [
+    com
+    for elems in range(min_flows, max_flows + 1)
+    for com in combinations(route_opt, elems)
+]
+
+traffic = {}
+for name, routes in enumerate(route_comb):
+    traffic[str(name)] = Traffic(
+        flows=[
+            Flow(
+                route=Route(
+                    begin=("E1", r[0], 0),
+                    # via=("E0"),
+                    end=("E2", r[1], "max"),
+                ),
+                # Random flow rate, between x and y vehicles per minute.
+                rate=60 * random.uniform(10, 20),
+                # Random flow start time, between x and y seconds.
+                begin=random.uniform(0, 5),
+                # For an episode with maximum_episode_steps=3000 and step
+                # time=0.1s, maximum episode time=300s. Hence, traffic set to
+                # end at 900s, which is greater than maximum episode time of
+                # 300s.
+                end=60 * 15,
+                actors={normal: 1},
+                randomly_spaced=True,
+            )
+            for r in routes
+        ]
+    )
+
+ego_missions = [
+    Mission(
+        Route(begin=("E1", 1, 10), end=("E2", 1, "max")),
+        start_time=14,
+    )
+]
+
+gen_scenario(
+    scenario=Scenario(
+        traffic=traffic,
+        ego_missions=ego_missions,
+    ),
+    output_dir=Path(__file__).parent,
+)
