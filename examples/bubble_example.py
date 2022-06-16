209fd62e5ed639f6e6d0584c0c23bde9efba7a0d
Tucker <montgomery.alban@huawei.com>
2022-06-16 19:07:22 -0400

    Simplify example

--- examples/bubble_example.py
+++ examples/bubble_example.py
@@ -132,11 +132,14 @@ def main(
     scenario_list = Scenario.get_scenario_list([scenario])
     scenarios_iterator = Scenario.variations_for_all_scenario_roots(scenario_list, [])
 
-    class obs_ref:
-        last_observations: Dict[str, Observation] = None
+    class ObservationState:
+        def __init__(self) -> None:
+            self.last_observations: Dict[str, Observation] = None
 
-    def observation_callback(obs: Observation):
-        obs_ref.last_observations = obs
+        def observation_callback(self, obs: Observation):
+            self.last_observations = obs
+    
+    obs_state = ObservationState()
 
     for scenario in scenarios_iterator:
         scenario.bubbles.clear()
@@ -145,7 +148,7 @@ def main(
         # XXX replace with AgentSpec appropriate for IL model
         agent_manager: AgentManager = smarts.agent_manager
         agent_manager.add_social_observation_callback(
-            observation_callback, "bubble_watcher"
+            obs_state.observation_callback, "bubble_watcher"
         )
 
         for episode in range(episodes):
@@ -154,32 +157,27 @@ def main(
             traffic_history_provider: TrafficHistoryProvider = smarts.get_provider_by_type(TrafficHistoryProvider)
             used_history_ids = set()
 
-            bubble_manager: BubbleManager = smarts._bubble_manager
-            bubbles = bubble_manager.bubbles
             agent = UsedAgent(logger=logger)
 
             for _ in range(run_steps):
-                agent_ids_per_bubble = [
-                    bubble_manager.agent_ids_for_bubble(b, smarts) for b in bubbles
-                ]
-                for agent_ids in agent_ids_per_bubble:
-                    if agent_ids == None:
+                for agent_id in obs_state.last_observations:
+                    if agent_id not in obs_state.last_observations:
                         continue
-                    for agent_id in agent_ids:
-                        if agent_id not in obs_ref.last_observations:
-                            continue
-                        # Replace the original action
-                        agent_manager.reserve_social_agent_action(
-                            agent_id, agent.act(obs_ref.last_observations[agent_id])
-                        )
-                        used_history_ids |= {obs_ref.last_observations[agent_id].ego_vehicle_state.id}
-                _, _, _, _ = smarts.step({})
+                    # Replace the original action that the social agent would do
+                    agent_manager.reserve_social_agent_action(
+                        agent_id, agent.act(obs_state.last_observations[agent_id])
+                    )
+                    used_history_ids |= {obs_state.last_observations[agent_id].ego_vehicle_state.id}
+                # Step SMARTS
+                _, _, _, _ = smarts.step({}) # observation_callback is called, obs_state updated
+                # Currently ensure vehicles are removed permanently when they leave bubble
                 traffic_history_provider.set_replaced_ids(used_history_ids)
                 # Update the current bubble in case there are new active bubbles
-                bubbles = bubble_manager.bubbles
 
-                for agent_id in obs_ref.last_observations.keys():
-                    if obs_ref.last_observations[agent_id].events.reached_goal:
+                # Iterate through the observations
+                # The agent ids of agents can be found here.
+                for agent_id in obs_state.last_observations.keys():
+                    if obs_state.last_observations[agent_id].events.reached_goal:
                         logger.info(
                             "agent_id={} reached goal @ sim_time={}".format(
                                 agent_id, smarts.elapsed_sim_time
@@ -187,7 +185,7 @@ def main(
                         )
                         logger.debug(
                             "   ... with {}".format(
-                                obs_ref.last_observations[agent_id].events
+                                obs_state.last_observations[agent_id].events
                             )
                         )
 
