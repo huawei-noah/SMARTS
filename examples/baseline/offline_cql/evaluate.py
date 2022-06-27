91e6aafa038757a58ffbc91869a8115d96d5c23b
CZ <cz.wang@uwaterloo.ca>
2022-06-27 18:29:47 -0400

    train with evaluation

--- examples/baseline/offline_cql/evaluate.py
+++ examples/baseline/offline_cql/evaluate.py
@@ -9,39 +9,79 @@ from d3rlpy.algos import CQL
 import pathlib
 from competition_env import CompetitionEnv
 
+from d3rlpy.ope import FQE
+from d3rlpy.metrics.scorer import initial_state_value_estimation_scorer
+from d3rlpy.metrics.scorer import soft_opc_scorer
+
 def _build_scenario():
     scenario = str(pathlib.Path(__file__).absolute().parent / "scenarios/cruise")
     build_scenario = f"scl scenario build-all --clean {scenario}"
     os.system(build_scenario)
 
 max_episode_steps = 300
-env = CompetitionEnv(["scenarios/cruise"], max_episode_steps=300, headless=False)
+env = CompetitionEnv(["scenarios/cruise"], max_episode_steps=300, headless=True)
 _build_scenario()
 print("Start evaluation.")
-saved_folder = sorted(os.listdir(pathlib.Path(__file__).absolute().parent/'d3rlpy_logs/'))[0]
-model = CQL.from_json('d3rlpy_logs/' + saved_folder + '/params.json', use_gpu=True)
-model.load_model('saved_model/model_500.pt')
-for i in range(10):
-    obs=env.reset()
-    # print(obs)
-    done = False
-    while not done:
-        bev = np.array([obs['EGO'].top_down_rgb.data.reshape(3,256,256)])
-        action = model.predict(bev)[0]
-        agent_actions = {'EGO': action}
-        print(action)
-        obs, reward, done, extra = env.step(agent_actions)
-        done = done['EGO']
+files = os.listdir('saved_model')
+files.sort(key=os.path.getctime)
+for filename in files:
+    saved_folder = sorted(os.listdir(pathlib.Path(__file__).absolute().parent/'d3rlpy_logs/'))[0]
+    model = CQL.from_json('d3rlpy_logs/' + saved_folder + '/params.json', use_gpu=True)
+    model.load_model('saved_model/' + filename)
+    print('Loading model ' + filename)
+
+    # evaluate using simulation
+    eval_rewards = list()
+    for i in range(1):
+        obs=env.reset()
+        acc_reward = 0
+        # print(obs)
+        done = False
+        while not done:
+            bev = np.array([obs['EGO'].top_down_rgb.data.reshape(3,256,256)])
+            action = model.predict(bev)[0]
+            agent_actions = {'EGO': action}
+            obs, reward, done, extra = env.step(agent_actions)
+            acc_reward += reward['EGO']
+            done = done['EGO']
+        eval_rewards.append(acc_reward)
+    print(eval_rewards)
 env.close()
+
+
+# evaluate using offline data
+
+
+
 
 # with open('collected_data/100-EGO.pkl', 'rb') as f:
 #     data = pickle.load(f)
 
 # obs = list()
+# actions = list()
+# rewards = list()
+# terminals = list()
 # for j in range(len(data)):
 #     if data[j]['rewards'] != None:
 #         obs.append(data[j]['obs'].top_down_rgb.data.reshape(3,256,256))
+#         actions.append(data[j]['actions'])
+#         rewards.append(data[j]['rewards'])
+#         if data[j]['dones']:
+#             terminals.append(1)
+#         else:
+#             terminals.append(0)
 # obs = np.array(obs)
-# actions = model.predict(obs)
-# print(actions)
-# print(actions.shape)
+# actions = np.array(actions)
+# rewards = np.array(rewards)
+# terminals = np.array(terminals)
+# dataset = MDPDataset(obs, actions, rewards, terminals)
+# fqe = FQE(algo=model)
+# fqe.fit(dataset.episodes,
+#         eval_episodes=dataset.episodes,
+#         n_steps=10,
+#         n_steps_per_epoch = 10,
+#         scorers={
+#            'init_value': initial_state_value_estimation_scorer,
+#            'soft_opc': soft_opc_scorer(return_threshold=180)
+#         })
+
--- examples/baseline/offline_cql/train_with_replay.py
+++ examples/baseline/offline_cql/train_with_replay.py
@@ -8,6 +8,11 @@ from d3rlpy.dataset import MDPDataset
 from d3rlpy.algos import CQL, BCQ
 import pathlib
 import glob
+from sklearn.model_selection import train_test_split
+from d3rlpy.metrics.scorer import average_value_estimation_scorer
+from d3rlpy.metrics.scorer import td_error_scorer
+from sklearn.model_selection import cross_validate
+
 
 data_size = len(os.listdir('collected_data'))
 index = list(range(data_size))
@@ -21,7 +26,7 @@ for i in index:
     terminals = list()
     filename = str(i) + '-EGO.pkl'
     print('Training with ' + filename)
-    if os.path.isfile('saved_model/model_' + str(i) + '.pt'):
+    if os.path.isfile('saved_model_eval/model_' + str(i) + '.pt'):
         print('skip ' + filename)
         total_num_train += 1
         continue
@@ -32,7 +37,6 @@ for i in index:
                 data = pickle.load(f)
             print('There are ' + str(len(data)) + ' slices')
 
-
             for j in range(len(data)):
                 if data[j]['rewards'] != None:
                     obs.append(data[j]['obs'].top_down_rgb.data.reshape(3,256,256))
@@ -42,34 +46,61 @@ for i in index:
                         terminals.append(1)
                     else:
                         terminals.append(0)
+            
             obs = np.array(obs)
             actions = np.array(actions)
             rewards = np.array(rewards)
             terminals = np.array(terminals)
             dataset = MDPDataset(obs, actions, rewards, terminals)
+            
 
             if total_num_train == 0:
-                model = d3rlpy.algos.CQL(use_gpu=True, batch_size=1)
-                model.fit(dataset, 
-                    eval_episodes=dataset, 
-                    n_steps_per_epoch = 100,
-                    n_steps = 100, 
-                )
-                model.save_model('saved_model/model_' + str(i) + '.pt')
+                model = d3rlpy.algos.CQL(use_gpu=True, batch_size=32)
             else:
-                saved_models = glob.glob('saved_model/*')
+                saved_models = glob.glob('d3rlpy_logs/*')
                 latest_model = max(saved_models, key=os.path.getctime)
-                saved_folder = sorted(os.listdir(pathlib.Path(__file__).absolute().parent/'d3rlpy_logs/'))[0]
-                model = CQL.from_json('d3rlpy_logs/' + saved_folder + '/params.json', use_gpu=True)
-                model.load_model(latest_model)
-                print('Load model_' + latest_model)
-                model.fit(dataset, 
-                    eval_episodes=dataset, 
-                    n_steps_per_epoch = 100,
-                    n_steps = 100, 
-                    save_metrics=False
-                )
-                model.save_model('saved_model/model_' + str(i) + '.pt')
+                model = CQL.from_json('d3rlpy_logs/1/params.json', use_gpu=True)
+                model.load_model(latest_model + '/model_10.pt')
+                # saved_models = glob.glob('d3rlpy_logs/*')
+                # latest_model = max(saved_models, key=os.path.getctime)
+                # saved_folder = sorted(os.listdir(pathlib.Path(__file__).absolute().parent/'d3rlpy_logs/'))[0]
+
+            model.fit(dataset, 
+                eval_episodes=dataset, 
+                n_steps_per_epoch = 10,
+                n_steps = 10, 
+                scorers={
+                            'td_error': td_error_scorer,
+                            'value_scale': average_value_estimation_scorer,
+                        }
+            )
+            saved_models = glob.glob('d3rlpy_logs/*')
+            latest_model = max(saved_models, key=os.path.getctime)
+            os.rename(latest_model, 'd3rlpy_logs/' + str(total_num_train + 1))
+
+            # else:
+            #     saved_models = glob.glob('d3rlpy_logs/*')
+            #     latest_model = max(saved_models, key=os.path.getctime)
+            #     saved_folder = sorted(os.listdir(pathlib.Path(__file__).absolute().parent/'d3rlpy_logs/'))[0]
+            #     # model = CQL.from_json('d3rlpy_logs/' + saved_folder + '/params.json', use_gpu=True)
+            #     model = CQL.from_json(latest_model + '/params.json', use_gpu=True)
+            #     model.load_model(latest_model + '/model_' + str(total_num_train + 1) + '.pt')
+            #     print('Load model_' + latest_model)
+            #     model.fit(dataset, 
+            #         eval_episodes=dataset, 
+            #         n_steps_per_epoch = 10,
+            #         n_steps = 10, 
+            #         scorers={
+            #                     'td_error': td_error_scorer,
+            #                     'value_scale': average_value_estimation_scorer,
+            #                 }
+
+            #     )
+                
+            #     # model.save_model('saved_model_eval/model_' + str(i) + '.pt')
+            #     saved_models = glob.glob('d3rlpy_logs/*')
+            #     latest_model = max(saved_models, key=os.path.getctime)
+            #     os.rename(latest_model, 'd3rlpy_logs/' + str(total_num_train + 1))
             
             total_num_train += 1
             print('There are ' + str(total_num_train) + ' being trained')
