Fix goal_angle calculation.

examples/rl/multi_scenario/train/train/observation.py
@@ -68,20 +68,22 @@ class FilterObs(gym.ObservationWrapper):
                 dtype=np.float32,
             )
 
-            # Ego's angle with respect to the map's axes.
-            # Note: All angles returned by smarts is with respect to the map axes.
-            #       Angle is zero at positive y axis, and increases anti-clockwise, on the map.
-            ego_angle = (agent_obs["ego"]["heading"] + np.pi) % (2 * np.pi) - np.pi
+            # Ego's heading with respect to the map's axes.
+            # Note: All angles returned by smarts is with respect to the map's axes.
+            #       On the map, angle is zero at positive y axis, and increases anti-clockwise.
+            ego_heading = (agent_obs["ego"]["heading"] + np.pi) % (2 * np.pi) - np.pi
+            ego_pos = agent_obs["ego"]["position"]
 
-            # Goal's angle with respect to the map's axes.
-            # Note: Angle is zero at positive x axis, and increases anti-clockwise, in np.angle().
+            # Goal's angle with respect to the ego's position.
+            # Note: In np.angle(), angle is zero at positive x axis, and increases anti-clockwise.
             #       Hence, map_angle = np.angle() - π/2
-            goal_x, goal_y = agent_obs["mission"]["goal_pos"][:2]
-            goal_angle = np.angle(goal_x + goal_y * 1j) - np.pi / 2
+            goal_pos = agent_obs["mission"]["goal_pos"]
+            rel_pos = goal_pos - ego_pos
+            goal_angle = np.angle(rel_pos[0] + 1j*rel_pos[1]) - np.pi / 2
             goal_angle = (goal_angle + np.pi) % (2 * np.pi) - np.pi
 
             # Goal heading is the angle correction required by ego agent to face the goal.
-            goal_heading = goal_angle - ego_angle
+            goal_heading = goal_angle - ego_heading
             goal_heading = (goal_heading + np.pi) % (2 * np.pi) - np.pi
             goal_heading = np.array([[goal_heading]], dtype=np.float32)
 