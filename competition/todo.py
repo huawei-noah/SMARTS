d513f1cd37d462c4cf91cdf2464ab47bd97176b1
adaickalavan <adaickalavan@gmail.com>
2022-06-30 12:49:31 -0400

    Update todo.

--- competition/todo.py
+++ competition/todo.py
@@ -1,15 +1,11 @@
 # Things to ask.
 # 1. The scales of score are different and cannot have a single overall score.
-# 2. Maximum steps allowed for each episodes? Currently it is 3000.
 # 3. Different traffic rou.xml file is build each time. For evaluation, must fix the built rou.xml files
 # to ensure same score for same models.
 # 4. How should costs be affected if no overtake happens in overtake scenario? 
-# 5. Files organization for Codalab. 
-
 
 
 # To Do
-# 1. Mismatched observation space (1,1)
 # 2. Overtake detector
 # 3. Traci Error
 # 4. Codalab uploading
