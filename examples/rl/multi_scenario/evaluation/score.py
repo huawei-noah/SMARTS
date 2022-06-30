980878201eda5b50754e7ea66df2fd637e47d40c
adaickalavan <adaickalavan@gmail.com>
2022-06-30 08:06:38 -0400

    Revamped scores.

--- examples/rl/multi_scenario/evaluation/score.py
+++ examples/rl/multi_scenario/evaluation/score.py
@@ -1,5 +1,7 @@
-from custom_dict import CustomDict
-from evaluation.metric import COST_FUNCS
+from dataclasses import asdict
+from typing import Dict
+from evaluation.costs import Costs
+from evaluation.metric import Counts
 
 
 class Score:
@@ -10,42 +12,35 @@ class Score:
             "rules": 0,
             "time": 0,
         }
-        self._costs = CustomDict(**{key: 0 for key in COST_FUNCS.keys()})
-        self._episodes = 0
-        self._incomplete = 0
-        self._steps = 0
-        self._adjusted_steps = 0
-        self._goals_unachieved = 0
+        self._counts = Counts()
+        self._costs = Costs()
 
-    def add(self, metric):
-        for agent_costs in metric["costs"].values():
-            self._costs += agent_costs
-        self._episodes += metric["episodes"]
-        self._incomplete += metric["incomplete"]
-        self._steps += metric["steps"]
-        self._adjusted_steps += metric["adjusted_steps"]
-        self._goals_unachieved += metric["goals_unachieved"]
+    def add(self, counts: Counts, costs: Costs):
+        for count_name, count_val in asdict(counts).items():
+            new_val = getattr(self._counts, count_name) + count_val
+            setattr(self._counts, count_name, new_val)
 
-    def compute(self):
-        self._results["completion"] = _completion(
-            self._incomplete, self._goals_unachieved, self._episodes
-        )
-        self._results["humanness"] = _humanness(self._costs, self._steps)
-        self._results["rules"] = _rules(self._costs, self._episodes)
-        self._results["time"] = _time(self._adjusted_steps, self._episodes)
+        for cost_name, cost_val in asdict(costs).items():
+            new_val = getattr(self._costs, cost_name) + cost_val
+            setattr(self._costs, cost_name, new_val)
+
+    def compute(self)->Dict[str,float]:
+        self._results["completion"] = _completion(counts=self._counts)
+        self._results["humanness"] = _humanness(counts=self._counts, costs=self._costs)
+        self._results["rules"] = _rules(counts=self._counts, costs=self._costs)
+        self._results["time"] = _time(counts=self._counts)
 
         return self._results
 
 
-def _completion(incomplete, goals_unachieved, episodes):
-    w_ic = 0.6
-    w_gua = 0.4
+def _completion(counts: Counts)->float:
+    w_cr = 0.6
+    w_ic = 0.4
 
-    return (w_ic * incomplete + w_gua * goals_unachieved) / episodes
+    return (w_cr * counts.crashes + w_ic * counts.incomplete) / counts.episodes
 
 
-# Need to modify to take average of average jerkiness
-def _humanness(costs, steps):
+def _humanness(counts: Counts, costs: Costs)->float:
     w_d = 0.2
     w_j = 0.2
     w_lc = 0.2
@@ -54,16 +49,16 @@ def _humanness(costs, steps):
     w_yr = 0.1
 
     return (
-        w_d * costs["dist_to_obstacles"]
-        + w_j * costs["jerk"]
-        + w_lc * costs["lane_center_offset"]
-        + w_sr * costs["steering_rate"]
-        + w_vo * costs["velocity_offset"]
-        + w_yr * costs["yaw_rate"]
-    ) / steps
+        w_d * costs.dist_to_obstacles
+        + w_j * costs.jerk
+        + w_lc * costs.lane_center_offset
+        + w_sr * costs.steering_rate
+        + w_vo * costs.velocity_offset
+        + w_yr * costs.yaw_rate
+    ) / counts.episodes_adjusted
 
 
-def _rules(costs, episodes):
+def _rules(counts: Counts, costs: Costs)->float:
     w_c = 0.2
     w_ord = 0.2
     w_ort = 0.2
@@ -71,12 +66,17 @@ def _rules(costs, episodes):
     w_ww = 0.2
 
     return (
-        w_c * costs["collisions"]
-        + w_ord * costs["off_road"]
-        + w_ort * costs["off_route"]
-        + w_os * costs["on_shoulder"]
-        + w_ww * costs["wrong_way"]
-    ) / episodes
+        w_c * costs.collisions
+        + w_ord * costs.off_road
+        + w_ort * costs.off_route
+        + w_os * costs.on_shoulder
+        + w_ww * costs.wrong_way
+    ) / counts.episodes_adjusted
+
+
+def _time(counts: Counts)->float:
+    return counts.steps_adjusted / counts.episodes
+
 
 
 def _time(adjusted_steps, episodes):
