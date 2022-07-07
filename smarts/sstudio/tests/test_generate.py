3e566bd3dddcb4613983dac687c975f15e6d82fd
Tucker <montgomery.alban@huawei.com>
2022-07-07 11:21:53 -0400

    Add test

--- smarts/sstudio/tests/test_generate.py
+++ smarts/sstudio/tests/test_generate.py
@@ -19,6 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+import shutil
 import os
 import tempfile
 from typing import Sequence
@@ -88,15 +89,51 @@ def test_generate_traffic(traffic: Traffic):
             name="generated",
         )
 
-        with open("smarts/sstudio/tests/baseline.rou.xml") as f:
-            items = [x.items() for x in ElementTree(file=f).iter()]
+        _compare_files(
+            "smarts/sstudio/tests/baseline.rou.xml",
+            os.path.join(temp_dir, "traffic", "generated.rou.xml"),
+        )
 
-        with open(os.path.join(temp_dir, "traffic", "generated.rou.xml")) as f:
-            generated_items = [x.items() for x in ElementTree(file=f).iter()]
 
-        print(sorted(items))
-        print(sorted(generated_items))
-        assert sorted(items) == sorted(generated_items)
+def _compare_files(file1, file2):
+    with open(file1) as f:
+        items = [x.items() for x in ElementTree(file=f).iter()]
+
+    with open(file2) as f:
+        generated_items = [x.items() for x in ElementTree(file=f).iter()]
+
+    print(sorted(items))
+    print(sorted(generated_items))
+    if not sorted(items) == sorted(generated_items):
+        for a, b in zip(items, generated_items):
+            assert a == b, f"{file1} is different than {file2}"
+
+
+def test_scenario_generation_unchanged():
+    number_of_comparisons_greater_than_0 = False
+    with tempfile.TemporaryDirectory() as temp_dir1:
+        with tempfile.TemporaryDirectory() as temp_dir2:
+            loc1 = temp_dir1 + "/scenarios"
+            loc2 = temp_dir2 + "/scenarios"
+            from cli.studio import _build_all_scenarios
+            import re
+
+            shutil.copytree("scenarios/sumo", loc1)
+            _build_all_scenarios(True, True, [loc1], 42)
+
+            shutil.copytree("scenarios/sumo", loc2)
+            _build_all_scenarios(True, True, [loc2], 42)
+
+            for dirpath, dirnames, files in os.walk(loc1):
+                print(dirpath)
+                if "traffic" in dirpath:
+                    assert len(files) > 0
+                    for file in files:
+                        dir2 = re.sub(loc1, loc2, dirpath)
+                        _compare_files(dirpath + "/" + file, dir2 + "/" + file)
+                        number_of_comparisons_greater_than_0 = True
+
+    assert number_of_comparisons_greater_than_0
 
 
 def _gen_map_from_spec(scenario_root: str, map_spec: MapSpec):
