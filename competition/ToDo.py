d993f361ea34ddd5fee44cf930929db173dbe18f
adaickalavan <adaickalavan@gmail.com>
2022-07-08 05:12:50 -0400

    Updated html for data and evaluation.

--- competition/ToDo.py
+++ competition/ToDo.py
@@ -3,8 +3,8 @@
 # 2. Cut-in scenario does not work.
 # 3. Overtake scenario does not have enough diversity.
 # 4. Retrain model.
-# 5. Different traffic rou.xml file is built each time. For evaluation, fix the built rou.xml files to ensure same score for same models.
-# 6. Preferably, redirect participants to get the competition starter code from SMARTS Github repository.
-# 7. Time limit for execution of submission in codalab, currently at six hours.
-# 8. Number of disk space and resources for parallel submissions might be limited in Codalab.
-# 9. Using self hosted Codalab instances.
+# 5. For evaluation, need deterministic scenario builds to ensure same score for same models.
+# 6. Number of disk space, time, and resources for parallel submissions might be limited in Codalab.
+# 7. Using self hosted Codalab instances.
+# 8. How will TargetPose action space affect the scoring of steering rate and yaw rate.
+# 9. PyType error due to dupbuild
