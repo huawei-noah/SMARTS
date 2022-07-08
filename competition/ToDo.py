4902c0be143f73922e09177cefeb8533fabb62fb
adaickalavan <adaickalavan@gmail.com>
2022-07-08 01:06:19 -0400

    Add Dockerfile.

--- competition/ToDo.py
+++ competition/ToDo.py
@@ -1,10 +1,10 @@
 # To Do
 # 1. Traci Error.
-# 2. Codalab uploading.
-# 3. Cut-in scenario does not work.
-# 4. Overtake scenario does not have enough diversity.
-# 5. Retrain model.
-# 6. Different traffic rou.xml file is built each time. For evaluation, fix the built rou.xml files to ensure same score for same models.
-# 7. Preferably, redirect participants to get the competition starter code from SMARTS Github repository.
-# 8. Use requirements.txt to install dependencies from paticipant submission for evaluation.
-# 9. Rank scores in order of priority i.e., completion > time > humanness > rules.
+# 2. Cut-in scenario does not work.
+# 3. Overtake scenario does not have enough diversity.
+# 4. Retrain model.
+# 5. Different traffic rou.xml file is built each time. For evaluation, fix the built rou.xml files to ensure same score for same models.
+# 6. Preferably, redirect participants to get the competition starter code from SMARTS Github repository.
+# 7. Time limit for execution of submission in codalab, currently at six hours.
+# 8. Number of disk space and resources for parallel submissions might be limited in Codalab.
+# 9.
