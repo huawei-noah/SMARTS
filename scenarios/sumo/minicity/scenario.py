4ed7c9f8081f180b9916b6beec0d26e27502c25e
Stephen Hockema <stephen.hockema1@huawei.com>
2022-07-08 16:21:11 -0400

    reverted test commit

--- scenarios/sumo/minicity/scenario.py
+++ scenarios/sumo/minicity/scenario.py
@@ -7,7 +7,6 @@ from smarts.sstudio import types as t
 NUM_TRAFFIC_FLOWS = 350
 
 traffic = t.Traffic(
-    engine="SMARTS",  # TODO STEVE TEMP
     flows=[
         t.Flow(
             route=t.RandomRoute(),
