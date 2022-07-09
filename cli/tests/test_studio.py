5c57e54af4c618284554835c26f127951db178e8
Stephen Hockema <stephen.hockema1@huawei.com>
2022-07-08 20:31:50 -0400

    reverted experiment

--- cli/tests/test_studio.py
+++ cli/tests/test_studio.py
@@ -32,9 +32,8 @@ def _compare_files(file1, file2):
     with open(file2) as f:
         generated_items = [x.items() for x in ElementTree(file=f).iter()]
 
-    # STEVE TODO REVERT THIS
-    sorted_items = items  # sorted(items)
-    sorted_generated_items = generated_items  # sorted(generated_items)
+    sorted_items = sorted(items)
+    sorted_generated_items = sorted(generated_items)
     if not sorted_items == sorted_generated_items:
         for a, b in zip(sorted_items, sorted_generated_items):
             assert a == b, f"{file1} is different than {file2}"
