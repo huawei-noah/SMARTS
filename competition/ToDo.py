1fda467c8972e382874cd87980e7478482c91402
adaickalavan <adaickalavan@gmail.com>
2022-07-10 20:41:27 -0400

    Update ToDo.

--- competition/ToDo.py
+++ competition/ToDo.py
@@ -3,7 +3,7 @@
 # 2. Cut-in scenario does not work.
 # 3. Overtake scenario does not have enough diversity.
 # 4. Retrain model.
-# 5. For evaluation, need deterministic scenario builds to ensure same score for same models.
+# 5. Deterministic scenario builds - PR1492.
 # 6. Number of disk space, time, and resources for parallel submissions might be limited in Codalab.
 # 7. Using self hosted Codalab instances.
 # 8. How will TargetPose action space affect the scoring of steering rate and yaw rate.
