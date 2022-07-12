bc38ea80c4a6be8ef43fdc4285e342a4a30fcc25
adaickalavan <adaickalavan@gmail.com>
2022-07-12 10:05:05 -0400

    Update ToDo.

--- competition/ToDo.py
+++ competition/ToDo.py
@@ -1,10 +1,17 @@
-# To Do
+# Track-1
+
+# Concerns
 # 1. Traci Error.
-# 2. Cut-in scenario does not work.
-# 3. Overtake scenario does not have enough diversity.
-# 4. Retrain model.
-# 5. Deterministic scenario builds - PR1492.
-# 6. Number of disk space, time, and resources for parallel submissions might be limited in Codalab.
-# 7. Using self hosted Codalab instances.
-# 8. How will TargetPose action space affect the scoring of steering rate and yaw rate.
-# 9. HTML rendering does not occur properly in Codalab competition description.
+# 2. How will TargetPose action space affect the scoring of steering rate and yaw rate.
+
+# Insignificant
+# 1. Codalab disk space, time, and resources.
+# 2. HTML rendering does not occur properly in Codalab competition description.
+
+# ToDo
+# 1. Action space changed to Targetpose --> debugging
+# 2. Merge deterministic scenario builds - PR1492.
+# 3. Replace Cut-in scenario from Patrick.
+# 4. Remove Overtake detector according to Soheil's suggestion.
+# 5. Check Distance to obstacles calculator according to Soheil's suggestion.
+# 6. Try diversifying Overtake scenario.
