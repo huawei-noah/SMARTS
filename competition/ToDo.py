d6f2d2cd997204a8fc93941b0e62908c4c85265c
adaickalavan <adaickalavan@gmail.com>
2022-07-12 11:32:11 -0400

    Update ToDo.

--- competition/ToDo.py
+++ competition/ToDo.py
@@ -2,17 +2,18 @@
 
 # Concerns
 # 1. Traci Error.
-# 2. All are invited to review PR1487, test Codalab Secret url: https://codalab.lisn.upsaclay.fr/competitions/6015?secret_key=cbfc4754-73d9-4f11-80c5-997b2dbfb3de
+# 2. Review PR1487, test Codalab at secret url: https://codalab.lisn.upsaclay.fr/competitions/6015?secret_key=cbfc4754-73d9-4f11-80c5-997b2dbfb3de
+# 3. Issue 1453 - Envision display : Incomplete JSONError lexical Error invalid char in json text
 
 # ToDo
 # 1. Action space changed to Targetpose --> Now debugging
 # 2. Merge deterministic scenario builds - PR1492.
 # 3. Replace Cut-in scenario from Patrick.
 # 4. Remove Overtake detector according to Soheil's suggestion.
-# 5. Check distance to obstacles calculator according to Soheil's suggestion.
-# 6. Try diversifying Overtake scenario.
+# 5. Change distance to obstacles according to Soheil's suggestion.
 
 # Minor
-# 1. How will TargetPose action space affect the scoring of steering rate.
-# 2. Codalab disk space, time, and resources.
-# 3. HTML rendering does not occur properly in Codalab competition description.
+# 1. Can both tracks run concurrently and how to do it in Codalab.
+# 2. How will TargetPose action space affect the scoring of steering rate.
+# 3. Codalab disk space, time, and resources.
+# 4. HTML rendering does not occur properly in Codalab competition description.
