3a4ffc99a711e2d1cd1bd7b4a4990ead35edfeae
adaickalavan <adaickalavan@gmail.com>
2022-07-12 10:26:01 -0400

    Update ToDo.

--- competition/ToDo.py
+++ competition/ToDo.py
@@ -1,17 +1,18 @@
-# Track-1
+# For Track-1
 
 # Concerns
 # 1. Traci Error.
-# 2. How will TargetPose action space affect the scoring of steering rate and yaw rate.
-
-# Insignificant
-# 1. Codalab disk space, time, and resources.
-# 2. HTML rendering does not occur properly in Codalab competition description.
+# 2. All are invited to review PR1487, test Codalab Secret url: https://codalab.lisn.upsaclay.fr/competitions/6015?secret_key=cbfc4754-73d9-4f11-80c5-997b2dbfb3de
 
 # ToDo
-# 1. Action space changed to Targetpose --> debugging
+# 1. Action space changed to Targetpose --> Now debugging
 # 2. Merge deterministic scenario builds - PR1492.
 # 3. Replace Cut-in scenario from Patrick.
 # 4. Remove Overtake detector according to Soheil's suggestion.
-# 5. Check Distance to obstacles calculator according to Soheil's suggestion.
+# 5. Check distance to obstacles calculator according to Soheil's suggestion.
 # 6. Try diversifying Overtake scenario.
+
+# Minor
+# 1. How will TargetPose action space affect the scoring of steering rate.
+# 2. Codalab disk space, time, and resources.
+# 3. HTML rendering does not occur properly in Codalab competition description.
