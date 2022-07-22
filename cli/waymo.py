73bb1eb7eb5c49694f73123f7e83ff2af7c319a5
Saul Field <saul.field@gmail.com>
2022-07-22 14:31:40 -0400

    Implement preview and add remaining commands

--- cli/waymo.py
+++ cli/waymo.py
@@ -1,14 +1,34 @@
+from collections import defaultdict
+import os
 from typing import Dict, List
+
 import click
+import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.lines import Line2D
-import numpy as np
 from smarts.core.utils.file import read_tfrecord_file
 from smarts.core.waymo_map import WaymoMap
+from smarts.sstudio.types import MapSpec
 from tabulate import tabulate
 from waymo_open_dataset.protos import scenario_pb2
 
-from smarts.sstudio.types import MapSpec
+MAP_HANDLES = [
+    Line2D([0], [0], linestyle=":", color="gray", label="Lane Polyline"),
+    Line2D([0], [0], linestyle="-", color="yellow", label="Single Road Line"),
+    Line2D([0], [0], linestyle="--", color="yellow", label="Double Road Line"),
+    Line2D([0], [0], linestyle="-", color="black", label="Road Edge"),
+    Line2D([0], [0], linestyle="--", color="black", label="Crosswalk"),
+    Line2D([0], [0], linestyle=":", color="black", label="Speed Bump"),
+    Line2D(
+        [],
+        [],
+        color="red",
+        marker="o",
+        linestyle="None",
+        markersize=5,
+        label="Stop Sign",
+    ),
+]
 
 
 @click.group(
@@ -61,131 +81,53 @@ def overview(tfrecord_file: str):
     )
 
 
-def _plot_map_features(map_features: Dict, feature_ids: List[str]) -> List[Line2D]:
-    """Plot the map features with some feature_ids highlighted and return extended legend handles"""
-    handles = []
+def _plot_map_features(map_features: Dict):
     for lane in map_features["lane"]:
         pts = np.array([[p.x, p.y] for p in lane[0].polyline])
-        if lane[1] in feature_ids:
-            plt.plot(pts[:, 0], pts[:, 1], linestyle=":", color="blue", linewidth=2.0)
-            handles.append(
-                Line2D(
-                    [0],
-                    [0],
-                    linestyle=":",
-                    color="blue",
-                    label=f"Lane Polyline {lane[1]}",
-                ),
-            )
-        else:
-            plt.plot(xs, ys, linestyle=":", color="gray")
+        plt.plot(pts[:, 0], pts[:, 1], linestyle=":", color="gray")
 
     for road_line in map_features["road_line"]:
         pts = np.array([[p.x, p.y] for p in road_line[0].polyline])
         xs, ys = pts[:, 0], pts[:, 1]
         if road_line[0].type in [1, 4, 5]:
-            if road_line[1] in feature_ids:
-                plt.plot(xs, ys, "b--", linewidth=2.0)
-                handles.append(
-                    Line2D(
-                        [0],
-                        [0],
-                        linestyle="--",
-                        color="blue",
-                        label=f"Single Road Line {road_line[1]}",
-                    )
-                )
-            else:
-                plt.plot(xs, ys, "y--")
+            plt.plot(pts[:, 0], pts[:, 1], "y--")
         else:
-            if road_line[1] in feature_ids:
-                plt.plot(xs, ys, "b-", linewidth=2.0)
-                handles.append(
-                    Line2D(
-                        [0],
-                        [0],
-                        linestyle="-",
-                        color="blue",
-                        label=f"Double Road Line {road_line[1]}",
-                    )
-                )
-            else:
-                plt.plot(xs, ys, "y-")
+            plt.plot(pts[:, 0], pts[:, 1], "y-")
 
     for road_edge in map_features["road_edge"]:
         pts = np.array([[p.x, p.y] for p in road_edge[0].polyline])
-        xs, ys = pts[:, 0], pts[:, 1]
-        if road_edge[1] in feature_ids:
-            plt.plot(xs, ys, "b-", linewidth=2.0)
-            handles.append(
-                Line2D(
-                    [0],
-                    [0],
-                    linestyle="-",
-                    color="blue",
-                    label=f"Road Edge {road_edge[1]}",
-                )
-            )
-        else:
-            plt.plot(xs, ys, "k-")
+        plt.plot(pts[:, 0], pts[:, 1], "k-")
 
     for crosswalk in map_features["crosswalk"]:
-        pts = np.array([[p.x, p.y] for p in crosswalk[0].polyline])
-        xs, ys = pts[:, 0], pts[:, 1]
-        if crosswalk[1] in feature_ids:
-            plt.plot(xs, ys, "b--", linewidth=2.0)
-            handles.append(
-                Line2D(
-                    [0],
-                    [0],
-                    linestyle="--",
-                    color="blue",
-                    label=f"Crosswalk {crosswalk[1]}",
-                )
-            )
-        else:
-            plt.plot(xs, ys, "k--")
+        poly_points = [[p.x, p.y] for p in crosswalk[0].polygon]
+        poly_points.append(poly_points[0])
+        pts = np.array(poly_points)
+        plt.plot(pts[:, 0], pts[:, 1], "k--")
 
     for speed_bump in map_features["speed_bump"]:
-        pts = np.array([[p.x, p.y] for p in speed_bump[0].polyline])
-        xs, ys = pts[:, 0], pts[:, 1]
-        if speed_bump[1] in feature_ids:
-            plt.plot(xs, ys, "b:", linewidth=2.0)
-            handles.append(
-                Line2D(
-                    [0],
-                    [0],
-                    linestyle=":",
-                    color="blue",
-                    label=f"Speed Bump {speed_bump[1]}",
-                )
-            )
-        else:
-            plt.plot(xs, ys, "k:")
+        poly_points = [[p.x, p.y] for p in speed_bump[0].polygon]
+        poly_points.append(poly_points[0])
+        pts = np.array(poly_points)
+        plt.plot(pts[:, 0], pts[:, 1], "k:")
 
     for stop_sign in map_features["stop_sign"]:
-        if stop_sign[1] in feature_ids:
-            s_color = "blue"
-            handles.append(
-                Line2D(
-                    [],
-                    [],
-                    color="blue",
-                    marker="o",
-                    markersize=5,
-                    label=f"Stop Sign {stop_sign[1]}",
-                )
-            )
-        else:
-            s_color = "#ff0000"
         plt.scatter(
             stop_sign[0].position.x,
             stop_sign[0].position.y,
             marker="o",
-            c=s_color,
+            c="red",
             alpha=1,
         )
-    return handles
+
+
+def _get_map_features(scenario: scenario_pb2.Scenario) -> Dict[str, List]:
+    map_features = defaultdict(lambda: [])
+    for i in range(len(scenario.map_features)):
+        map_feature = scenario.map_features[i]
+        key = map_feature.WhichOneof("feature_data")
+        if key is not None:
+            map_features[key].append((getattr(map_feature, key), str(map_feature.id)))
+    return map_features
 
 
 @waymo_cli.command(
@@ -196,20 +138,98 @@ def _plot_map_features(map_features: Dict, feature_ids: List[str]) -> List[Line2
 )
 @click.argument("scenario_id", type=str, metavar="<scenario_id>")
 @click.option(
-    "--animate",
+    "--animate-trajectories",
     is_flag=True,
     default=False,
-    help="Animate the vehicle trajectories instead of plotting them.",
+    help="Animate the vehicle trajectories.",
 )
-def preview(tfrecord_file: str, scenario_id: str, animate: bool):
-    spec = MapSpec(f"{tfrecord_file}#{scenario_id}")
-    map = WaymoMap.from_spec(spec)
-    assert map is not None, "Invalid tfrecord file or scenario id"
-    scenario = WaymoMap.parse_source_to_scenario(spec.source)
+@click.option(
+    "--plot-trajectories",
+    is_flag=True,
+    default=False,
+    help="Plot the vehicle trajectories.",
+)
+def preview(
+    tfrecord_file: str,
+    scenario_id: str,
+    animate_trajectories: bool,
+    plot_trajectories: bool,
+):
+    source = f"{tfrecord_file}#{scenario_id}"
+    scenario = WaymoMap.parse_source_to_scenario(source)
+    fig = plt.figure()
+    mng = plt.get_current_fig_manager()
+    mng.resize(1000, 1000)
+    map_features = _get_map_features(scenario)
+    if animate_trajectories:
+        pass  # TODO
+    if plot_trajectories:
+        pass  # TODO
+    _plot_map_features(map_features)
+    plt.title(f"Scenario {scenario_id}")
+    plt.legend(handles=MAP_HANDLES)
+    plt.axis("equal")
+    plt.show()
 
-    # fig = plt.figure()
-    # highlighted_handles = _plot_map_features(map_features, [])
-    # plt.title(f"Scenario {scenario_id}")
+
+def _scenario_code(dataset_path, scenario_id):
+    return f"""from pathlib import Path
+from smarts.sstudio import gen_scenario
+from smarts.sstudio import types as t
+dataset_path = "{dataset_path}"
+scenario_id = "{scenario_id}"
+traffic_histories = [
+    t.TrafficHistoryDataset(
+        name=f"waymo",
+        source_type="Waymo",
+        input_path=dataset_path,
+        scenario_id=scenario_id,
+    )
+]
+gen_scenario(
+    t.Scenario(
+        map_spec=t.MapSpec(
+            source=f"{{dataset_path}}#{{scenario_id}}", lanepoint_spacing=1.0
+        ),
+        traffic_histories=traffic_histories,
+    ),
+    output_dir=Path(__file__).parent,
+)
+"""
+
+
+@waymo_cli.command(
+    name="export", help="Export the Waymo scenario to a SMARTS scenario."
+)
+@click.argument(
+    "tfrecord_file", type=click.Path(exists=True), metavar="<tfrecord_file>"
+)
+@click.argument("scenario_id", type=str, metavar="<scenario_id>")
+@click.argument(
+    "export_folder", type=click.Path(exists=False), metavar="<export_folder>"
+)
+@click.option(
+    "--generate-offline-data",
+    is_flag=True,
+    default=False,
+    help="Extract observations and birds-eye view images for a selected set of vehicles for the scenario, for use in offline RL.",
+)
+def export(
+    tfrecord_file: str,
+    scenario_id: str,
+    export_folder: str,
+    generate_offline_data: bool,
+):
+    if not os.path.exists(export_folder):
+        os.makedirs(export_folder)
+    scenario_file = os.path.join(export_folder, "scenario.py")
+    with open(scenario_file, "w") as f:
+        f.write(_scenario_code(tfrecord_file, scenario_id))
+
+    if generate_offline_data:
+        pass  # TODO
 
 
 waymo_cli.add_command(overview)
+waymo_cli.add_command(preview)
+waymo_cli.add_command(export)
