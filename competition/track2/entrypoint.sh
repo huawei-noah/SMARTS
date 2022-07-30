2f15ad1a75e792370762d25291508bf69062071d
adaickalavan <adaickalavan@gmail.com>
2022-07-30 12:15:41 -0400

    Maintain same directory path in repo and docker.

--- competition/track2/entrypoint.sh
+++ competition/track2/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # entrypoint.sh
 # [Do Not Modify]
-python3.8 /track2/train/train.py
-zip -j -r /output/train.zip /track2/train
-zip -j -r /output/submission.zip /track2/submission
+python3.8 /SMARTS/competition/track2/train/train.py
+zip -j -r /SMARTS/competition/output/train.zip /SMARTS/competition/track2/train
+zip -j -r /SMARTS/competition/output/submission.zip /SMARTS/competition/track2/submission
