f275700fd6ff41b6173957114163894a7d2cb17f
Tucker <montgomery.alban@huawei.com>
2022-07-31 19:11:20 -0400

    Black format

--- competition/track2/train/train_IL.py
+++ competition/track2/train/train_IL.py
@@ -16,15 +16,16 @@ from utility_IL import load_data
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-data_transform = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.ToTensor(),
-]) 
+data_transform = transforms.Compose(
+    [
+        transforms.ToPILImage(),
+        transforms.ToTensor(),
+    ]
+)
+
 
 def create_datasets(dataset_path, save_path=None, cache=False, batch_size=32):
-
     class TensorDatasetTransforms(torch.utils.data.TensorDataset):
-
         def __init__(self, x, y):
             super().__init__(x, y)
 
@@ -32,55 +33,52 @@ def create_datasets(dataset_path, save_path=None, cache=False, batch_size=32):
             tensor = data_transform(self.tensors[0][index])
 
             return (tensor,) + tuple(t[index] for t in self.tensors[1:])
-    
+
     class CustomTensorDataset(Dataset):
-        
         def __init__(self, x, y):
             tensors = (x, y)
             self.tensors = tensors
-        
+
         def __getitem__(self, index):
             img = data_transform(self.tensors[0][0][index])
             goal = self.tensors[0][1][index]
             y = self.tensors[1][index]
 
             return [img, goal], y
-        
+
         def __len__(self):
 
             return self.tensors[0][1].size(0)
-        
+
     x, y = load_data(dataset_path, save_path, cache=cache)
-    
-    
+
     x_train = x[:]
     y_train = y[:]
     img = np.array(x_train[:, 0].tolist())
     goal = np.array(x_train[:, 1].tolist())
     x_train = [torch.tensor(img), torch.tensor(goal)]
 
-    train_set = CustomTensorDataset(
-        x_train,
-        torch.tensor(y_train))
+    train_set = CustomTensorDataset(x_train, torch.tensor(y_train))
 
-    train_loader = torch.utils.data.DataLoader(train_set,
-                                               batch_size=batch_size,
-                                               shuffle=True,
-                                               num_workers=2)
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=batch_size, shuffle=True, num_workers=2
+    )
 
     return train_loader
+
 
 """
 Model
 """
+
+
 class Flatten(nn.Module):
-    
     def forward(self, x):
 
         return x.view(x.size()[0], -1)
 
-class ConvBlock(nn.Module):
 
+class ConvBlock(nn.Module):
     def __init__(self):
         super(ConvBlock, self).__init__()
 
@@ -99,18 +97,18 @@ class ConvBlock(nn.Module):
         self.linear2 = torch.nn.Linear(128, 32)
 
     def forward(self, x):
-        x = self.conv1(x) 
-        x = self.bachnorm1(x) 
-        x = self.elu(x) 
-        x = self.dropout2d(x) 
+        x = self.conv1(x)
+        x = self.bachnorm1(x)
+        x = self.elu(x)
+        x = self.dropout2d(x)
 
-        x = self.conv2(x) 
-        x = self.batchnorm2(x) 
-        x = self.elu(x) 
-        x = self.dropout2d(x) 
-        
-        x = self.conv3(x) 
-        x = self.elu(x) 
+        x = self.conv2(x)
+        x = self.batchnorm2(x)
+        x = self.elu(x)
+        x = self.dropout2d(x)
+
+        x = self.conv3(x)
+        x = self.elu(x)
         x = self.flatten(x)
         x = self.batchnorm3(x)
         x = self.dropout(x)
@@ -125,8 +123,8 @@ class ConvBlock(nn.Module):
 
         return x
 
-class EmbBlock(nn.Module):
 
+class EmbBlock(nn.Module):
     def __init__(self):
         super(EmbBlock, self).__init__()
 
@@ -139,13 +137,13 @@ class EmbBlock(nn.Module):
 
         return x
 
-class MainNet(nn.Module):
 
+class MainNet(nn.Module):
     def __init__(self):
         super(MainNet, self).__init__()
         self.convblock = ConvBlock()
         self.embblock = EmbBlock()
-        
+
         self.linear1 = torch.nn.Linear(64, 32)
         self.linear4 = torch.nn.Linear(32, 1)
         self.linear2 = torch.nn.Linear(64, 32)
@@ -156,14 +154,13 @@ class MainNet(nn.Module):
         self.elu = torch.nn.ELU()
 
         self.criterion = nn.MSELoss()
-       
 
     def forward(self, x):
         img_emb = self.convblock(x[0])
         goal_emb = self.embblock(x[1])
-        
+
         x = torch.cat((img_emb, goal_emb), dim=1)
-        
+
         dx = self.linear1(x)
         dy = self.linear2(x)
         dheading = self.linear3(x)
@@ -173,27 +170,42 @@ class MainNet(nn.Module):
         dheading = self.elu(dheading)
 
         nn_outputs = {}
-        nn_outputs['dx'] = self.linear4(dx)
-        nn_outputs['dy'] = self.linear5(dy)
-        nn_outputs['d_heading'] = self.linear6(dheading)
+        nn_outputs["dx"] = self.linear4(dx)
+        nn_outputs["dy"] = self.linear5(dy)
+        nn_outputs["d_heading"] = self.linear6(dheading)
 
         return nn_outputs
-    
+
     def compute_loss(self, nn_outputs, labels):
-        dx_loss = self.criterion(nn_outputs['dx'], labels[:,0].view(labels.shape[0], 1))
-        dy_loss = self.criterion(nn_outputs['dy'], labels[:,1].view(labels.shape[0], 1))
-        dh_loss = self.criterion(nn_outputs['d_heading'], labels[:,2].view(labels.shape[0], 1))
+        dx_loss = self.criterion(
+            nn_outputs["dx"], labels[:, 0].view(labels.shape[0], 1)
+        )
+        dy_loss = self.criterion(
+            nn_outputs["dy"], labels[:, 1].view(labels.shape[0], 1)
+        )
+        dh_loss = self.criterion(
+            nn_outputs["d_heading"], labels[:, 2].view(labels.shape[0], 1)
+        )
         total_loss = dx_loss + dy_loss + dh_loss
 
         return total_loss, dx_loss, dy_loss, dh_loss
 
-def save_losses(train_total_losses, train_total_loss, 
-                train_dx_losses, train_dx_loss, 
-                train_dy_losses, train_dy_loss, 
-                test_total_losses, test_total_loss,
-                test_dx_losses, test_dx_loss, 
-                test_dy_losses ,test_dy_loss):
-    
+
+def save_losses(
+    train_total_losses,
+    train_total_loss,
+    train_dx_losses,
+    train_dx_loss,
+    train_dy_losses,
+    train_dy_loss,
+    test_total_losses,
+    test_total_loss,
+    test_dx_losses,
+    test_dx_loss,
+    test_dy_losses,
+    test_dy_loss,
+):
+
     train_total_losses.append(train_total_loss)
     train_dx_losses.append(train_dx_loss)
     train_dy_losses.append(train_dy_loss)
@@ -201,9 +213,26 @@ def save_losses(train_total_losses, train_total_loss,
     test_dx_losses.append(test_dx_loss)
     test_dy_losses.append(test_dy_loss)
 
-    return train_total_losses, train_dx_losses, train_dy_losses, test_total_losses, test_dx_losses, test_dy_losses
+    return (
+        train_total_losses,
+        train_dx_losses,
+        train_dy_losses,
+        test_total_losses,
+        test_dx_losses,
+        test_dy_losses,
+    )
 
-def train(model, dataset_path, checkpoint_path, cache=False, lr=0.001, num_epochs=100, batch_size=32, save_steps=30):
+
+def train(
+    model,
+    dataset_path,
+    checkpoint_path,
+    cache=False,
+    lr=0.001,
+    num_epochs=100,
+    batch_size=32,
+    save_steps=30,
+):
     """
     Training main method
     :param model: the network
@@ -217,51 +246,76 @@ def train(model, dataset_path, checkpoint_path, cache=False, lr=0.001, num_epoch
 
     loss_function = nn.MSELoss(reduction="mean")
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    
-    train_loader = create_datasets(dataset_path, save_path=checkpoint_path, cache=cache, batch_size=batch_size)  # read datasets
-    
+
+    train_loader = create_datasets(
+        dataset_path, save_path=checkpoint_path, cache=cache, batch_size=batch_size
+    )  # read datasets
+
     # train
     save_step = 0
-    
+
     for epoch in range(num_epochs):
         save_step += 1
-        print('Epoch {}/{}'.format(epoch + 1, num_epochs))
+        print("Epoch {}/{}".format(epoch + 1, num_epochs))
         epochs.append(epoch)
-        train_total_loss, train_dx_loss, train_dy_loss = train_epoch(model, loss_function, optimizer, train_loader)
+        train_total_loss, train_dx_loss, train_dy_loss = train_epoch(
+            model, loss_function, optimizer, train_loader
+        )
         test_total_loss = 0
         test_dx_loss = 0
         test_dy_loss = 0
 
-        train_total_losses, train_dx_losses, train_dy_losses, test_total_losses, test_dx_losses, test_dy_losses = save_losses(train_total_losses, train_total_loss, 
-                                                                                                                                train_dx_losses, train_dx_loss, 
-                                                                                                                                train_dy_losses, train_dy_loss, 
-                                                                                                                                test_total_losses, test_total_loss,
-                                                                                                                                test_dx_losses, test_dx_loss, 
-                                                                                                                                test_dy_losses ,test_dy_loss)
-        plt.ylim(0, 1)                                                                                                                        
+        (
+            train_total_losses,
+            train_dx_losses,
+            train_dy_losses,
+            test_total_losses,
+            test_dx_losses,
+            test_dy_losses,
+        ) = save_losses(
+            train_total_losses,
+            train_total_loss,
+            train_dx_losses,
+            train_dx_loss,
+            train_dy_losses,
+            train_dy_loss,
+            test_total_losses,
+            test_total_loss,
+            test_dx_losses,
+            test_dx_loss,
+            test_dy_losses,
+            test_dy_loss,
+        )
+        plt.ylim(0, 1)
         plt.plot(epochs, train_total_losses)
         plt.xlabel("Epoch")
         plt.ylabel("Loss")
         plt.title("Train Loss")
         plt.savefig("train_losses")
-        plt.clf()   
+        plt.clf()
 
         # save model
         if save_step == save_steps:
             save_step = 0
-            torch.save({
-                        'epoch': epoch,
-                        'model_state_dict': model.state_dict(),
-                        'optimizer_state_dict': optimizer.state_dict(),
-                        'train_loss_history': train_total_loss,
-                        }, os.path.join(checkpoint_path, "ckpt_{}".format(epoch)))
-    
-    torch.save({
-                'epoch': epoch,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'train_loss_history': train_total_loss,
-                },os.path.join(checkpoint_path, "ckpt_{}".format(num_epochs)))
+            torch.save(
+                {
+                    "epoch": epoch,
+                    "model_state_dict": model.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                    "train_loss_history": train_total_loss,
+                },
+                os.path.join(checkpoint_path, "ckpt_{}".format(epoch)),
+            )
+
+    torch.save(
+        {
+            "epoch": epoch,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "train_loss_history": train_total_loss,
+        },
+        os.path.join(checkpoint_path, "ckpt_{}".format(num_epochs)),
+    )
 
 
 def train_epoch(model, loss_function, optimizer, data_loader):
@@ -285,7 +339,7 @@ def train_epoch(model, loss_function, optimizer, data_loader):
             outputs = model(inputs)
             labels = labels.float()
             loss, dx_l, dy_l, dh_l = model.compute_loss(outputs, labels)
-            
+
             # backward
             loss.backward()
             optimizer.step()
@@ -299,17 +353,14 @@ def train_epoch(model, loss_function, optimizer, data_loader):
     total_dx_loss = dx_losses / len(data_loader.dataset)
     total_dy_loss = dy_losses / len(data_loader.dataset)
     total_dh_loss = dh_losses / len(data_loader.dataset)
-  
-    print('Train Loss: {:.4f}'
-          .format(total_loss))
-    print('Train dx ;oss: {:.4f}'
-          .format(total_dx_loss))
-    print('Train dy loss: {:.4f}'
-          .format(total_dy_loss))
-    print('Train dh loss: {:.4f}'
-          .format(total_dh_loss))      
-    
+
+    print("Train Loss: {:.4f}".format(total_loss))
+    print("Train dx ;oss: {:.4f}".format(total_dx_loss))
+    print("Train dy loss: {:.4f}".format(total_dy_loss))
+    print("Train dh loss: {:.4f}".format(total_dh_loss))
+
     return total_loss, total_dx_loss, total_dy_loss
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -357,10 +408,20 @@ def main():
     )
     args = parser.parse_args()
 
-    print('Training...')
+    print("Training...")
     m = MainNet()
-    train(m, args.dataset_path, args.output_path, cache=args.cache, lr=args.learning_rate, num_epochs=args.num_epochs, batch_size=args.batch_size, save_steps=args.save_steps)
-    print('Training Done.')
+    train(
+        m,
+        args.dataset_path,
+        args.output_path,
+        cache=args.cache,
+        lr=args.learning_rate,
+        num_epochs=args.num_epochs,
+        batch_size=args.batch_size,
+        save_steps=args.save_steps,
+    )
+    print("Training Done.")
+
 
 if __name__ == "__main__":
     main()
--- competition/track2/train/utility_IL.py
+++ competition/track2/train/utility_IL.py
@@ -11,9 +11,11 @@ import argparse
 
 torch.cuda.empty_cache()
 
+
 def dheading(heading_l, heading_2):
 
     return (heading_l - heading_2 + math.pi) % (2 * math.pi) - math.pi
+
 
 def rotate_points(x, y, heading):
     if heading < 0:
@@ -22,18 +24,20 @@ def rotate_points(x, y, heading):
         angle = 2 * math.pi - heading
     else:
         angle = 0
-        
+
     new_x = (x * math.cos(angle)) - (y * math.sin(angle))
     new_y = (y * math.cos(angle)) + (x * math.sin(angle))
 
     return new_x, new_y
 
+
 def load_from_npy(data_path):
-    with open(os.path.join(data_path, 'dataset.npy'), 'rb') as f:
+    with open(os.path.join(data_path, "dataset.npy"), "rb") as f:
         x = np.load(f, allow_pickle=True)
         y = np.load(f, allow_pickle=True)
 
     return x, y
+
 
 def load_data_scratch(dataset_path, save_path):
     path = dataset_path
@@ -51,53 +55,62 @@ def load_data_scratch(dataset_path, save_path):
     for scenario_name in os.listdir(path):
         scenarios.append(scenario_name)
 
-    x, y = rotate_points(1, 1, (-1*math.pi/4))
-    
+    x, y = rotate_points(1, 1, (-1 * math.pi / 4))
+
     for scenario in scenarios:
         vehicle_ids = []
         for filename in os.listdir(os.path.join(path, scenario)):
-            if filename.endswith('.png'):
-                match = re.search('vehicle-(.*).png', filename)
+            if filename.endswith(".png"):
+                match = re.search("vehicle-(.*).png", filename)
                 if match is None:
-                    raise ValueError(f"Name matching does not match in filename: {filename}")
+                    raise ValueError(
+                        f"Name matching does not match in filename: {filename}"
+                    )
                 vehicle_id = match.group(1)
                 if vehicle_id not in vehicle_ids:
                     vehicle_ids.append(vehicle_id)
 
         for id in vehicle_ids:
-            with open(os.path.join(path, scenario) +  '/Agent-history-vehicle-' + id + '.pkl', 'rb') as f:
+            with open(
+                os.path.join(path, scenario) + "/Agent-history-vehicle-" + id + ".pkl",
+                "rb",
+            ) as f:
                 vehicle_data = pickle.load(f)
-            
+
             image_names = []
 
             for filename in os.listdir(os.path.join(path, scenario)):
-                if filename.endswith("-" + id + '.png'):
+                if filename.endswith("-" + id + ".png"):
                     image_names.append(filename)
 
             image_names = sorted(image_names)
             prev_dheading = 0
 
             for i in range(len(image_names) - 1):
-                image = Image.open(os.path.join(path, scenario) + '/' + image_names[i])
+                image = Image.open(os.path.join(path, scenario) + "/" + image_names[i])
                 obs.append([np.moveaxis(np.asarray(image), -1, 0)])
-                sim_time = image_names[i].split('_Agent')[0]
-                sim_time_next = image_names[i + 1].split('_Agent')[0]
-                last_sim_time = image_names[-1].split('_Agent')[0]
+                sim_time = image_names[i].split("_Agent")[0]
+                sim_time_next = image_names[i + 1].split("_Agent")[0]
+                last_sim_time = image_names[-1].split("_Agent")[0]
 
                 try:
-                    current_position = vehicle_data[float(sim_time)]['ego']['pos']
-                    next_position = vehicle_data[float(sim_time_next)]['ego']['pos']
-                    current_heading = vehicle_data[float(sim_time)]['ego']['heading']
-                    next_heading = vehicle_data[float(sim_time_next)]['ego']['heading']
-                    goal_location = vehicle_data[float(last_sim_time)]['ego']['pos']
+                    current_position = vehicle_data[float(sim_time)]["ego"]["pos"]
+                    next_position = vehicle_data[float(sim_time_next)]["ego"]["pos"]
+                    current_heading = vehicle_data[float(sim_time)]["ego"]["heading"]
+                    next_heading = vehicle_data[float(sim_time_next)]["ego"]["heading"]
+                    goal_location = vehicle_data[float(last_sim_time)]["ego"]["pos"]
 
                 except:
                     print("scenario ", scenario)
                     print("id ", id)
                     print(float(sim_time))
 
-                new_current_x, new_current_y = rotate_points(current_position[0], current_position[1], current_heading)
-                new_next_x, new_next_y = rotate_points(next_position[0], next_position[1], current_heading)
+                new_current_x, new_current_y = rotate_points(
+                    current_position[0], current_position[1], current_heading
+                )
+                new_next_x, new_next_y = rotate_points(
+                    next_position[0], next_position[1], current_heading
+                )
                 dx = new_next_x - new_current_x
                 dy = new_next_y - new_current_y
                 d_heading = dheading(current_heading, next_heading) * 100
@@ -109,13 +122,15 @@ def load_data_scratch(dataset_path, save_path):
 
                 prev_dheading = d_heading
 
-                rotated_goal_location_x, rotated_goal_location_y = rotate_points(goal_location[0], goal_location[1], current_heading)
+                rotated_goal_location_x, rotated_goal_location_y = rotate_points(
+                    goal_location[0], goal_location[1], current_heading
+                )
                 dx_goal = rotated_goal_location_x - new_current_x
                 dy_goal = rotated_goal_location_y - new_current_y
 
                 obs[-1].append(np.array([dx_goal, dy_goal]))
                 actions.append([dx, dy, d_heading])
-    
+
     obs = np.array(obs)
     actions = np.array(actions)
 
@@ -136,14 +151,15 @@ def load_data_scratch(dataset_path, save_path):
     print("== Loading data finished ==")
     print("state dimension = ", obs.shape)
     print("action dimension = ", actions.shape)
-    
-    with open(os.path.join(save_path, 'dataset.npy'), 'wb') as f:
+
+    with open(os.path.join(save_path, "dataset.npy"), "wb") as f:
         np.save(f, obs)
         np.save(f, actions)
-    
+
     print("Data cache successfully saved")
 
     return obs, actions
+
 
 def load_data(dataset_path, save_path, cache=False):
     if cache is False:
