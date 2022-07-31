69595c50536ecd5b3f1de60b267211d73c1e8574
Tucker Alban <mtuck2005@yahoo.ca>
2022-07-31 19:33:44 -0400

    Revert "Offline baseline 2 (#1533)"
    This reverts commit f5f8385ce14b1099d1c563d27e4ab5d363796ade.

--- competition/track2/submission/policy.py
+++ competition/track2/submission/policy.py
@@ -1,6 +1,4 @@
 from typing import Any, Dict
-import numpy as np
-from utility import get_goal_layer, global_target_pose
 
 
 class BasePolicy:
@@ -41,14 +39,8 @@ class Policy(BasePolicy):
         performed here. To be implemented by the user.
         """
 
-        from d3rlpy.algos import CQL
-
-        self.model = CQL.from_json(
-            Path(__file__).absolute().parents[0] / "model/params.json", use_gpu=True
-        )
-        self.model.load_model(
-            Path(__file__).absolute().parents[0] / "model/model_100.pt"
-        )
+        # Load saved model and instantiate any needed objects.
+        self.model = ...
 
     def act(self, obs: Dict[str, Any]):
         """Act function to be implemented by user.
@@ -59,25 +51,9 @@ class Policy(BasePolicy):
         Returns:
             Dict[str, Any]: A dictionary of actions for each ego agent.
         """
-
         wrapped_act = {}
         for agent_id, agent_obs in obs.items():
-            bev_obs = np.moveaxis(agent_obs["rgb"], -1, 0)
-
-            goal_x = agent_obs["mission"]["goal_pos"][0]
-            goal_y = agent_obs["mission"]["goal_pos"][1]
-            current_x = agent_obs["ego"]["pos"][0]
-            current_y = agent_obs["ego"]["pos"][1]
-            current_heading = agent_obs["ego"]["heading"]
-            goal_obs = get_goal_layer(
-                goal_x, goal_y, current_x, current_y, current_heading
-            )
-
-            final_obs = list()
-            final_obs.append(np.concatenate((bev_obs, goal_obs), axis=0))
-            final_obs = np.array(final_obs, dtype=np.uint8)
-            action = self.model.predict(final_obs)[0]
-            print(action)
-            target_pose = global_target_pose(action, agent_obs)
-            wrapped_act.update({agent_id: target_pose})
+            # Use saved model to predict multi-agent action output given multi-agent SMARTS observation input.
+            action = ...
+            wrapped_act.update({agent_id: action})
         return wrapped_act
---
+++ competition/track2/train/train.py
@@ -0,0 +1,2 @@
+# Add offline training code for Track-2 here.
+# On completion of training, automatically save the trained model to `track2/submission` directory.
