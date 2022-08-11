16aefc0b908737aa3c35ee92f2a147f7feb9af34
adaickalavan <adaickalavan@gmail.com>
2022-08-11 15:41:22 -0400

    Update zipping of folder.

--- competition/track2/entrypoint.sh
+++ competition/track2/entrypoint.sh
@@ -2,5 +2,7 @@
 # entrypoint.sh
 # [Do Not Modify]
 python3.8 /SMARTS/competition/track2/train/train.py
-zip -j -r /SMARTS/competition/output/train.zip /SMARTS/competition/track2/train
-zip -j -r /SMARTS/competition/output/submission.zip /SMARTS/competition/track2/submission
+cd /SMARTS/competition/track2
+zip -r /SMARTS/competition/output/train.zip ./train
+zip -r /SMARTS/competition/output/submission.zip ./submission
+cd -
