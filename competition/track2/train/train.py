21bdba3a8ab26f5d843068a2c506d82598c0fb5e
alfredgu001324 <89165526+alfredgu001324@users.noreply.github.com>
2022-09-15 10:41:49 -0400

    Update competition/track2/train/train.py
    Co-authored-by: adai <adaickalavan@gmail.com>

--- competition/track2/train/train.py
+++ competition/track2/train/train.py
@@ -86,10 +86,8 @@ def train(input_path, output_path):
         for id in vehicle_ids[0:n_vehicles]:
             print(f"Adding data for vehicle id {id} in scenario {scenario}.")
 
-            for pkl_file in os.listdir(scenario_path):
-                if pkl_file == (id + '.pkl'):
-                    with open(scenario_path / pkl_file, "rb") as f:
-                        vehicle_data = pickle.load(f)
+            with open(scenario_path / f"{id}.pkl", "rb") as f:
+                vehicle_data = pickle.load(f)
             image_names = list()
             for filename in os.listdir(scenario_path):
                 if filename.endswith(f"{id}.png"):
