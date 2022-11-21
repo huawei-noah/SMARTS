53bf705e571ae748d15d49c949db98e4ade465a5
adaickalavan <adaickalavan@gmail.com>
2022-11-21 18:02:28 -0500

    Ignore reports and improved graph plot.

--- smarts/benchmark/run.py
+++ smarts/benchmark/run.py
@@ -19,6 +19,7 @@
 # THE SOFTWARE.
 
 import logging
+import matplotlib.pyplot as plt
 import platform
 import subprocess
 from dataclasses import dataclass
@@ -86,9 +87,7 @@ def _compute(scenario_dir, ep_per_scenario=5, max_episode_steps=_MAX_EPISODE_STE
     records = {}
     for k, v in results.items():
         parsed_name = k.split("benchmark/")[1]
-        records[parsed_name] = _readable(
-            func=v, num_episodes=num_episodes, num_steps=num_episode_steps[k]
-        )
+        records[parsed_name] = _readable(func=v)
 
     return records
 
@@ -98,12 +97,12 @@ class _Funcs:
     update: Callable[[float], None]
     mean: Callable[[], float]
     std: Callable[[], float]
+    steps: int
 
 
 @dataclass
 class _Result:
-    num_episodes: int
-    num_steps: int
+    steps: int
     mean: float
     std: float
 
@@ -140,22 +139,25 @@ def welford() -> Tuple[
         std = math.sqrt(S / (n - 1))
         return std
 
-    return update, mean, std
+    def steps() -> int:
+        return n
+
+    return update, mean, std, steps
 
 
 def _get_funcs() -> _Funcs:
-    update, mean, std = welford()
+    update, mean, std, steps = welford()
     return _Funcs(
         update=lambda x: update(1000 / x),  # Steps per sec. Units: step/s
         mean=mean,
         std=std,
+        steps=steps,
     )
 
 
-def _readable(func: _Funcs, num_episodes: int, num_steps: int) -> _Result:
+def _readable(func: _Funcs) -> _Result:
     return _Result(
-        num_episodes=num_episodes,
-        num_steps=num_steps,
+        steps=func.steps(),
         mean=func.mean(),
         std=func.std(),
     )
@@ -178,12 +180,12 @@ def git_branch() -> str:
 
 
 def _write_report(results: Dict[str, Any]):
-    dir = Path(__file__).resolve().parent / "reports"
-    dir.mkdir(exist_ok=True)
     datetime_now = datetime.now()
-    filename = datetime_now.strftime("%d_%m_%Y_%H_%M_%S")
+    folder = datetime_now.strftime("%Y_%m_%d_%H_%M_%S")
+    dir = Path(__file__).resolve().parent / f"reports" / folder
+    dir.mkdir(parents=True, exist_ok=True)
 
-    mdFile = MdUtils(file_name=str(dir / filename), title="Benchmark Report")
+    mdFile = MdUtils(file_name=str(dir / "Report"), title="Benchmark Report")
     mdFile.write(f"SMARTS version: {smarts.VERSION}\n\n")
     mdFile.write(f"Date & Time: {datetime_now.strftime('%d/%m/%Y %H:%M:%S')}\n\n")
     mdFile.write(f"Branch: {git_branch()}\n\n")
@@ -194,6 +196,40 @@ def _write_report(results: Dict[str, Any]):
     )
     mdFile.write(
         f"RAM: {str(round(psutil.virtual_memory().total / (1024.0 **3)))+' GB'}\n\n"
+    )
+
+    mdFile.new_header(level=2, title="Function", add_table_of_contents="n")
+    mdFile.write("- Track performance of SMARTS in each version.\n")
+    mdFile.write("- Test effects of improvements or optimizations.\n")
+
+    means = []
+    stds=[]
+    scenarios=[]
+    content = ["Scenario(s)", "Total Time Steps", "Mean (steps/sec)", "Std (steps/sec)"]
+    for scenario, data in results.items():
+        scenarios.append(scenario)
+        means.append(data.mean)
+        stds.append(data.std)
+        content.extend(
+            [
+                f"{scenario}",
+                f"{data.steps}",
+                f"{data.mean:.2f}",
+                f"{data.std:.2f}",
+            ]
+        )
+
+    mdFile.new_header(level=2, title="Result", add_table_of_contents="n")
+    mdFile.new_table(columns=4, rows=len(list(results.keys())) + 1, text=content)
+    plt.plot(scenarios, means)
+    plt.errorbar(scenarios, means, stds, marker="o", capsize=3)
+    plt.xlabel("Scenario")
+    plt.ylabel("Steps / Sec")
+    plt.savefig(dir / "Fig1.png")
+    mdFile.new_paragraph(
+        "<figure>"
+        f"\n<img src={dir/'Fig1.png'} alt='line chart' style='width:500px;'/>"
+        "\n</figure>"
     )
 
     mdFile.create_md_file()
