c6ca57518d1a7af13494a3e9b80cdb19f84c2a7e
qianyi-sun <sqy201x@hotmail.com>
2022-11-22 11:00:47 -0500

    remove unnecessary files

---
+++ smarts/benchmark/10_agents_to_n_roads/20_roads/scenario.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+
+from smarts.sstudio import gen_scenario
+from smarts.sstudio.types import Mission, Route, Scenario, SocialAgentActor,RandomRoute
+
+actors = [
+    SocialAgentActor(
+        name=f"non-interactive-agent-50-v0",
+        agent_locator="zoo.policies:non-interactive-agent-v0",
+        policy_kwargs={"speed": 50},
+    )
+]
+
+
+def to_missions(agent_num):
+    missions = {}
+    for i in range(0,agent_num):
+        missions[f'group-{i}']=tuple((actors, [Mission(route=RandomRoute())]),)
+    return missions
+
+gen_scenario(
+    Scenario(
+        social_agent_missions=to_missions(10)
+    ),
+    output_dir=Path(__file__).parent,
+)
