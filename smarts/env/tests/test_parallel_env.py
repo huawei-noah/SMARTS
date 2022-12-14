ab10960d0bdf084757db9c8ecbc3d4d037262731
adaickalavan <adaickalavan@gmail.com>
2022-12-14 15:16:01 -0500

    Remove adapters from parallel env.

--- smarts/env/tests/test_parallel_env.py
+++ smarts/env/tests/test_parallel_env.py
@@ -110,12 +110,13 @@ def test_seed(env_constructor, num_env):
     env.close()
 
 
-def _compare_observations(num_env, batched_observations, single_observations):
-    assert len(batched_observations) == num_env
-    for observations in batched_observations:
-        assert observations.keys() == single_observations.keys()
-        for agent_id, obs in observations.items():
-            assert type(obs) == type(single_observations[agent_id])
+def _compare_outputs(num_env, batched_outputs, single_outputs):
+    assert len(batched_outputs) == num_env
+    for outputs in batched_outputs:
+        outputs.pop("__all__", None)
+        assert outputs.keys() == outputs.keys()
+        for agent_id, out in outputs.items():
+            assert type(out) is type(single_outputs[agent_id])
 
 
 @pytest.mark.parametrize("num_env", [2])
@@ -128,7 +129,7 @@ def test_reset(env_constructor, num_env):
     batched_observations = env.reset()
     env.close()
 
-    _compare_observations(num_env, batched_observations, single_observations)
+    _compare_outputs(num_env, batched_observations, single_observations)
 
 
 @pytest.mark.parametrize("num_env", [2])
@@ -136,7 +137,7 @@ def test_reset(env_constructor, num_env):
 def test_step(env_constructor, single_env_actions, num_env, auto_reset):
     single_env = env_constructor()
     single_env.reset()
-    single_observations, _, _, _ = single_env.step(single_env_actions)
+    single_observations, single_rewards, single_dones, single_infos = single_env.step(single_env_actions)
     single_env.close()
 
     env = _make_parallel_env(env_constructor, num_env, auto_reset=auto_reset)
@@ -146,16 +147,11 @@ def test_step(env_constructor, single_env_actions, num_env, auto_reset):
     )
     env.close()
 
-    _compare_observations(num_env, batched_observations, single_observations)
-
-    for batched_outputs, kind in zip(
-        (batched_rewards, batched_dones, batched_infos), [float, bool, float]
+    for batched_outputs, single_outputs in zip(
+        [batched_observations, batched_rewards, batched_dones, batched_infos],
+        [single_observations, single_rewards, single_dones, single_infos]
     ):
-        assert len(batched_outputs) == num_env
-        for outputs in batched_outputs:
-            outputs.pop("__all__", None)
-            assert outputs.keys() == single_observations.keys()
-            assert all(isinstance(val, kind) for val in outputs.values())
+        _compare_outputs(num_env, batched_outputs, single_outputs)
 
 
 @pytest.mark.parametrize("auto_reset", [True, False])
