520f2cad4329375e9e3244d3245bd68f3d371f88
adaickalavan <adaickalavan@gmail.com>
2022-12-22 16:20:23 -0500

    Add header.

--- smarts/waymo/traffic_histories_to_observations.py
+++ smarts/waymo/traffic_histories_to_observations.py
@@ -1,3 +1,23 @@
+# Copyright (C) 2022. Huawei Technologies Co., Ltd. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import argparse
 import logging
 import math
@@ -10,7 +30,6 @@ import numpy as np
 from PIL import Image
 
 from envision.client import Client as Envision
-from smarts import sstudio
 from smarts.core import seed as smarts_seed
 from smarts.core.agent_interface import (
     OGM,
@@ -30,7 +49,7 @@ from smarts.core.scenario import Scenario
 from smarts.core.smarts import SMARTS
 from smarts.core.sumo_traffic_simulation import SumoTrafficSimulation
 from smarts.core.vehicle import VEHICLE_CONFIGS
-
+from smarts.sstudio.scenario_construction import build_scenario
 
 class ObservationRecorder:
     def __init__(
@@ -391,7 +410,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if not args.no_build:
-        sstudio.build_scenario([args.scenario])
+        build_scenario(clean=False, scenario=args.scenario, seed=args.seed)
 
     recorder = ObservationRecorder(
         scenario=args.scenario,
