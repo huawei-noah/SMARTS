f7f1b7129d1d5ba5c36e9fb31a5ebf830170dd24
Montgomery Alban <mtuck2005@yahoo.ca>
2022-12-31 00:35:57 +0000

    Split tests in two files to ignore baseline test.

--- smarts/core/tests/test_sensor_worker.py
+++ smarts/core/tests/test_sensor_worker.py
@@ -27,9 +27,8 @@ from smarts.core.agent_interface import AgentInterface, AgentType
 from smarts.core.controllers import ActionSpaceType
 from smarts.core.plan import Mission
 from smarts.core.scenario import Scenario
-from smarts.core.sensors import Sensors
+from smarts.core.sensors import Observation, SensorsWorker, WorkerKwargs
 from smarts.core.simulation_frame import SimulationFrame
-from smarts.core.simulation_local_constants import SimulationLocalConstants
 from smarts.core.smarts import SMARTS
 from smarts.core.sumo_traffic_simulation import SumoTrafficSimulation
 from smarts.core.utils.logging import diff_unpackable
@@ -91,42 +90,31 @@ def sim(scenario, request) -> SMARTS:
     smarts.destroy()
 
 
-def test_sensor_parallelization(
+def test_sensor_worker(
     sim: SMARTS,
 ):
-    import time
-
     del sim.cached_frame
     simulation_frame: SimulationFrame = sim.cached_frame
-    simulation_local_constants: SimulationLocalConstants = sim.local_constants
+    agent_ids = set(AGENT_IDS)
+    worker = SensorsWorker()
+    worker.run(sim_local_constants=sim.local_constants)
+    assert worker.running
+    worker_args = WorkerKwargs(sim_frame=simulation_frame)
+    worker.send_to_process(worker_args=worker_args, agent_ids=agent_ids)
+    observations, dones, updated_sensors = SensorsWorker.local(
+        simulation_frame, sim.local_constants, agent_ids
+    )
+    other_observations, other_dones, updated_sensors = worker.result(timeout=5)
 
-    def observe_with_processes(processes):
-        start_time = time.monotonic()
-        obs, dones = sim.sensor_manager.observe(
-            sim_frame=simulation_frame,
-            sim_local_constants=simulation_local_constants,
-            agent_ids=simulation_frame.agent_ids,
-            renderer_ref=sim.renderer,
-            physics_ref=sim.bc,
-            process_count_override=processes,
-        )
-        assert len(obs) > 0
-        return time.monotonic() - start_time
-
-    sensors_instance = Sensors.instance()
-    sensors_instance.get_workers(4, sim_local_constants=sim.local_constants)
-
-    time.sleep(0.5)
-
-    serial_total = observe_with_processes(0)
-    parallel_1_total = observe_with_processes(1)
-    parallel_2_total = observe_with_processes(2)
-    parallel_3_total = observe_with_processes(3)
-    parallel_4_total = observe_with_processes(5)
-
-    assert (
-        serial_total > parallel_1_total
-        or serial_total > parallel_2_total
-        or serial_total > parallel_3_total
-        or serial_total > parallel_4_total
-    ), f"{serial_total}, {parallel_1_total}, {parallel_2_total}, {parallel_3_total} {parallel_4_total}"
+    assert isinstance(observations, dict)
+    assert all(
+        [isinstance(obs, Observation) for obs in observations.values()]
+    ), f"{observations}"
+    assert isinstance(dones, dict)
+    assert all([isinstance(obs, bool) for obs in dones.values()])
+    assert isinstance(other_observations, dict)
+    assert all([isinstance(obs, Observation) for obs in other_observations.values()])
+    assert isinstance(other_dones, dict)
+    assert all([isinstance(obs, bool) for obs in other_dones.values()])
+    assert observations.keys() == other_dones.keys()
+    assert diff_unpackable(other_observations, observations) == ""
