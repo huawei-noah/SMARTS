73aeba95d5f98560a609aa7c4dfc262806774540
Tucker <montgomery.alban@huawei.com>
2023-01-13 00:10:33 -0500

    Remove unused imports.

--- smarts/env/gymnasium/driving_smarts_competition_env.py
+++ smarts/env/gymnasium/driving_smarts_competition_env.py
@@ -31,19 +31,11 @@ from envision.client import Client as Envision
 from envision.client import EnvisionDataFormatterArgs
 from smarts import sstudio
 from smarts.core.agent_interface import (
-    OGM,
-    RGB,
     AgentInterface,
-    DoneCriteria,
-    DrivableAreaGridMap,
-    RoadWaypoints,
-    Waypoints,
 )
 from smarts.core.controllers import ActionSpaceType
 from smarts.env.gymnasium.hiway_env_v1 import HiWayEnvV1, SumoOptions
 from smarts.env.multi_scenario_env import resolve_agent_interface
-from smarts.env.wrappers.format_obs import FormatObsGymnasium
-from smarts.env.gymnasium.wrappers.metrics import CompetitionMetrics
 
 logger = logging.getLogger(__file__)
 logger.setLevel(logging.WARNING)
