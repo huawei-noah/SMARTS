a1b21923e1dd03e2964b3b0516b832c993b46d57
Tucker <montgomery.alban@huawei.com>
2023-01-17 18:14:46 -0500

    Fix key changes in format obs test.

--- smarts/env/tests/test_format_obs.py
+++ smarts/env/tests/test_format_obs.py
@@ -141,7 +141,7 @@ def test_init(make_env):
     rcv_space = env.observation_space
     rcv_space_keys = set([key for key in rcv_space[agent_id]])
 
-    des_space_keys = set(["dist", "ego", "events", "mission"])
+    des_space_keys = set(["distance_travelled", "ego_vehicle_state", "events", "mission"])
     opt_space_keys = [
         intrfc_to_stdobs(intrfc)
         for intrfc, val in cur_intrfcs[0].items()
