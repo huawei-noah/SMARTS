aaa0568a472fbbb6e41f20f33286ffa2a8a2fba2
Montgomery Alban <mtuck2005@yahoo.ca>
2023-02-18 00:04:52 +0000

    Update based on master action space changes.

--- smarts/core/controllers/action_space_type.py
+++ smarts/core/controllers/action_space_type.py
@@ -110,7 +110,7 @@ class ActionSpaceType(Enum):
     """
     RelativeTargetPose = 11
     """
-    Action=(delta_x, delta_y, heading). Type= ``Sequence[float,
+    Action=(delta_x, delta_y, delta_heading). Type= ``Sequence[float,
     float, float]``. Continuous action space of vehicle's next pose in terms of delta x 
-    coordinate, delta y coordinate, and heading, to be reached in 0.1 seconds.
+    coordinate, delta y coordinate, and delta heading, to be reached in 0.1 seconds.
     """
