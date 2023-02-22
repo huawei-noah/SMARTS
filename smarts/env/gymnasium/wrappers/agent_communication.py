e021a065a80b7a9b9990988b61c0a6811ee07e02
Montgomery Alban <mtuck2005@yahoo.ca>
2023-02-22 19:28:14 +0000

    Add docstrings.

--- smarts/env/gymnasium/wrappers/agent_communication.py
+++ smarts/env/gymnasium/wrappers/agent_communication.py
@@ -6,19 +6,29 @@ import numpy as np
 
 
 class Transmitter(NamedTuple):
+    """A configuration utility to set up agents to transmit messages."""
+
     pass
 
 
 class Receiver(NamedTuple):
+    """A configuratoin utility to set up agent to receive messages."""
+
     pass
 
 
 class MessagePasser(gym.Wrapper):
-    def __init__(self, env: gym.Env):
+    """This wrapper augments the observations and actions to require passing messages from agents."""
+
+    def __init__(self, env: gym.Env, message_size_bytes=125000):
         super().__init__(env)
         assert isinstance(env, HiWayEnvV1)
         o_action_space: gym.spaces.Dict = self.env.action_space
-        msg_space = (gym.spaces.Box(low=0, high=256, shape=(125000,), dtype=np.uint8),)
+        msg_space = (
+            gym.spaces.Box(
+                low=0, high=256, shape=(message_size_bytes,), dtype=np.uint8
+            ),
+        )
         self.action_space = gym.spaces.Dict(
             {
                 a_id: gym.spaces.Tuple(
@@ -40,17 +50,17 @@ class MessagePasser(gym.Wrapper):
             }
         )
 
-    def step(self, actions):
+    def step(self, action):
         std_actions = {}
         msgs = {}
-        for a_id, ma in actions.items():
+        for a_id, ma in action.items():
             std_actions[a_id] = ma[0]
             msgs[a_id] = ma[1]
 
         obs, rewards, terms, truncs, infos = self.env.step(std_actions)
         obs_with_msgs = {
             "agents": obs,
-            "messages": {a_id: msgs for a_id, ob in obs.items()},
+            "messages": {a_id: msgs for a_id in obs},
         }
         return obs_with_msgs, rewards, terms, truncs, infos
 
