b960850a3f8dc59a8b69bc1de69d37c4cdaec742
adaickalavan <adaickalavan@gmail.com>
2023-02-27 11:23:09 -0500

    Fix preprocess for multi agent.

--- examples/rl/baseline_single/train/README.md
+++ examples/rl/baseline_single/train/README.md
@@ -4,4 +4,4 @@ $ source ./.venv/bin/activate
 $ pip install --upgrade pip wheel
 $ pip install -e ./../../../.[camera_obs,dev,doc]
 $ pip install -e ./inference/
-$ python3.8 train/run1.py
+$ python3.8 train/run.py --mode=train --head
--- examples/rl/baseline_single/train/preprocess.py
+++ examples/rl/baseline_single/train/preprocess.py
@@ -10,34 +10,51 @@ class Preprocess(gym.Wrapper):
     def __init__(self, env: gym.Env, config, top_down_rgb):
         super().__init__(env)
 
-        self._filter_obs = FilterObs(top_down_rgb=top_down_rgb)
-        self._frame_stack = FrameStack(
-            input_space=self._filter_obs.observation_space,
-            num_stack=config.num_stack,
-            stack_axis=0,
-        )
-        self._frame_stack.reset()
+        self._agent_ids = env.agent_ids
+        self._filter_obs = {
+            agent_id: FilterObs(top_down_rgb=top_down_rgb)
+            for agent_id in self._agent_ids
+        }
+        self._frame_stack = {
+            agent_id: FrameStack(
+                input_space=self._filter_obs[agent_id].observation_space,
+                num_stack=config.num_stack,
+                stack_axis=0,
+            )
+            for agent_id in self._agent_ids
+        }
+        for agent_id in self._agent_ids:
+            self._frame_stack[agent_id].reset()
 
-        self.observation_space = self._frame_stack.observation_space
-        self._format_action = FormatAction()
-        self.action_space = self._format_action.action_space
+        self.observation_space = gym.spaces.Dict({
+            agent_id: self._frame_stack[agent_id].observation_space
+            for agent_id in self._agent_ids
+        })
+        self._format_action = {
+            agent_id: FormatAction()
+            for agent_id in self._agent_ids
+        }
+        self.action_space = gym.spaces.Dict({
+            agent_id: self._format_action[agent_id].action_space
+            for agent_id in self._agent_ids
+        })
 
         print("Policy initialised.")
 
-    def _process(self, obs):
-        obs = self._filter_obs.filter(obs)
-        obs = self._frame_stack(obs)
+    def _process(self, agent_id, obs):
+        obs = self._filter_obs[agent_id].filter(obs)
+        obs = self._frame_stack[agent_id].stack(obs)
         return obs
 
     def step(self, action):
         """Uses the :meth:`step` of the :attr:`env` that can be overwritten to change the returned data."""
         formatted_action = {
-            agent_id: self._format_action.format(agent_action)
+            agent_id: self._format_action[agent_id].format(agent_action)
             for agent_id, agent_action in action.items()
         }
         obs, reward, terminated, truncated, info = self.env.step(formatted_action)
         obs = {
-            agent_id: self._process(agent_obs)
+            agent_id: self._process(agent_id, agent_obs)
             for agent_id, agent_obs in obs.items()
         }
         return obs, reward, terminated, truncated, info
@@ -47,6 +64,11 @@ class Preprocess(gym.Wrapper):
     ):
         """Uses the :meth:`reset` of the :attr:`env` that can be overwritten to change the returned data."""
 
-        self._frame_stack.reset()
+        for agent_id in self._agent_ids:
+            self._frame_stack[agent_id].reset()
         obs, info = self.env.reset(seed=seed, options=options)
-        return self._process(obs), info
+        obs = {
+            agent_id: self._process(agent_id, agent_obs)
+            for agent_id, agent_obs in obs.items()
+        }
+        return obs, info
