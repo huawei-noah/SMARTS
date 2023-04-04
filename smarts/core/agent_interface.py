ed3e52f3323a45b5687fb3c2cdeb83aa253f356d
Tucker Alban <montgomery.alban@huawei.com>
2023-04-04 13:15:12 -0400

    Add actor alive interface (#1919)
    * Add actor alive interface
    
    * Working implentation
    
    * Fix test.
    
    * Make format
    
    * Fix id iterator.
    
    * Add actors alive to conversion.
    
    * Forgotten review address.
    
    * Fix variable length tuple typing.

--- smarts/core/agent_interface.py
+++ smarts/core/agent_interface.py
@@ -20,7 +20,7 @@
 import warnings
 from dataclasses import dataclass, field, replace
 from enum import IntEnum
-from typing import List, Optional, Union
+from typing import List, Optional, Tuple, Union
 
 from smarts.core.controllers import ActionSpaceType
 from smarts.core.lidar_sensor_params import BasicLidar
@@ -194,6 +194,19 @@ class AgentsAliveDoneCriteria:
 
 
 @dataclass(frozen=True)
+class ActorsAliveDoneCriteria:
+    """Require actors to persist."""
+
+    actors_of_interest: Tuple[str, ...] = ()
+    """Actors that should exist to continue this agent."""
+
+    strict: bool = True
+    """If strict the agent will be done instantly if an actor of interest is not available
+    immediately.
+    """
+
+
+@dataclass(frozen=True)
 class EventConfiguration:
     """Configure the conditions in which an Event is triggered."""
 
@@ -225,6 +238,8 @@ class DoneCriteria:
     """
     agents_alive: Optional[AgentsAliveDoneCriteria] = None
     """If set, triggers the ego agent to be done based on the number of active agents for multi-agent purposes."""
+    actors_alive: Optional[ActorsAliveDoneCriteria] = None
+    """If set, triggers the ego agent to be done based on actors existing in the simulation."""
 
 
 @dataclass
