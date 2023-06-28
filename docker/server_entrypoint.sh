4cf6a4f5ae054b2059e5de8522d25f361d58aae9
adaickalavan <adaickalavan@gmail.com>
2023-06-28 06:51:46 +0000

    Avoid bloating of docker container size

--- docker/server_entrypoint.sh
+++ docker/server_entrypoint.sh
@@ -4,8 +4,10 @@
 # a matching user account inside the container
 if [ -n "${HOST_UID}" ]; then
   groupadd --gid "${HOST_GID:-9001}" --force "host-group"
-  useradd  --gid "${HOST_GID:-9001}" --uid "${HOST_UID}" \
-    --create-home "${HOST_NAME:-'user'}"
+  useradd  --gid "${HOST_GID:-9001}" --uid "${HOST_UID}" --no-log-init --create-home "${HOST_NAME:-'user'}"
+  usermod -a -G sudo "${HOST_NAME}"
+  PASSWD="1234"
+  echo "$HOST_NAME:$PASSWD" | chpasswd
   exec gosu "${HOST_UID}" "$@"
 else
   exec "$@"
