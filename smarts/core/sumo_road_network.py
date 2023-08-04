0847dddc1c9e7d1c508b870134d183c8fcbeb9e9
Montgomery Alban <mtuck2005@yahoo.ca>
2023-08-04 20:20:03 +0000

    Fix lane sorting.

--- smarts/core/sumo_road_network.py
+++ smarts/core/sumo_road_network.py
@@ -406,18 +406,19 @@ class SumoRoadNetwork(RoadMap):
                 onlyDirect=True
             ) or self._sumo_lane.getIncoming(onlyDirect=False)
             return sorted(
-                [self._map.lane_by_id(incoming.getID()) for incoming in incoming_lanes]
+                (self._map.lane_by_id(incoming.getID()) for incoming in incoming_lanes),
+                key=lambda l: l.lane_id,
             )
 
         @cached_property
         def outgoing_lanes(self) -> List[RoadMap.Lane]:
             return sorted(
-                [
+                (
                     self._map.lane_by_id(
                         outgoing.getViaLaneID() or outgoing.getToLane().getID()
                     )
                     for outgoing in self._sumo_lane.getOutgoing()
-                ],
+                ),
                 key=lambda l: l.lane_id,
             )
 
