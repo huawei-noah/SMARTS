c1f23682a693fc82f3fe49b08fd0b2828f007eef
Tucker <montgomery.alban@huawei.com>
2023-12-21 11:37:56 -0500

    Rename math to core_math.

--- smarts/core/utils/__init__.py
+++ smarts/core/utils/__init__.py
@@ -17,3 +17,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
+import sys
+from . import core_math as math
+
+sys.modules["smarts.core.utils.math"] = math
