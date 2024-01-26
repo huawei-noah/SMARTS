f2e93449aeec0287917461cdcd6dc790a40ac5b3
Montgomery Alban <mtuck2005@yahoo.ca>
2024-01-26 16:30:16 +0000

    Use a lower port.

--- smarts/core/configuration.py
+++ smarts/core/configuration.py
@@ -72,7 +72,7 @@ _config_defaults: Final = {
     ("ray", "num_gpus"): 0,
     ("ray", "num_cpus"): None,
     ("ray", "log_to_driver"): False,
-    ("sumo", "server_port"): 62232,
+    ("sumo", "server_port"): 8619,
     ("sumo", "server_host"): "localhost",
     ("sumo", "serve_mode"): "remote",  # local|remote
 }
